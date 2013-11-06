ff9ede62a447e4ad83aec253692cd7ab0467d137
Matthew Skomarovsky <matthew@verypolite.com>
2013-11-06 18:23:58 -0500

    added campaign header to group page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	def centered_content(id=nil, &block)
+		content_id = (id.nil? ? nil : id.to_s + "_content")
+		str = content_tag("div", 
+			content_tag("div", 
+				"\n" + capture(&block) + "\n", 
+				{ :class => "centered_content", :id => content_id },
+				false
+			), 
+			{ :class => "centered", :id => id },
+			false
+		)			
+		raw str
+	end
 end
