f361533c55abcf0ebc1e2cdd95e3db8d413c862c
Matthew Skomarovsky <matthew@verypolite.com>
2014-02-11 16:09:30 -0500

    added task for clearing expired sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ActiveRecord::Base
   def self.clear_expired(seconds=nil)
-    seconds ||= 14.days
+    seconds ||= 7.days
 
     # session updated_at field always stored in UTC time zone
     datetime = DateTime.now.ago(seconds).in_time_zone('UTC').to_s(:db) 
