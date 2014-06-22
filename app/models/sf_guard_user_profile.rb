7eea4c2d2f6e561a3bc400cdd4d27cc09483f0aa
Matthew Skomarovsky <matthew@verypolite.com>
2014-06-22 12:35:18 -0400

    user avatar s3 url bug fix

--- app/models/sf_guard_user_profile.rb
+++ app/models/sf_guard_user_profile.rb
@@ -21,8 +21,13 @@ class SfGuardUserProfile < ActiveRecord::Base
 	end
 
   def s3_url(type)
-  	return S3.url(ActionController::Base.helpers.asset_path("images/system/user.png")) if filename.nil?
-    S3.url(image_path(type))
+    if ENV['RAILS_ENV'] == 'production'
+      return ActionController::Base.helpers.asset_path("images/system/user.png") if filename.nil?
+      image_path(type)
+    else
+    	return S3.url(ActionController::Base.helpers.asset_path("images/system/user.png")) if filename.nil?
+      S3.url(image_path(type))
+    end
   end
         
   def image_path(type)
