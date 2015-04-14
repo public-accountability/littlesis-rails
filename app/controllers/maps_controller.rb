218fa675db879ae8f8be6e29003d563059434c3c
Matthew Skomarovsky <matthew@verypolite.com>
2015-04-14 14:04:00 -0400

    more map thumbnails on oligrapher home page

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -52,8 +52,21 @@ class MapsController < ApplicationController
 
   def splash
     @maps = NetworkMap.featured.order("updated_at DESC, id DESC").page(params[:page]).per(50)
+
     @fcc_map = NetworkMap.find(101)
     @ferguson_map = NetworkMap.find(259)
+
+    @shale_map = NetworkMap.find(152)
+    @hadley_map = NetworkMap.find(238)
+    @moma_map = NetworkMap.find(282)
+
+    @lawmaking_map = NetworkMap.find(542)
+    @uc_map = NetworkMap.find(228)
+    @goldwyn_map = NetworkMap.find(431)
+
+    @mugabe_map = NetworkMap.find(266)
+    @goldman_map = NetworkMap.find(157)
+    @berman_map = NetworkMap.find(137)
   end
 
   def show
