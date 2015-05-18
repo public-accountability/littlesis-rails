08ebc0f9a90d1d5b1b74331c2ecb9bc0e1a651d1
Matthew Skomarovsky <matthew@verypolite.com>
2015-05-18 18:17:51 -0400

    fixed setup script

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,10 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => Lilsis::Application.config.aws_key,
-    :aws_secret_access_key  => Lilsis::Application.config.aws_secret,
-  }
-  config.fog_directory  = Lilsis::Application.config.aws_s3_bucket
+unless Rails.env.development?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Lilsis::Application.config.aws_key,
+      :aws_secret_access_key  => Lilsis::Application.config.aws_secret,
+    }
+    config.fog_directory  = Lilsis::Application.config.aws_s3_bucket
+  end
 end
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+git submodule init
 git submodule update
 cp config/database.yml.sample config/database.yml
 cp config/lilsis.yml.sample config/lilsis.yml
