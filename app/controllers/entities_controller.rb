ee4e73fc0dee0d1a2d9c09f96964644ae8d3c4f0
Matthew Skomarovsky <matthew@verypolite.com>
2015-06-09 12:07:48 -0400

    added basic entity json endpoint

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,6 +1,23 @@
 class EntitiesController < ApplicationController
-	before_filter :auth, except: [:relationships]
-  before_action :set_entity, only: [:relationships, :fields, :update_fields, :edit_twitter, :add_twitter, :remove_twitter, :find_articles, :import_articles, :articles, :remove_article, :new_article, :create_article, :find_merges, :merge, :refresh]
+	before_filter :auth, except: [:show, :relationships]
+  before_action :set_entity, only: [:show, :relationships, :fields, :update_fields, :edit_twitter, :add_twitter, :remove_twitter, :find_articles, :import_articles, :articles, :remove_article, :new_article, :create_article, :find_merges, :merge, :refresh]
+
+  def show
+    respond_to do |format|
+      format.json {
+        entity = {
+          id: @entity.id,
+          name: @entity.name,
+          description: @entity.blurb,
+          bio: @entity.summary,
+          primary_type: @entity.primary_ext,
+          image: @entity.featured_image ? @entity.featured_image.s3_url('large') : nil
+        }
+
+        render json: { entity: entity }
+      }
+    end
+  end
 
   def relationships
   end
