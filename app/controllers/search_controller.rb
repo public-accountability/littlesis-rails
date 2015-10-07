d4d792958a16ced7b2982ab360a32c644be5a6e9
Matthew Skomarovsky <matthew@verypolite.com>
2015-10-07 11:02:57 -0400

    added entity pagination to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,14 +6,21 @@ class SearchController < ApplicationController
 
     if @q.present?      
       q = Riddle::Query.escape(@q)
+      page = (params[:page] or 1).to_i
 
-      admin = current_user.present? and current_user.has_legacy_permission("admin")
-      list_is_admin = admin ? [0, 1] : 0
+      @entities = Entity.search("@(name,aliases) #{q}", page: page, match_mode: :extended, with: { is_deleted: false })
 
-      @groups = Group.search("@(name,tagline,description,slug) #{q}", match_mode: :extended)
-      @lists = List.search("@(name,description) #{q}", match_mode: :extended, with: { is_deleted: false, is_admin: list_is_admin, is_network: false })
-      @entities = Entity.search("@(name,aliases) #{q}", match_mode: :extended, with: { is_deleted: false })
-      @maps = NetworkMap.search("@(title,description,index_data) #{q}", match_mode: :extended, with: { is_deleted: false, is_private: false })
+      if page > 1
+        @groups = []
+        @lists = []
+        @maps = []
+      else
+        admin = current_user.present? and current_user.has_legacy_permission("admin")
+        list_is_admin = admin ? [0, 1] : 0
+        @groups = Group.search("@(name,tagline,description,slug) #{q}", per: 50, match_mode: :extended)
+        @lists = List.search("@(name,description) #{q}", per: 50, match_mode: :extended, with: { is_deleted: false, is_admin: list_is_admin, is_network: false })
+        @maps = NetworkMap.search("@(title,description,index_data) #{q}", per: 100, match_mode: :extended, with: { is_deleted: false, is_private: false })
+      end
     end
   end
 end
