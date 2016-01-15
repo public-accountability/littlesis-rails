a5988ce746dba73551879c770c8b45f6f4f9dfdf
Matthew Skomarovsky <matthew@verypolite.com>
2016-01-15 02:52:38 -0500

    latest oligrapher

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,8 +27,7 @@
 //= require twitter/typeahead
 //= require hogan-3.0.1.js
 //= require d3.v3.min
-//= require oligrapher/netmap
-//= require oli2/oligrapher.annotations.min
+//= require oli2/oligrapher.min
 //= require oli2/LsDataConverter
 //= require oli2/LsDataSource
 //= require_tree .
