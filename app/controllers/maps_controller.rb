81f758f2c1e00718ba1ad94d3d98fb2e91aeb4a4
Matthew Skomarovsky <matthew@verypolite.com>
2016-03-22 13:48:42 -0400

    allow normal map view to be embedded in iframes

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -96,6 +96,7 @@ class MapsController < ApplicationController
         @links.push({ text: "edit", url: edit_map_url(@map) }) if is_owner
         @links.push({ text: "share link", url: share_map_url(id: @map.id, secret: @map.secret) }) if @map.is_private and is_owner
 
+        response.headers.delete('X-Frame-Options')
         render "story_map"
       }
       format.json {
