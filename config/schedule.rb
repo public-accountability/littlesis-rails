3f73398b3e88df1e45df7cffb9d69c280dc66e6e
Matthew Skomarovsky <matthew@verypolite.com>
2016-06-20 19:25:11 -0400

    disable map thumbnail generation

--- config/schedule.rb
+++ config/schedule.rb
@@ -36,10 +36,11 @@ every 1.day, at: '5:00 am' do
   rake "ts:index", output: nil
 end
 
-every 1.day, at: '5:30 am' do
-  rake "maps:generate_recent_thumbs", output: nil
-end
+# these tasks broke after upgrade to oligrapher2:
+# every 1.day, at: '5:30 am' do
+#   rake "maps:generate_recent_thumbs", output: nil
+# end
 
-every :sunday, at: "6:00 am" do
-  rake "maps:generate_all_thumbs", output: nil
-end
+# every :sunday, at: "6:00 am" do
+#   rake "maps:generate_all_thumbs", output: nil
+# end
