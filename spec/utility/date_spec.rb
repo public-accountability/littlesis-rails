423b0bd69eefe4d9837b2f37c1a6192ca3abb77d
ziggy <ziggy@elephant-bird.net>
2016-06-22 13:13:44 -0400

    Setup RSpec
    + Added gems: rspec-rails, factory_girl_rails
    + Made sample test for utility/Date
    + tests can be run with command: rspec

---
+++ spec/utility/date_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Date" do
+
+  it "does something" do
+    expect(3).to eq(3)
+  end
+
+  it "does not do something" do
+    expect(:hi).to eq(:bye)
+  end
+
+end 
