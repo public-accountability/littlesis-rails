758182103b3e62363d699e8cd839083a53fe1283
ziggy <ziggy@elephant-bird.net>
2016-07-05 12:43:41 -0400

    View test for index.html.erb
    + added require capybara/rspec to rails_helper

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,25 +2,32 @@ require 'rails_helper'
 
 RSpec.describe ListsController, type: :controller do
 
-  before do
-    @new_list = create(:list)
-    @new_list_2 = create(:list, name: 'my interesting list')
-    @inc = create(:mega_corp_inc)
-    ListEntity.find_or_create_by(list_id: @new_list.id, entity_id: @inc.id)
-    ListEntity.find_or_create_by(list_id: @new_list_2.id, entity_id: @inc.id)
-    get :index
-  end
-  
-  describe "GET #index" do
-    it 'renders the index template' do
-      expect(response).to render_template(:index)
-      expect(response).to be_success
-    end
+  describe '/lists' do
 
-    it '@lists has correct names' do
-      expect(assigns(:lists).length).to eq(2)
-      expect(assigns(:lists)[0].name).to eq("Fortune 1000 Companies")
-      expect(assigns(:lists)[1].name).to eq("my interesting list")
+    before do
+      @new_list = create(:list)
+      @new_list_2 = create(:list, name: 'my interesting list')
+      @inc = create(:mega_corp_inc)
+      ListEntity.find_or_create_by(list_id: @new_list.id, entity_id: @inc.id)
+      ListEntity.find_or_create_by(list_id: @new_list_2.id, entity_id: @inc.id)
+      get :index
     end
+    
+    describe "GET #index" do
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
+        expect(response).to be_success
+      end
+
+      it '@lists has correct names' do
+        expect(assigns(:lists).length).to eq(2)
+        expect(assigns(:lists)[0].name).to eq("Fortune 1000 Companies")
+        expect(assigns(:lists)[1].name).to eq("my interesting list")
+      end
+    end
+  end
+
+  describe '/lists/new' do
+
   end
 end
