8af4c93f954c710fbcff8bde45aa4d7f8c583144
ziggy <ziggy@elephant-bird.net>
2016-07-11 15:28:24 -0400

    Update paper_trail gem
    + added missing factory for references

---
+++ spec/factories/references.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :ref, class: Reference do
+    object_model 'list'
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
 require 'devise'
+require 'paper_trail/frameworks/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
