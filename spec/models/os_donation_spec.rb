1e839cc60f66039653517d45b65be27ed7c49b4d
ziggy <ziggy@elephant-bird.net>
2016-07-25 15:23:46 -0400

    Method to create fec_cycle_id

--- spec/models/os_donation_spec.rb
+++ spec/models/os_donation_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe OsDonation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'create_fec_cycle_id' do 
+    it 'creates id' do 
+      d = OsDonation.new
+      d.cycle = '2010'
+      d.fectransid = '123'
+      expect(d.fec_cycle_id).to be_nil
+      d.create_fec_cycle_id
+      expect(d.fec_cycle_id).to eql('2010_123')
+    end
+  end
+  
 end
