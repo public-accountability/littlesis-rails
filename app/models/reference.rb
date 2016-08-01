3bcf3a0901b5ef10a583f50bcd2317d871fc974f
ziggy <ziggy@elephant-bird.net>
2016-08-01 10:49:42 -0400

    Add field, ref_type, to Reference
    Provides a way to keep track of reference types. The 'type' is an
    integer and the lookup for those values can be found on an class
    var (@@ref_types) on the Reference model.
    
    Right now there are only two types:
    1) Generic
    2) FEC Filing

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,6 +1,11 @@
 class Reference < ActiveRecord::Base
   include SingularTable
-
+  @@ref_types = {1=>"Generic", 2=>"FEC Filing"}
+  
+  def ref_types
+    @@ref_types
+  end
+  
   has_one :os_match
   has_one :os_donation, :through => :os_match
 end
