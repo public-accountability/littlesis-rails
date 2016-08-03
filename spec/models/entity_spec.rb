357a57b8639ab4e3c4e82c65caaeec175d6f1da1
ziggy <ziggy@elephant-bird.net>
2016-08-03 17:02:43 -0400

    Find potential contributions
    + add more indexes to os_donation

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Entity do
   describe 'summary_excerpt' do 
-    
     it 'returns nil if there is no summary' do
       mega_corp = build(:mega_corp_inc, summary: nil)
       expect(mega_corp.summary_excerpt).to be_nil
@@ -19,5 +18,42 @@ describe Entity do
       expect(mega_corp.summary_excerpt.length).to eql(53)
       expect(mega_corp.summary_excerpt).to eql(('x ' * 25) + '...')
     end
+  end
+  
+  describe 'Political' do 
+
+    describe 'sqlize_array' do 
+      it 'stringifies an array for a sql query' do 
+        expect(Entity.sqlize_array ['123', '456', '789']).to eql("('123','456','789')")
+      end
+    end
+
+    describe '#potential_contributions' do
+      before(:all) do
+        DatabaseCleaner.start
+        Entity.skip_callback(:create, :after, :create_primary_ext)
+        @loeb = create(:loeb)
+        @nrsc = create(:nrsc)
+        @loeb_donation = create(:loeb_donation) # relationship model
+        @loeb_os_donation = create(:loeb_donation_one)
+        @loeb_ref_one = create(:loeb_ref_one, object_id: @loeb_donation.id, object_model: "Relationship")
+        @donation_class = create(:donation, relationship_id: @loeb_donation.id)
+        @os_match = OsMatch.create(
+          os_donation_id: @loeb_os_donation.id,
+          donation_id: @donation_class.id,
+          donor_id: @loeb.id,
+          recip_id: @nrsc.id,
+          reference_id: @loeb_ref_one.id,
+          relationship_id: @loeb_donation.id)
+      end
+      after(:all) do
+        Entity.set_callback(:create, :after, :create_primary_ext)
+        DatabaseCleaner.clean
+      end
+
+      it 'retrieves matched contributions'
+    
+    end
+
   end
 end
