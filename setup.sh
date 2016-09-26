796f850c10c42ee187587446096cd2d5755b99e9
ziggy <ziggy@elephant-bird.net>
2016-09-26 16:53:46 -0400

    Add sample Passengerfile + update setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ git submodule init
 git submodule update
 cp config/database.yml.sample config/database.yml
 cp config/lilsis.yml.sample config/lilsis.yml
+cp config/Passengerfile.json.sample ./Passengerfile.json
