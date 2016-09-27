bb6004c4927830474ff157b2533734dde85b7f6d
ziggy <ziggy@elephant-bird.net>
2016-09-27 12:15:39 -0400

    Don't run tests in development mode
    + passenger gitignore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort("The Rails environment is running in development mode!") if Rails.env.development?
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
