555fa37da4a5e56b15223ed3ce52361caf6bcd82
ziggy <ziggy@elephant-bird.net>
2016-10-12 14:32:04 -0400

    Potential contributions search on NyDisclosure

--- app/models/ny_disclosure.rb
+++ app/models/ny_disclosure.rb
@@ -2,16 +2,44 @@ class NyDisclosure < ActiveRecord::Base
   #self.table_name = "d_sample" # FOR TESTING
 
   has_one :ny_match, inverse_of: :ny_disclosure
+  belongs_to :ny_filer, class_name: "NyFiler", foreign_key: "filer_id", primary_key: "filer_id"
 
   def full_name
-    unless first_name.nil? and last_name.nil?
-      middle_name = mid_init.nil? ? " " : " #{mid_init} "
-      "#{first_name.to_s}#{middle_name}#{last_name.to_s}".titleize
+    if corp_name.present?
+      corp_name
+    else
+      unless first_name.nil? and last_name.nil?
+        middle_name = mid_init.nil? ? " " : " #{mid_init} "
+        "#{first_name.to_s}#{middle_name}#{last_name.to_s}".titleize
+      end
     end
   end
   
   def is_matched
     !ny_match.blank?
+  end
+ 
+  def contribution_attributes
+    {
+      name: full_name,
+      address: format_address,
+      date: original_date.nil? ? schedule_transaction_date : original_date,
+      amount: amount1,
+      filer_id: filer_id,
+      filer_name: ny_filer.name.titleize
+    }
+  end
+
+  def self.potential_contributions(name)
+    search(name, :with => { :is_matched => false }, :sql => { :include => :ny_filer } )
+      .map(&:contribution_attributes)
+  end
+
+  private 
+
+  def format_address
+    look_nice = lambda { |x| x.to_s.titleize }
+    [ look_nice.call(address), look_nice.call(city) + ',', state.to_s, zip.to_s ].join(' ')
   end
 
 end
---
+++ spec/models/ny_disclosure_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe NyDisclosure, type: :model do
+
+  describe '#full_name' do 
+    
+    it 'returns corp_name if it exists' do 
+      d = build(:ny_disclosure, corp_name: 'corp inc')
+      expect(d.full_name).to eql 'corp inc'
+    end
+
+    it 'returns formatted name' do 
+      d = build(:ny_disclosure, first_name: 'ALICE', last_name: 'COLTRANE')
+      expect(d.full_name).to eql 'Alice Coltrane'
+      d2 = build(:ny_disclosure, first_name: 'ALICE', last_name: 'COLTRANE', mid_init: 'X')
+      expect(d2.full_name).to eql 'Alice X Coltrane'
+    end
+    
+    it 'returns nil otherwise' do 
+      d = build(:ny_disclosure)
+      expect(d.full_name).to be nil
+    end
+
+  end
+
+end
