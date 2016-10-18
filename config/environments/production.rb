39ab6fd6197dd31ea28d41b40ce2b36aa9060212
ziggy <ziggy@elephant-bird.net>
2016-10-18 10:33:01 -0400

    Add nys.js to precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Lilsis::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( nys.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
