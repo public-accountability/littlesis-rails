be0a0d293fc01b9eb49f6e8c24591ba3e14d4804
ziggy <ziggy@littlesis.org>
2017-01-09 16:56:11 -0500

    Add all categories to #relationshipDetails

--- app/assets/javascripts/common/utility.js
+++ app/assets/javascripts/common/utility.js
@@ -64,33 +64,63 @@ utility.relationshipCategories = [
 
 /**
  * Returns an nested array of [ display, fieldname, type ] 
- * possible types: 'text', 'date', 'boolean', 'money'
+ * possible types: 'text', 'date', 'boolean', 'money', 'number'
  * @param {number} category
  */
 utility.relationshipDetails = function(category) {
-  // fields are common to multiple categories and are reusable
+  // reusable fields that are common to multiple categories
   var title = ['Title', 'description1', 'text'];
   var isCurrent = ['Is current?', 'is_current', 'boolean'];
   var startDate = ['Start date', 'start_date', 'date' ];
   var endDate = ['End date', 'end_date', 'date' ];
-  
-  // 
+  var type = ['Type', 'description1', 'text'];
+  var amount = ['Amount', 'amount', 'money'];
+  var goods = ['Goods', 'goods', 'text'];
+  var d1 = ['entity 1 is __ of entity 2', 'description1', 'text'];
+  var d2 = ['entity 2 is __ of entity 1', 'description2', 'text'];
+      
   switch(category) {
   case 1: // postition
     return [
       title, isCurrent, startDate, endDate,
       ['Board member?', 'is_board', 'boolean' ],
       ['Executive?', 'is_executive', 'boolean' ],
-      ['Compensation','', 'boolean', 'money' ]
+      ['Compensation','compensation', 'money' ]
     ];
   case 2: // eduction
     return [
-      ['Type', 'description1', 'text'],
-      startDate, endDate,
+      type, startDate, endDate,
       ['Degree', 'degree', 'text'],
       ['Field', 'education_field', 'text'],
       ['Dropout?', 'is_dropout', 'boolean']
     ];
+  case 3: // members
+    return [
+      title, startDate, endDate, isCurrent,
+      ['Membership Dues', 'membership_dues', 'money']
+    ];
+  case 4: // family
+    return [ d1, d2, startDate, endDate, isCurrent ];
+  case 5: // donation
+    return [ type, amount, startDate, endDate, isCurrent, goods ];
+  case 6: // transaction
+    return [ d1, d2, amount, startDate, endDate, isCurrent, goods ];
+  case 7: // lobby
+    throw 'Lobbying relationships are not currently supposed by the bulk add tool';
+  case 8: // social
+    return [ d1, d2, startDate, endDate, isCurrent ];
+  case 9: // professional
+    return [ d1, d2, startDate, endDate, isCurrent ];
+  case 10: // ownership
+    return [ 
+      title, startDate, endDate, isCurrent,
+      [ 'Percent Stake', 'percent_stake', 'number'],
+      [ 'Shares Owned', 'shares_owned', 'number']
+    ];
+  case 11: // hierarchy
+    return [ d1, d2, startDate, endDate, isCurrent ];
+  case 12: // generic
+    return [ d1, d2, startDate, endDate, isCurrent ];
   default:
     throw 'Invalid relationship category. It must a be a number between 1 and 12';
   }
--- test/test.utility.js
+++ test/test.utility.js
@@ -7,4 +7,26 @@ describe('utility', function(){
       expect(utility.range(4, [1,2])).to.eql([0,3]);
     });
   });
+
+  describe('RelationshipDetails', function(){
+    it('returns an nested array', function(){
+      utility.range(13, [7]).slice(1).forEach( i => {
+	var details = utility.relationshipDetails(i);
+	expect(details).to.be.instanceof(Array);
+	details.forEach( x => expect(x).to.be.instanceof(Array));
+	details.forEach( detail => {
+	  expect(detail.length).eql(3);
+	  detail.forEach( x => expect(x).to.be.a('string'));
+	});
+      });
+    });
+    it('throws errors if not between 1-12 and not 7', function(){
+      utility.range(13, [0, 7]).forEach( i => {
+	expect(() => utility.relationshipDetails(i)).to.not.throw(Error);
+      });
+      expect(() => utility.relationshipDetails(7)).to.throw(/Lobbying/);
+      expect(() => utility.relationshipDetails(0)).to.throw(/Invalid relationship/);
+      expect(() => utility.relationshipDetails(13)).to.throw(/Invalid relationship/);
+    });
+  });
 });
