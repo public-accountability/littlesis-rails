a6d44b990967c9446e803de9618d06a5ff15647b
ziggy <ziggy@littlesis.org>
2017-01-09 12:21:47 -0500

    OsMatch & test: make the linter happier

--- app/models/os_match.rb
+++ app/models/os_match.rb
@@ -2,20 +2,18 @@
 class OsMatch < ActiveRecord::Base
   # include SoftDelete
   # has_paper_trail
-  
+
   belongs_to :os_donation
   belongs_to :donation, inverse_of: :os_matches
-  belongs_to :donor, class_name: "Entity", foreign_key: "donor_id", inverse_of: :matched_contributions
-  belongs_to :recipient, class_name: "Entity", foreign_key: "recip_id", inverse_of: :donors
-  belongs_to :committee, class_name: "Entity", foreign_key: "cmte_id", inverse_of: :committee_donors
+  belongs_to :donor, class_name: 'Entity', foreign_key: 'donor_id', inverse_of: :matched_contributions
+  belongs_to :recipient, class_name: 'Entity', foreign_key: 'recip_id', inverse_of: :donors
+  belongs_to :committee, class_name: 'Entity', foreign_key: 'cmte_id', inverse_of: :committee_donors
   belongs_to :reference
   belongs_to :relationship, inverse_of: :os_matches
-  belongs_to :user, foreign_key: "matched_by"
+  belongs_to :user, foreign_key: 'matched_by'
 
   validates_presence_of :os_donation_id, :donor_id
-
   after_create :post_process
-
   after_destroy :unmatch
 
   def post_process
@@ -62,18 +60,17 @@ class OsMatch < ActiveRecord::Base
       entity2_id: recipient.id,
       category_id: 5)
     update_attribute(:relationship, r)
-    
-    r.last_user_id = 1    
-    r.description1 = "Campaign Contribution" 
-    r.description2 = "Campaign Contribution"
-    
+
+    r.last_user_id = 1
+    r.description1 = 'Campaign Contribution'
+    r.description2 = 'Campaign Contribution'
+
     r.update_os_donation_info
 
     r.update_start_date_if_earlier os_donation.date
     r.update_end_date_if_later os_donation.date
-    
-    r.save
 
+    r.save
   end
 
   #  Int -> Int | Nil
@@ -92,20 +89,21 @@ class OsMatch < ActiveRecord::Base
   def create_reference
     unless relationship.nil?
       ref = Reference.find_or_create_by!(
-        name: os_donation.reference_name, 
-        source: os_donation.reference_source, 
+        name: os_donation.reference_name,
+        source: os_donation.reference_source,
         publication_date: os_donation.date.to_s,
         object_model: 'Relationship',
         object_id: relationship.id,
         ref_type: 2,
-        last_user_id: 1)
+        last_user_id: 1
+      )
       update_attribute(:reference, ref)
     end
   end
 
   # output: <Entity> or Nil
   def find_or_create_cmte
-    fundraising = PoliticalFundraising.includes(:entity).find_by(fec_id: os_donation.cmteid, entity: {is_deleted: false})
+    fundraising = PoliticalFundraising.includes(:entity).find_by(fec_id: os_donation.cmteid, entity: { is_deleted: false })
     if fundraising.nil?
       cmte = OsCommittee.find_by(cmte_id: os_donation.cmteid, cycle: os_donation.cycle)
       if cmte.nil?
@@ -124,26 +122,24 @@ class OsMatch < ActiveRecord::Base
   def unmatch
     relationship.update_os_donation_info.save!
 
-    if relationship.filings == 0 
-      relationship.soft_delete 
+    if relationship.filings.zero?
+      relationship.soft_delete
       relationship.donation.destroy
-      relationship.links.each { |link| link.delete }
+      relationship.links.each(&:delete)
     end
-    
     reference.destroy
   end
-  
+
   # input <OsCommittee>
   # output <Entity> or nil
   def self.create_new_cmte(cmte)
     if cmte.name.blank?
-      return nil
+      nil
     else
-      entity = Entity.create!(name: cmte.name, primary_ext: "Org")
+      entity = Entity.create!(name: cmte.name, primary_ext: 'Org')
       ExtensionRecord.create!(entity_id: entity.id, definition_id: 11, last_user_id: 1)
       PoliticalFundraising.create!(fec_id: cmte.cmte_id, entity_id: entity.id)
       entity
     end
   end
-  
 end
