e452c3ec55aeb4522b5677961bf98b2c0f14f7a6
ziggy <ziggy@littlesis.org>
2017-01-10 14:23:57 -0500

    Extract data from table

--- app/assets/javascripts/pages/tools.js
+++ app/assets/javascripts/pages/tools.js
@@ -9,38 +9,44 @@ var bulkAdd = (function($, utility){
   // input [str]
   function td(col) {
     var isBooleanColumn = col[2] === 'boolean';
-    var editable = isBooleanColumn ? 'false' : 'true';
-    var td = $('<td>', { contenteditable: editable, text: '' });
-    if (isBooleanColumn) {
-      // include checkbox if boolean
-      return td.append('<input type="checkbox">');
-    } else {
-      return td;
-    }
-  }
-
-  function relationshipSelect() {
-    var categories = (utility.entityInfo('entitytype') === 'Org') ?
-	  utility.range(13, [4,7,8,9]) :
-	  utility.range(13, [7]);
-    
-    var options = categories.map(function(i) {
-      return $('<option>', { value: i, text: utility.relationshipCategories[i] });
-    });
-    return $('<select>').append(options);
+    var td = $('<td>', { contenteditable: isBooleanColumn ? 'false' : 'true' });
+    // include checkbox if boolean
+    return isBooleanColumn ? td.append('<input type="checkbox">') : td;
   }
   
   // Adds column titled (located at col[0]) to thead
   // [ string ] -> 
   function addColToThead(col) {
-    $('#table thead tr').append($('<th>', {text: col[0] }));
+    $('#table thead tr').append(
+      $('<th>', {
+	text: col[0], 
+	data: { 'colName': col[1], 'colType': col[2] }
+      })
+    );
   }
   
+  // -> [[]]
+  function relationshipDetails() {
+    var category = Number($('#relationship-cat-select option:selected').val());
+    var entityColumns = [ [ 'Name or ID', 'name', 'text'], ['Blurb', 'blurb', 'text'], ['Entity type', 'primary_ext', 'text'] ];
+    return entityColumns.concat(utility.relationshipDetails(category));
+  }
+
+  function relationshipDetailsAsObject() {
+    return relationshipDetails().map(function(x) {
+      return {
+	display: x[0],
+	key: x[1],
+	type: x[2]
+      };
+    });
+  }
+
+
   // recreates table with provded relationship category
-  // int -> 
-  function createTable(selectedCat) {
+  function createTable() {
     $('#table table').html('<thead><tr></tr></thead><tbody></tbody>');
-    utility.relationshipDetails(selectedCat).forEach(addColToThead);
+    relationshipDetails().forEach(addColToThead);
     $('#table thead tr').append('<th><span class="glyphicon glyphicon-plus table-add"></span></th>');
   }
 
@@ -49,11 +55,6 @@ var bulkAdd = (function($, utility){
     $('#relationship-cat-select').change(function(x){
       createTable(Number($(this).find('option:selected').val()));
     });
-  }
-
-  function relationshipDetails() {
-    var category = Number($('#relationship-cat-select option:selected').val());
-    return utility.relationshipDetails(category);
   }
 
   function appendNewRow() {
@@ -71,19 +72,56 @@ var bulkAdd = (function($, utility){
     });
   } 
 
+  
+  // this returns the cell data according to it's type
+  // Most types simply need to return the text, but a few,
+  // such as boolean require a different step
+  function extractCellData(cell, type) {
+    if (type === 'boolean') {
+      // Technically we should allow three values for this field: true, false, and null.
+      // However, to keep things simple, right now the false/un-checked state defaults to null
+      // So in this tool there is no way of saying that a person is NOT a board member.
+      return cell.find('input').is(':checked') ? true : null; 
+    } else {
+      return cell.text();
+    }
+  }
+  
+  //  [{}], element -> {}
+  function rowToJson(tableDetails, row) {
+    var obj = {};
+    tableDetails.forEach(function(rowInfo,i) {
+      var cell = $(row).find('td:nth-child(' + (i + 1) + ')');
+      obj[rowInfo.key] = extractCellData(cell, rowInfo.type);
+    });
+    return obj;
+  }
+
+  // str -> [ {} ]
+  // Given the selector of a <table> it returns the data as an array
+  // of objects
+  function tableToJson(selector) {
+    var _rowToJson = rowToJson.bind(null, relationshipDetailsAsObject());
+    return $(selector + ' tbody tr').map(function(){
+      return _rowToJson(this);
+    }).toArray();
+  }
+
   function exportClick() {
     $('#export-btn').click(function() {
+      console.log(tableToJson('#table'));
     });
   }
   
   return {
     appendNewRow: appendNewRow,
-    relationshipSelect: relationshipSelect,
     relationshipDetails: relationshipDetails,
     createTable: createTable,
+    tableToJson: tableToJson,
     init: function() { 
       tableSetup();
       addRemove();
+      exportClick();
     }
   };
 
--- test/test.bulkAdd.js
+++ test/test.bulkAdd.js
@@ -4,44 +4,77 @@ describe('bulkAdd', function(){
   describe('createTable()', function() {
     it('adds thead and tbody', sinon.test(function() {
       var html = this.stub($.fn, 'html');
-      bulkAdd.createTable(1);
+      this.stub($.fn, 'val').returns(1);
+      bulkAdd.createTable();
       expect(html.calledOnce).to.eql(true);
       expect(html.firstCall.args[0]).to.eql('<thead><tr></tr></thead><tbody></tbody>');
     }));
     
     it('appends <th> 8 times with position', sinon.test(function() {
       this.stub($.fn, 'html');
+      this.stub($.fn, 'val').returns(1);
       var append = this.stub($.fn, 'append');
-      bulkAdd.createTable(1);
-      expect(append.callCount).to.eql(8);
+      bulkAdd.createTable();
+      expect(append.callCount).to.eql(11);
     }));
     
     it('appends <th> 7 times with eduction', sinon.test(function() {
       this.stub($.fn, 'html');
+      this.stub($.fn, 'val').returns(2);
       var append = this.stub($.fn, 'append');
-      bulkAdd.createTable(2);
-      expect(append.callCount).to.eql(7);
+      bulkAdd.createTable();
+      expect(append.callCount).to.eql(10);
     }));
   });
 
-  
-  describe('relationshipSelect', function(){
-    before( () => {
-      var stub = sinon.stub(utility, "entityInfo");
-      stub.onCall(0).returns('Org');
-      stub.onCall(1).returns('Person');
+
+  describe('tableToJson', function(){
+
+    before(function() {
+      $('#test').html('<table><thead><tr></tr><tbody><tr></tr></tbody></table>');
+      utility.range(10).forEach(function(i){
+	$('#test tbody tr').append($('<td>', { text: i}));
+      });
     });
+    after( () => $('#test').empty() );
     
-    after( () => utility.entityInfo.restore() );
-
-    it('returns list with 9 options when entity is an org', function(){
-      expect(bulkAdd.relationshipSelect().children().length).to.eql(9);
-    });
-
-    it('returns list with 12 options when entity is a person', function(){
-      expect(bulkAdd.relationshipSelect().children().length).to.eql(12);
-    });
-
+    it('returns table as json', sinon.test(function() {
+      this.stub($.fn, 'val').returns(1);
+      var result = bulkAdd.tableToJson('table');
+      console.log(result);
+      var expected = [{
+	name: '0',
+	blurb: '1',
+	primary_ext: '2',
+	description1: '3',
+	is_current: '4',
+	start_date: '5',
+	end_date: '6',
+	is_board: '7',
+	is_executive: '8',
+	compensation: '9'
+      }];
+      expect(result).to.eql(expected);
+    }));
+    
   });
+  
+  // describe('relationshipSelect', function(){
+  //   before( () => {
+  //     var stub = sinon.stub(utility, "entityInfo");
+  //     stub.onCall(0).returns('Org');
+  //     stub.onCall(1).returns('Person');
+  //   });
+    
+  //   after( () => utility.entityInfo.restore() );
+
+  //   it('returns list with 9 options when entity is an org', function(){
+  //     expect(bulkAdd.relationshipSelect().children().length).to.eql(9);
+  //   });
+
+  //   it('returns list with 12 options when entity is a person', function(){
+  //     expect(bulkAdd.relationshipSelect().children().length).to.eql(12);
+  //   });
+  // });
 
 });
