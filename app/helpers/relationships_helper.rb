4cc44a3e3873ac1f8d0894625ab5e7a45baba998
ziggy <ziggy@elephant-bird.net>
2017-01-25 16:03:15 -0500

    Minor style changes to edit relationship page

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -76,7 +76,7 @@ module RelationshipsHelper
 
   def description_fields(f)
     return nil unless requires_description_fields
-    content_tag(:div) do
+    content_tag(:div, id: 'description-fields') do
       [entity_link(@relationship.entity),
        ' is ',
        f.text_field(:description1),
