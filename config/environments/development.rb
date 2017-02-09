bd483f33cc5ddb5a71dcef64670fbfcc77cda247
ziggy <ziggy@elephant-bird.net>
2017-02-09 12:46:58 -0500

    Fix sass/css cdn issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Lilsis::Application.configure do
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.assets.css_compressor = :sass
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Lilsis::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
