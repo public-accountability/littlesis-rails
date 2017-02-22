3c2a0f68d31cc0c1fcbe0629e5a4426ec3dccf4b
ziggy <ziggy@littlesis.org>
2017-02-22 14:56:19 -0500

    Change dev embedded route to /embedded/v2

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -10,7 +10,7 @@ class MapsController < ApplicationController
   protect_from_forgery except: [:create, :clone]
 
   # defaults for embedded oligrapher
-  EMBEDDED_HEADER_PCT = 10
+  EMBEDDED_HEADER_PCT = 8
   EMBEDDED_ANNOTATION_PCT = 28
 
   def index
@@ -77,7 +77,7 @@ class MapsController < ApplicationController
     @berman_map = NetworkMap.find(137)
   end
 
-  def dev_embedded
+  def embedded_v2
     @dev_version = true
     @header_pct = embedded_params.fetch(:header_pct, EMBEDDED_HEADER_PCT)
     @annotation_pct = embedded_params.fetch(:annotation_pct, EMBEDDED_ANNOTATION_PCT)
