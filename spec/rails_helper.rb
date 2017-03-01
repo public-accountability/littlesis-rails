10663556fdd61af83b1eac1588bcbe5d7f761e7e
ziggy <ziggy@littlesis.org>
2017-03-01 11:09:44 -0500

    disable sphinx test configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,27 +42,26 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  
 
-  config.before :each do |example|
-    # Configure and start Sphinx for request specs
-    if example.metadata[:type] == :request
-      ThinkingSphinx::Test.init
-      ThinkingSphinx::Test.start index: false
-    end
+  # config.before :each do |example|
+  #   # Configure and start Sphinx for request specs
+  #   if example.metadata[:type] == :request
+  #     ThinkingSphinx::Test.init
+  #     ThinkingSphinx::Test.start index: false
+  #   end
 
-    # Disable real-time callbacks if Sphinx isn't running
-    ThinkingSphinx::Configuration.instance.settings['real_time_callbacks'] =
-      (example.metadata[:type] == :request)
-  end
+  #   # Disable real-time callbacks if Sphinx isn't running
+  #   ThinkingSphinx::Configuration.instance.settings['real_time_callbacks'] =
+  #     (example.metadata[:type] == :request)
+  # end
 
-  config.after(:each) do |example|
-    # Stop Sphinx and clear out data after request specs
-    if example.metadata[:type] == :request
-      ThinkingSphinx::Test.stop
-      ThinkingSphinx::Test.clear
-    end
-  end
+  # config.after(:each) do |example|
+  #   # Stop Sphinx and clear out data after request specs
+  #   if example.metadata[:type] == :request
+  #     ThinkingSphinx::Test.stop
+  #     ThinkingSphinx::Test.clear
+  #   end
+  # end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
