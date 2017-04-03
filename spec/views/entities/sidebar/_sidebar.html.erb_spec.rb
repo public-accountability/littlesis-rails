46586a1d12f8257a969cc39b7a8b6dc3ba2da556
ziggy <ziggy@littlesis.org>
2017-04-03 13:22:14 -0400

    admin tools section on sidebar

---
+++ spec/views/entities/sidebar/_sidebar.html.erb_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+describe "partial: sidebar" do
+  before do
+      org = build(:org, org: build(:organization), id: rand(1000))
+      assign(:entity, org)
+  end
+
+  describe 'layout' do
+    before { render partial: 'entities/sidebar.html.erb' }
+
+    it 'renders partial sidebar/image' do
+      expect(view).to render_template(partial: 'entities/sidebar/_image')
+    end
+
+    it 'has basic info' do
+      css 'span.sidebar-title-text', text: 'Basic info'
+    end
+  end 
+  
+  describe 'Admin tools' do
+    context 'When admin' do
+      before do
+        allow(view).to receive(:user_signed_in?).and_return(true)
+        allow(view).to receive(:current_user).and_return(double(:admin? => true))
+        render partial: 'entities/sidebar.html.erb' 
+      end
+      it 'has admin tools' do
+        css 'span.sidebar-title-text', text: 'Admin tools'
+        css 'a', text: 'Addresses'
+      end
+    end
+
+    context 'When not admin' do
+      before do
+        allow(view).to receive(:user_signed_in?).and_return(true)
+        allow(view).to receive(:current_user).and_return(double(:admin? => false))
+        render partial: 'entities/sidebar.html.erb' 
+      end
+
+      it 'has admin tools' do
+        not_css 'span.sidebar-title-text', text: 'Admin tools'
+      end
+    end
+  end
+end
