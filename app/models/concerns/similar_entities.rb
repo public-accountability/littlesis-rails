c83ba785a6c65f2b4a67192e97b9103681c4e104
ziggy <ziggy@littlesis.org>
2017-04-04 13:23:57 -0400

    don't search summary field when looking for similar entities

--- app/models/concerns/similar_entities.rb
+++ app/models/concerns/similar_entities.rb
@@ -3,11 +3,11 @@ require 'active_support/concern'
 module SimilarEntities
   extend ActiveSupport::Concern
 
-  SIMILAR_ENTITY_FIELD_WEIGHTS = { name: 15, aliases: 10, blurb: 3, summary: 1 }.freeze
+  SIMILAR_ENTITY_FIELD_WEIGHTS = { name: 15, aliases: 10, blurb: 3 }.freeze
 
   # -> [ <Entity> ]
   def similar_entities(per_page = 5)
-    Entity.search(generate_search_terms,
+    Entity.search("@!summary #{generate_search_terms}",
                   :with => { primary_ext: "'#{primary_ext}'", is_deleted: false },
                   :without => { sphinx_internal_id: id },
                   :per_page => per_page,
@@ -24,14 +24,20 @@ module SimilarEntities
     alias_names = aliases.map(&:name)
 
     search_terms.concat(alias_names)
-    search_terms.append("#{person.name_first} #{person.name_last}") if person?
-    search_terms.append("#{person.name_first} #{person.name_last}*") if person?
-    search_terms.append("#{person.name_first}* #{person.name_last}") if person?
-    search_terms.concat(alias_names.map { |n| Org.strip_name(n) }) if org?
-    search_terms.append("*#{self.name}*") if org?
+    search_terms.append(ts_escape("#{person.name_first} #{person.name_last}")) if person?
+    search_terms.append(ts_surround_escape("#{person.name_first} * #{person.name_last}")) if person?
 
-    search_terms.uniq
-      .map { |name| "(#{ThinkingSphinx::Query.escape(name)})" }
-      .join(' | ')
+    search_terms.concat(alias_names.map { |n| ts_escape(Org.strip_name(n)) }) if org?
+    search_terms.append("*#{ts_escape(self.name)}*") if org?
+
+    search_terms.uniq.map { |term| "(#{term})" }.join(' | ')
+  end
+
+  def ts_surround_escape(x)
+    '"' + ts_escape(x) + '"'
+  end
+
+  def ts_escape(x)
+    ThinkingSphinx::Query.escape(x)
   end
 end
