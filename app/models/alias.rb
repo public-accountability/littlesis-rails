3647b3b42c7a01dfdb633bfd9fb9007f7cd5cf65
ziggy <ziggy@littlesis.org>
2017-04-10 17:31:47 -0400

    layout for edit/add aliases

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,10 +1,11 @@
 class Alias < ActiveRecord::Base
   include SingularTable
-  
-  belongs_to :entity, inverse_of: :aliases
+
+  belongs_to :entity, inverse_of: :aliases, touch: true
 
   def name_regex(require_first = true)
     return nil unless person = NameParser.parse_to_person(name)
     person.name_regex(require_first)
   end
 end
+
