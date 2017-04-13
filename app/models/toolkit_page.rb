c8a1ad122b2eb5ca369cbd034f4119ad4db5a185
ziggy <ziggy@littlesis.org>
2017-04-13 17:49:12 -0400

    downcase and add underscores to toolkit page name

--- app/models/toolkit_page.rb
+++ app/models/toolkit_page.rb
@@ -1,6 +1,12 @@
 class ToolkitPage < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
-  
+  before_validation :modify_name
   belongs_to :last_user, foreign_key: "last_user_id", class_name: 'User'
   
+  private
+
+  def modify_name
+    return if persisted? || name.nil?
+    self.name = name.downcase.tr(' ', '_')
+  end
 end
--- spec/models/toolkit_page_spec.rb
+++ spec/models/toolkit_page_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 
 describe ToolkitPage, type: :model do
   describe 'validations' do
-    subject { ToolkitPage.new(name: "page_name") }
-    it { should validate_uniqueness_of(:name) }
+    subject { ToolkitPage.new(name: "page") }
+    it { should validate_uniqueness_of(:name).case_insensitive }
     it { should validate_presence_of(:name) }
     it { should belong_to(:last_user) }
   end
+
+  describe '#modify_name' do
+    it 'changes name' do
+      page1 = ToolkitPage.create!(name: 'Page Name')
+      page2 = ToolkitPage.create!(name: 'good_name')
+      page3 = ToolkitPage.create!(name: 'okay')
+      page4 = ToolkitPage.create(name: nil)
+      page5 = ToolkitPage.create!(name: 'multi word name')
+      expect(page1.name).to eq 'page_name'
+      expect(page2.name).to eq 'good_name'
+      expect(page3.name).to eq 'okay'
+      expect(page4.name).to be nil
+      expect(page5.name).to eq 'multi_word_name'
+    end
+  end
+  
 end
