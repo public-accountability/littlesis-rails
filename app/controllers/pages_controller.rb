6e9c9c041e24fdaaed67695dd4fa0db5f771edb2
ziggy <ziggy@littlesis.org>
2017-04-20 17:28:01 -0400

    Re-design oligrapher splash page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,14 @@ class PagesController < ApplicationController
     @maps = NetworkMap.featured.order("updated_at DESC, id DESC").page(params[:page]).per(50)
 
     @fcc_map = NetworkMap.find(101)
+    @lawmaking_map = NetworkMap.find(542)
     @ferguson_map = NetworkMap.find(259)
 
     @shale_map = NetworkMap.find(152)
     @hadley_map = NetworkMap.find(238)
     @moma_map = NetworkMap.find(282)
 
-    @lawmaking_map = NetworkMap.find(542)
-    @uc_map = NetworkMap.find(228)
-    @goldwyn_map = NetworkMap.find(431)
-
-    @mugabe_map = NetworkMap.find(266)
-    @goldman_map = NetworkMap.find(157)
-    @berman_map = NetworkMap.find(137)
+    render layout: 'splash'
   end
 
   
