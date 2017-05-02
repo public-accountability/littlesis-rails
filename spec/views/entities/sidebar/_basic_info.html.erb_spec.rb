44430127f51a4a83487b91a96a45c7be146d5378
ziggy <ziggy@littlesis.org>
2017-05-02 18:41:22 -0400

    remove underscore in basic info and sanitize links

---
+++ spec/views/entities/sidebar/_basic_info.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "partial: sidebar/basic_info" do
+  before do
+    @org = create(:org, start_date: '1978-01-01', website: 'http://example.com')
+    assign(:entity, @org)
+    render partial: 'entities/sidebar/basic_info.html.erb',
+           locals: { basic_info: @org.basic_info }
+  end
+
+  it 'has one table' do
+    css 'table', count: 1
+  end
+
+  it 'has one link' do
+    css 'a'
+  end
+
+  it 'turns "start_date" into "Start date"' do
+    expect(rendered).to match '<strong>Start date</strong>'
+  end
+end
