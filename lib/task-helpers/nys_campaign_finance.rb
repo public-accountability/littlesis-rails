4a47e725736e37b77fe87e84ce7d526aa4b4a196
ziggy <ziggy@littlesis.org>
2017-05-04 10:53:21 -0400

    load donation data into staging table with INFILE

--- lib/task-helpers/nys_campaign_finance.rb
+++ lib/task-helpers/nys_campaign_finance.rb
@@ -8,7 +8,7 @@ module NYSCampaignFinance
   def self.create_staging_table
     ActiveRecord::Base.connection.create_table STAGING_TABLE_NAME do |t|
       t.string :filer_id, limit: 10, null: false
-      t.string :report_id
+      t.string :report_id, null: false
       t.string :transaction_code, limit: 1, null: false
       t.string :e_year, limit: 4, null: false
       t.integer :transaction_id, null: false
@@ -43,11 +43,11 @@ module NYSCampaignFinance
   end
 
   def self.import_disclosure_data(file)
-    sql = "LOAD DATA INFILE '#{Pathname.new(file).expand_path}'
+    sql = "LOAD DATA LOCAL INFILE '#{Pathname.new(file).expand_path}'
            INTO TABLE #{STAGING_TABLE_NAME}
            FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
            LINES TERMINATED BY '\\r\\n'
-           (filer_id, report_id, transaction_code, e_year, transaction_id, @var1, @var2, contrib_code,contrib_type_code,corp_name,first_name,mid_init,last_name,address,city,state,zip,check_number,@var3,amount1,amount2,description,other_recpt_code,purpose_code1,purpose_code2,explanation,transfer_type,bank_loan_check_box,crerec_uid, @var4 )
+           (filer_id, report_id, transaction_code, e_year, transaction_id, @var1, @var2, contrib_code, contrib_type_code, corp_name, first_name, mid_init, last_name, address, city, state, zip, check_number, @var3, amount1, amount2, description, other_recpt_code, purpose_code1, purpose_code2, explanation, transfer_type, bank_loan_check_box, crerec_uid, @var4)
            SET created_at = CURRENT_TIMESTAMP,
                updated_at = CURRENT_TIMESTAMP,
                schedule_transaction_date = STR_TO_DATE(@var1, '%m/%d/%Y'),
@@ -55,9 +55,8 @@ module NYSCampaignFinance
                check_date = STR_TO_DATE(@var3, '%m/%d/%Y'),
                crerec_date = STR_TO_DATE(@var4, '%m/%d/%Y %T')"
 
-    puts sql
-    # ActiveRecord::Base.connection.execute(sql)
+    puts "executing sql: \n\n#{sql}\n\n"
+    ActiveRecord::Base.connection.execute(sql)
   end
-
   
 end
