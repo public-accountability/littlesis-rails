fd1ce94fd87c96798bae0a7e2e90b19b447213b1
ziggy <ziggy@littlesis.org>
2017-05-08 11:30:06 -0400

    create Page model & migration

---
+++ app/models/page.rb
@@ -0,0 +1,3 @@
+class Page < ActiveRecord::Base
+  include EditablePage
+end
---
+++ spec/models/page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Page, type: :model do
+  describe 'db columns' do
+    [:name, :title, :markdown, :last_user_id].each do |col|
+      it { should have_db_column(col) }
+    end
+  end
+
+  it { should belong_to(:last_user) }
+
+  it 'responds to pagify_name' do
+    expect(Page.respond_to?(:pagify_name)).to be true
+  end
+end
