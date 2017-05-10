4a2f8e662a347540b1b79937d11e3d25fb4c6fdc
ziggy <ziggy@littlesis.org>
2017-05-10 12:12:41 -0400

    add validations to NyDisclosure

--- spec/factories/ny_disclosures.rb
+++ spec/factories/ny_disclosures.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :ny_disclosure, class: NyDisclosure do
     filer_id 'A1'
+    report_id "A"
     transaction_code 'A'
     e_year '2014'
     transaction_id '123'
+    schedule_transaction_date '1999-01-14'
   end
 
   factory :ny_disclosure_for_import_test, class: NyDisclosure do
