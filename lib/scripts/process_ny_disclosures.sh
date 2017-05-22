cf5c7ab46c9bc6eb20fd0a247b9cb2619362cb74
ziggy <ziggy@littlesis.org>
2017-05-22 15:05:58 -0400

    improved nys cleaning script
    + encode in ruby replace with invalid chars with blank str
    + use sed to convert quote char to '|'

--- lib/scripts/process_ny_disclosures.sh
+++ lib/scripts/process_ny_disclosures.sh
@@ -2,8 +2,13 @@
 
 set -eu
 
-cat ALL_REPORTS.out  |
-    iconv -f ISO-8859-1 -t UTF-8 |
-    sed 's/\"\"/\\N/g' |
+cat ALL_REPORTS.out |
+    dos2unix |
+    sed 's/|/\//g' |
+    sed 's/,\"/,|/g' |
+    sed 's/\",/|,/g' |
+    sed 's/^\"/|/g' |
+    sed 's/\"$/|/g' |
+    sed 's/||/\\N/g' |
     sed 's/\x0//g' |
-    ./clean_ny_disclosures 1> good_disclosures.csv 2> bad_rows.csv
+    ruby clean_ny_disclosures 1> good_disclosures.csv 2> bad_rows.csv
