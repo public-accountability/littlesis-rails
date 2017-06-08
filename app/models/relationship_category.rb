bbf50458d72d927a7ff0ea121c06d894fe828ea6
ziggy <ziggy@littlesis.org>
2017-06-08 17:55:34 -0400

    valid categories lists from RelationshipCategory

--- app/models/relationship_category.rb
+++ app/models/relationship_category.rb
@@ -2,4 +2,39 @@ class RelationshipCategory < ActiveRecord::Base
   include SingularTable
 
   has_many :relationships, inverse_of: :category
+
+  def self.valid_categories
+    person_to_person = (1..12).to_a
+    person_to_org = (1..12).to_a
+    org_to_person = (1..12).to_a
+    org_to_org = (1..12).to_a
+    all.each do |cat|
+      if cat.entity1_requirements == 'Person'
+        org_to_person.delete(cat.id)
+        org_to_org.delete(cat.id)
+      end
+
+      if cat.entity1_requirements == 'Org'
+        person_to_org.delete(cat.id)
+        person_to_person.delete(cat.id)
+      end
+
+      if cat.entity2_requirements == 'Person'
+        org_to_org.delete(cat.id)
+        person_to_org.delete(cat.id)
+      end
+
+      if cat.entity2_requirements == 'Org'
+        org_to_person.delete(cat.id)
+        person_to_person.delete(cat.id)
+      end
+    end
+
+    {
+      person_to_person: person_to_person,
+      person_to_org: person_to_org,
+      org_to_person: org_to_person,
+      org_to_org: org_to_org
+    }
+  end
 end
