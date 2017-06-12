f837720cc42a9903cb35ec9673aec4a617d5c60b
ziggy <ziggy@littlesis.org>
2017-06-12 17:20:07 -0400

    validate person to person relationships

--- app/validators/relationship_validator.rb
+++ app/validators/relationship_validator.rb
@@ -1,9 +1,26 @@
 class RelationshipValidator < ActiveModel::Validator
   VALID_CATEGORIES = RelationshipCategory.valid_categories.freeze
+  PERSON_TO_PERSON = VALID_CATEGORIES[:person_to_person]
+  PERSON_TO_ORG = VALID_CATEGORIES[:person_to_org]
+  ORG_TO_ORG = VALID_CATEGORIES[:org_to_org]
+  ORG_TO_PERSON = VALID_CATEGORIES[:org_to_person]
   
-  def validate(record)
+  def validate(rel)
+    return unless rel.entity1_id.present? && rel.entity2_id.present? && rel.category_id.present?
+
+    if rel.entity.person? && rel.related.person?
+      unless RelationshipValidator::PERSON_TO_PERSON.include?(rel.category_id)
+        rel.errors[:category] << error_msg(rel.category_id, 'Person to Person')
+      end
+    end
+
+    
   end
 
+  private
+  
+  def error_msg(cat_id, rel_direction)
+    "#{Relationship.all_categories[cat_id]} is a not a valid category for #{rel_direction} relationships"
+  end
   
 end
-
--- spec/validators/relationship_validator_spec.rb
+++ spec/validators/relationship_validator_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 describe 'RelationshipValidator' do
   class RelTester <
     include ActiveModel::Validations
+    attr_accessor :entity1_id
+    attr_accessor :entity2_id
+    attr_accessor :category_id
     validates_with RelationshipValidator
+
+    def entity; end
+
+    def related; end
   end
 
-  describe 'VALID_CATEGORIES' do
+  describe 'Constants' do
     it 'has VALID_CATEGORIES constant' do
       expect(RelationshipValidator::VALID_CATEGORIES).to be_a Hash
       expect(RelationshipValidator::VALID_CATEGORIES).to have_key :person_to_person
@@ -14,5 +21,37 @@ describe 'RelationshipValidator' do
       expect(RelationshipValidator::VALID_CATEGORIES).to have_key :org_to_org
       expect(RelationshipValidator::VALID_CATEGORIES).to have_key :org_to_person
     end
+
+    it 'has other constants' do
+      expect(RelationshipValidator::PERSON_TO_PERSON).to be_a Array
+      expect(RelationshipValidator::PERSON_TO_ORG).to be_a Array
+      expect(RelationshipValidator::ORG_TO_ORG).to be_a Array
+      expect(RelationshipValidator::ORG_TO_PERSON).to be_a Array
+    end
   end
+
+  
+  describe 'invalid relationships' do
+    describe 'person to person set to Ownership' do
+      before do
+        @rel = RelTester.new
+        @rel.entity1_id = 1
+        @rel.entity2_id = 2
+        @rel.category_id = 10
+        expect(@rel).to receive(:entity).and_return(double(:person? => true))
+        expect(@rel).to receive(:related).and_return(double(:person? => true))
+      end
+      
+      it 'sets relationship to be invalid' do
+        expect(@rel.valid?).to eql false
+      end
+
+      it 'sets error message' do
+        @rel.valid?
+        expect(@rel.errors[:category]).to eq ["Ownership is a not a valid category for Person to Person relationships"]
+      end
+      
+    end
+  end
+  
 end
