3b0c420e3f1c65460c95f466f8ef6580e00280fa
Liz Starin <liz.starin@gmail.com>
2017-07-06 16:05:27 -0400

    adds short description field to lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20170706142752) do
 
   create_table "address", force: :cascade do |t|
