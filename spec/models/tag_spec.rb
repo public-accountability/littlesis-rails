665728a5532348b0522430eabe5aaeb0a5ad891c
ziggy <ziggy@elephant-bird.net>
2017-08-02 18:20:40 -0400

    [#245] simplify tag lookup
    * on loading tag yaml, parse hash table that maps from *both* tag id
      and tag name to tag, keep reference to it as constant
    * payoff: simpler signature & constant time lookup

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,34 +1,69 @@
 require 'rails_helper'
 
 describe Tag do
-  let(:tags) do
-    [
-      {
-        'name' => 'oil',
-        'description' => 'the reason for our planet\'s demise',
-        'id' => 1
-      },
-      {
-        'name' => 'nyc',
-        'description' => 'anything related to New York City',
-        'id' => 2
-      }
-    ]
+  let(:oil) do
+    {
+      'name' => 'oil',
+      'description' => 'the reason for our planet\'s demise',
+      'id' => 1
+    }
   end
+
+  let(:nyc) do
+    {
+      'name' => 'nyc',
+      'description' => 'anything related to New York City',
+      'id' => 2
+    }
+  end
+
+  let(:tags) { [oil, nyc] }
   let(:invalid_tags) { tags.dup.tap { |t| t[1]['id'] = 1 } }
 
   it "returns all tags" do
     expect(Tag.all).to eql tags
   end
 
-  it "throws an argument error if ids not unique" do
-    expect(YAML).to receive(:load).and_return(invalid_tags)
+  describe 'invalid initialization' do
+    before(:all) do
+      Object.send(:remove_const, :Tag) # unload Tag class
+    end
 
-    Object.send(:remove_const, :Tag) # unload Tag class
-    expect {
-      load Rails.root.join('app', 'models', 'tag.rb') # reload tag class
-    }.to raise_error(ArgumentError)
-    
+    after(:all) do
+      Object.send(:remove_const, :Tag) # unload Tag class
+      load Rails.root.join('app', 'models', 'tag.rb')
+    end
+
+    it "throws an argument error if ids not unique" do
+      expect(YAML).to receive(:load).and_return(invalid_tags)
+      expect {
+        load Rails.root.join('app', 'models', 'tag.rb') # reload tag class
+      }.to raise_error(ArgumentError)
+    end
   end
-  
+
+  it 'generates a LOOKUP constant hash' do
+    expect(Tag::LOOKUP)
+      .to eq({
+               1 => oil,
+               'oil' => oil,
+               2 => nyc,
+               'nyc' => nyc
+             })
+  end
+
+  it 'finds tags by id' do
+    expect(Tag.find(1)).to eq oil
+    expect(Tag.find(2)).to eq nyc
+  end
+
+  it 'finds tags by name' do
+    expect(Tag.find('oil')).to eq oil
+    expect(Tag.find('nyc')).to eq nyc
+  end
+
+  it 'handles non-existent ids or names' do
+    expect(Tag.find('foobar')).to be nil
+    expect(Tag.find(Integer::MAX_64BIT)).to be nil
+  end
 end
