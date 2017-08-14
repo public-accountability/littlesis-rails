bb334a120772c306bfb2c1c2ee9dd6a07efb61ef
aguestuser <aguestuser@riseup.net>
2017-08-14 14:18:12 -0400

    [#245] validate presence of required tagable fields

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -4,4 +4,7 @@ describe Tagging, type: :model do
   it { should have_db_column(:tag_id) }
   it { should have_db_column(:tagable_class) }
   it { should have_db_column(:tagable_id) }
+  it { should validate_presence_of(:tag_id) }
+  it { should validate_presence_of(:tagable_class) }
+  it { should validate_presence_of(:tagable_id) }
 end
