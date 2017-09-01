public-accountability/littlesis-rails

commit aa066804db1461a4fe48b66d700bb71a88120afc

[ag|#233] fix broken doubles in view specs
* BUG: `current_user` double in tag view specs receiving unexpected `:permissions`
  message (which in turn receives unexpecte `:tag_permissions` message
* CAUSE: these views now call `Tagable#tags_for(user)`,
  which calls `user#permissions` (unlike before), so the (brittle?)
  double was now wrong
* FIX: stub the `current_user` double to have empty values for
  permissions

spec/views/entities/sidebar/_sidebar.html.erb_spec.rb
@@ -36,7 +36,10 @@ describe "partial: sidebar" do
     before do
       allow(view).to receive(:user_signed_in?).and_return(true)
       allow(view).to receive(:current_user)
-                      .and_return(double(:admin? => true, :importer? => true, :merger? => false))
+                      .and_return(double(:admin? =>      true,
+                                         :importer? =>   true,
+                                         :merger? =>     false,
+                                         :permissions => double(:tag_permissions => {})))
       
     end
     context 'entity has tags' do
@@ -58,7 +61,12 @@ describe "partial: sidebar" do
       before do
         assign(:entity, org)
         allow(view).to receive(:user_signed_in?).and_return(true)
-        allow(view).to receive(:current_user).and_return(double(:admin? => false, :importer? => false, :merger? => false))
+        allow(view).to receive(:current_user)
+                        .and_return(double(:admin? =>      false,
+                                           :importer? =>   false,
+                                           :merger? =>     false,
+                                           :permissions => double(:tag_permissions => {})))
+        
         render partial: 'entities/sidebar.html.erb'
       end
 
@@ -83,7 +91,12 @@ describe "partial: sidebar" do
       before do
         assign(:entity, org)
         allow(view).to receive(:user_signed_in?).and_return(true)
-        allow(view).to receive(:current_user).and_return(double(:admin? => false, :importer? => true, :merger? => false))
+        allow(view).to receive(:current_user)
+                        .and_return(double(:admin? =>      false,
+                                           :importer? =>   true,
+                                           :merger? =>     false,
+                                           :permissions => double(:tag_permissions => {})))
+        
         render partial: 'entities/sidebar.html.erb'
       end
 
@@ -96,7 +109,12 @@ describe "partial: sidebar" do
       before do
         assign(:entity, org)
         allow(view).to receive(:user_signed_in?).and_return(true)
-        allow(view).to receive(:current_user).and_return(double(:admin? => false, :importer? => false, :merger? => true))
+        allow(view).to receive(:current_user)
+                        .and_return(double(:admin? =>      false,
+                                           :importer? =>   false,
+                                           :merger? =>     true,
+                                           :permissions => double(:tag_permissions => {})))
+        
         render partial: 'entities/sidebar.html.erb'
       end
 
@@ -125,7 +143,12 @@ describe "partial: sidebar" do
       before do
         assign(:entity, org)
         allow(view).to receive(:user_signed_in?).and_return(true)
-        allow(view).to receive(:current_user).and_return(double(:admin? => false, :importer? => false, :merger? => false))
+        allow(view).to receive(:current_user)
+                        .and_return(double(:admin? =>      false,
+                                           :importer? =>   false,
+                                           :merger? =>     false,
+                                           :permissions => double(:tag_permissions => {})))
+
         render partial: 'entities/sidebar.html.erb'
       end
 

spec/views/lists/edit.html.erb_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'lists/edit.html.erb', type: :view do
   before(:each) do
     allow(controller).to receive(:current_user)
-                           .and_return(double(:admin? => false))
+                          .and_return(double(:admin? => false,
+                                             :permissions => double(:tag_permissions => {})))
 
     assign(:list, build(:list))
     render

spec/views/relationships/show.html.erb_spec.rb
@@ -85,7 +85,9 @@ describe 'relationships/show.html.erb', type: :view do
       let(:user_signed_in) { true }
 
       before(:each) do
-        allow(view).to receive(:current_user).and_return(double(has_legacy_permission: true))
+        allow(view).to receive(:current_user)
+                        .and_return(double(has_legacy_permission: true,
+                                           permissions: double(tag_permissions: {})))
         render
       end
       context 'relationship has tags' do
@@ -121,7 +123,9 @@ describe 'relationships/show.html.erb', type: :view do
     before do
       assign(:relationship, @rel)
       allow(view).to receive(:current_user)
-                       .and_return(double('user', has_legacy_permission: true))
+                      .and_return(double('user',
+                                         has_legacy_permission: true,
+                                         permissions: double(tag_permissions: {})))
       allow(view).to receive(:user_signed_in?).and_return(true)
       render
     end

commit 6e13846a02b2c09e0a1876d22195cb2a6f2a6fd3

[ag|#233] cleanup: missed broken double in aa06680 -- whoops!

spec/views/entities/sidebar/_sidebar.html.erb_spec.rb
@@ -129,7 +129,12 @@ describe "partial: sidebar" do
       before do
         assign(:entity, org)
         allow(view).to receive(:user_signed_in?).and_return(true)
-        allow(view).to receive(:current_user).and_return(double(:admin? => true, :importer? => false, :merger? => false))
+        allow(view).to receive(:current_user)
+                        .and_return(double(:admin? =>      true,
+                                           :importer? =>   false,
+                                           :merger? =>     false,
+                                           :permissions => double(:tag_permissions => {})))
+        
         render partial: 'entities/sidebar.html.erb'
       end
 