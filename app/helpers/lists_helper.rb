b481fccc248253df52e055882f26aecf1a08c6c0
ziggy <ziggy@elephant-bird.net>
2017-09-07 18:47:48 -0400

    [#235] Tags always appear on right on list page

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -4,15 +4,8 @@ module ListsHelper
     link_to(name, members_list_path(list))
   end
 
-  def tags_on_right?(tags)
-    tags.count > 2
-  end
-
-  # For lists with less than 2 tags
-  # they will appear appended to the list name.
-  # Otherwise, they go in their own column on the right.
   def list_name_class(tags)
-    tags_on_right?(tags) ? "col-sm-8 col-md-9" : "col-sm-12"
+    tags.count.positive? ? "col-sm-8 col-md-9" : "col-sm-12"
   end
 
   def network_link(list)
