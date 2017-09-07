b867cf0388c490407d6ab18674b664d604437067
ziggy <ziggy@elephant-bird.net>
2017-09-07 17:42:38 -0400

    [#297] add search methods for Tag
    search_by_names looks in a phrase to see if it contains any tag names

---
+++ spec/factories/tag.rb
@@ -0,0 +1,29 @@
+FactoryGirl.define do
+
+  factory :tag, class: Tag  do
+    sequence(:id)
+    sequence(:name) { |n| "tag-name#{n}"}
+    sequence(:description) { |n| "description of tag-name#{n}"}
+  end
+
+  factory :oil_tag, class: Tag do
+    name 'oil'
+    description "the reason for our planet's demise"
+  end
+
+  factory :nyc_tag, class: Tag do
+    name 'nyc'
+    description "anything related to New York City"
+    restricted true
+  end
+
+  factory :finance_tag, class: Tag do
+    name 'finance'
+    description 'banks and such'
+  end
+
+  factory :real_estate_tag, class: Tag do
+    name 'real-estate'
+    description 'The real estate industry'
+  end
+end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,33 +1,105 @@
 require 'rails_helper'
 
 describe Tag, :tag_helper do
-  seed_tags
 
   it { should have_db_column(:restricted) }
   it { should have_db_column(:name) }
   it { should have_db_column(:description) }
   it { should have_many(:taggings) }
 
-  describe 'validations' do
-    subject { Tag.new(name: 'fake tag name', description: 'all about fake tags') }
+  context 'with seed tags' do
+    seed_tags
 
-    it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
+    describe 'validations' do
+      subject { Tag.new(name: 'fake tag name', description: 'all about fake tags') }
+
+      it { should validate_uniqueness_of(:name) }
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:description) }
+    end
+
+    it 'can determine if a tag is restricted' do
+      expect(Tag.find_by_name('oil').restricted?).to be false
+      expect(Tag.find_by_name('nyc').restricted?).to be true
+    end
+
+    it 'partitions tag ids from client into hash of update actions to be taken' do
+      client_ids = [1, 2, 3].to_set
+      server_ids = [2, 3, 4].to_set
+      expect(Tag.parse_update_actions(client_ids, server_ids)).to eql(
+                                                                    add: [1].to_set,
+                                                                    remove: [4].to_set,
+                                                                    ignore: [2, 3].to_set
+                                                                  )
+    end
   end
 
-  it 'can determine if a tag is restricted' do
-    expect(Tag.find_by_name('oil').restricted?).to be false
-    expect(Tag.find_by_name('nyc').restricted?).to be true
-  end
+  describe 'Class Methods' do
+    before(:each) do
+      @oil = build(:oil_tag)
+      @nyc = build(:nyc_tag)
+      @finance = build(:finance_tag)
+      @real_estate = build(:real_estate_tag)
+      Tag.instance_variable_set(:@lookup, nil)
+      allow(Tag).to receive(:all).and_return([@oil, @nyc, @finance, @real_estate])
+    end
 
-  it 'partitions tag ids from client into hash of update actions to be taken' do
-    client_ids = [1, 2, 3].to_set
-    server_ids = [2, 3, 4].to_set
-    expect(Tag.parse_update_actions(client_ids, server_ids)).to eql(
-      add: [1].to_set,
-      remove: [4].to_set,
-      ignore: [2, 3].to_set
-    )
+    describe '#search_by_name' do
+      it 'finds tag by if search includes exact name' do
+        expect(Tag.search_by_name('oil')).to eql @oil
+        expect(Tag.search_by_name('nyc')).to eql @nyc
+      end
+
+      it 'finds tag regardless of capitalization' do
+        expect(Tag.search_by_name('OIL')).to eql @oil
+        expect(Tag.search_by_name('nYc')).to eql @nyc
+      end
+
+      it 'return nil if there is no tag' do
+        expect(Tag.search_by_name('NOTATAG')).to be nil
+      end
+    end
+
+    describe '#search_by_names' do
+      let(:phrase) { '' }
+      subject { Tag.search_by_names(phrase) }
+
+      it { is_expected.to be_a Array }
+
+      context 'phrase contains one tag' do
+        let(:phrase) { "oil barons" }
+        it { should eql [@oil] }
+      end
+
+      context 'phrase contains two tag' do
+        let(:phrase) { "oil barons who like finance" }
+        it { should eql [@oil, @finance] }
+      end
+
+      context 'phrase contains a repeated tag name' do
+        let(:phrase) { "nyc nyc" }
+        it { should eql [@nyc] }
+      end
+
+      context 'phrase contains real estate' do
+        let(:phrase) { "my rent is too high. DAMN REAL ESTATE INDUSTRY" }
+        it { should eql [@real_estate] }
+      end
+
+      context 'phrase is unrelated to tags' do
+        let(:phrase) { "nothing to see here" }
+        it { should eql [] }
+      end
+    end
+
+    describe 'lookup' do
+      it 'returns a look up table of all hash by name' do
+        expect(Tag.lookup).to eq('oil' => @oil,
+                                 'nyc' => @nyc,
+                                 'finance' => @finance,
+                                 'real estate' => @real_estate)
+      end
+
+    end
   end
 end
