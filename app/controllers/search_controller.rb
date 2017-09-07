58555901e352ba4af98faec090c48fdf1066d00a
ziggy <ziggy@elephant-bird.net>
2017-09-07 14:04:35 -0400

    Refactor SearchController#Basic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,52 +1,28 @@
 class SearchController < ApplicationController
   before_filter :authenticate_user!, except: [:basic]
+  before_action :set_page, only: [:basic]
 
   def basic
     @q = (params[:q] or "").gsub(/\b(and|the|of)\b/, "")
     @cant_find = params[:q].present? && @q.empty?
 
-    if @q.present?      
-      q = Riddle::Query.escape(@q)
-      page = (params[:page] or 1).to_i
-
-      @entities = Entity::Search.search(@q, page: page)
-
-      if page > 1
-        @groups = []
-        @lists = []
-        @maps = []
+    if @q.present?
+      if @page > 1
+        # only show entities
+        set_groups_lists_maps_to_be_empty
+        entities_search(@q)
       else
-        admin = current_user.present? and current_user.has_legacy_permission("admin")
-        list_is_admin = admin ? [0, 1] : 0
-        @groups = Group.search("@(name,tagline,description,slug) #{q}", per: 50, match_mode: :extended)
-        @lists = List.search("@(name,description) #{q}",
-                             per: 50,
-                             match_mode: :extended,
-                             with: { is_deleted: false, is_admin: list_is_admin, is_network: false },
-                             without: { access: Permissions::ACCESS_PRIVATE } )
-        @maps = NetworkMap.search("@(title,description,index_data) #{q}", per: 100, match_mode: :extended, with: { is_deleted: false, is_private: false })
+        perform_search(@q)
       end
     end
 
     respond_to do |format|
-      format.html {    
-        render "basic"
-      }
+      format.html { render "basic" }
 
-      format.json {
-        entities = @entities.map do |e|
-          {
-            id: e.id,
-            name: e.name,
-            description: e.blurb,
-            summary: e.summary,
-            primary_type: e.primary_ext,
-            url: e.legacy_url
-          }
-        end
-
+      format.json do
+        entities = @entities.map { |e| Entity::Search.entity_with_summary(e) }
         render json: { entities: entities }
-      }
+      end
     end
   end
 
@@ -64,5 +40,51 @@ class SearchController < ApplicationController
     else
       render json: search_results.map { |e| Entity::Search.entity_with_summary(e) }
     end
+  end
+
+  private
+
+  def set_groups_lists_maps_to_be_empty
+    @groups = []
+    @lists = []
+    @maps = []
+  end
+
+  def perform_search(query)
+    q = ThinkingSphinx::Query.escape(query)
+    entities_search(query)
+    groups_search(q)
+    lists_search(q)
+    maps_search(q)
+  end
+
+  # unlike groups, lists, and maps, Entity::Search takes
+  # the "raw" query before it has been escaped.
+  def entities_search(query)
+    @entities = Entity::Search.search(query, page: @page)
+  end
+
+  def groups_search(q)
+    @groups = Group.search("@(name,tagline,description,slug) #{q}", per: 50, match_mode: :extended)
+  end
+
+  def lists_search(q)
+    list_is_admin = current_user&.admin? ? [0, 1] : 0
+    @lists = List.search("@(name,description) #{q}",
+                         per: 50,
+                         match_mode: :extended,
+                         with: { is_deleted: false, is_admin: list_is_admin, is_network: false },
+                         without: { access: Permissions::ACCESS_PRIVATE })
+  end
+
+  def maps_search(q)
+    @maps = NetworkMap.search("@(title,description,index_data) #{q}",
+                              per: 100,
+                              match_mode: :extended,
+                              with: { is_deleted: false, is_private: false })
+  end
+
+  def set_page
+    @page = (params[:page] or 1).to_i
   end
 end
