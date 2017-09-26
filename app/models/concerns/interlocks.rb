34af43b5be417ba03bf50e501db9988487336816
ziggy <ziggy@littlesis.org>
2017-09-26 11:57:28 -0400

    [#333] refactor Interlocks#interlocks
    * extract helper methods that begin to sketch pattern of
      connected/connecting abstraction for other interlocks
    * create fake type `ConnectedIdsHash`, abstract `person_ids`,
      `org_ids`
      to `connected_ids`, `connecting_ids`, respectively
    * use `#lookup_table_for` AR monkeypatch

--- app/models/concerns/interlocks.rb
+++ app/models/concerns/interlocks.rb
@@ -2,39 +2,49 @@ module Interlocks
   # extend ActiveSupport::Concern
   def interlocks
     # TODO: create position_scope for Link
-    org_ids = links
-                .where(entity1_id: id,
-                       category_id: Relationship::POSITION_CATEGORY,
-                       is_reverse: false)
-                .pluck(:entity2_id)
+    org_ids = connecting_ids(Relationship::POSITION_CATEGORY)
 
-    # [{
-    #   person_id => Integer
-    #   org_ids => [Integer],
-    # }]
-    # TODO: extract this and give it more descriptive name for future re-use
-    id_hashes = Link
-            .where(entity2_id: org_ids,
-                   category_id: Relationship::POSITION_CATEGORY,
-                   is_reverse: false)
-            .to_a
-            .group_by(&:entity1_id)
-            .tap { |grouped_ids| grouped_ids.delete(id) }
-            .map { |person_id, links| { person_id: person_id,
-                                        org_ids: links.map(&:entity2_id).uniq } }
-            .sort { |a, b| b[:org_ids].count <=> a[:org_ids].count }
+    people_and_org_ids = connected_and_connecting_ids(org_ids, Relationship::POSITION_CATEGORY)
 
-    entity_ids = id_hashes.map { |x| [x[:person_id], x[:org_ids]] }.flatten
+    entities_by_id = Entity.lookup_table_for(collapse(people_and_org_ids))
 
-    entities_by_id = Entity.find(entity_ids).reduce({}) do |acc, entity|
-      acc.merge(entity.id => entity)
-    end
-
-    id_hashes.map do |id_hash|
+    people_and_org_ids.map do |connected_id_hash|
       {
-        "person" => entities_by_id.fetch(id_hash[:person_id]),
-        "orgs" => id_hash[:org_ids].map { |org_id| entities_by_id.fetch(org_id) }
+        "person" => entities_by_id.fetch(connected_id_hash[:connected_id]),
+        "orgs" => connected_id_hash[:connecting_ids].map { |id| entities_by_id.fetch(id) }
       }
     end
+  end
+
+  private
+
+  def connecting_ids(rel_category_id)
+    links
+      .where(entity1_id: id,
+             category_id: rel_category_id,
+             is_reverse: false)
+      .pluck(:entity2_id)
+  end
+
+  # type ConnectedIdHash = { connected_id   => Integer,
+  #                          connecting_ids => [Integer] }
+  # ---
+  # [Integer], Integer -> ConnectedIdHash
+  def connected_and_connecting_ids(connecting_ids, rel_category_id)
+    Link
+      .where(entity2_id: connecting_ids,
+             category_id: rel_category_id,
+             is_reverse: false)
+      .to_a
+      .group_by(&:entity1_id)
+      .tap { |grouped_ids| grouped_ids.delete(id) }
+      .map { |connected_id, links| { connected_id: connected_id,
+                                     connecting_ids: links.map(&:entity2_id).uniq } }
+      .sort { |a, b| b[:connecting_ids].count <=> a[:connecting_ids].count }
+  end
+
+  # Array(ConnectedIdHash) => [Integer]
+  def collapse(connected_id_hashes)
+    connected_id_hashes.map { |x| [x[:connected_id], x[:connecting_ids]] }.flatten.uniq
   end
 end
