98adab1e8fbdd0457a5606fb15a9c1f2951d0910
aguestuser <aguestuser@riseup.net>
2017-10-04 12:09:25 -0400

    [#335|ag] extract `connection_type` param in `#interlocks` helpers
    * in anticipation of wanting to pass `:giving` as param to
      `#connected_id_hashes` and `#connecting_ids`, extract
      `connection_type` param to both functions and pass it `:interlocks`,
      while ensuring that current implementation still passes specs
    * all helpers pass these params along to
      `#relationship_categories_for`
    * note: the fact that we keep threading `connection_type` as the first
      argument to so many functions suggests extracting an class with a
      @connection_type attribute might be a good look... holding off for now

--- app/models/concerns/network_analysis.rb
+++ app/models/concerns/network_analysis.rb
@@ -1,7 +1,7 @@
 module NetworkAnalysis
-  # extend ActiveSupport::Concern
+
   def interlocks(page = 1)
-    id_hashes = paginate(page, Entity::PER_PAGE, connected_id_hashes)
+    id_hashes = paginate(page, Entity::PER_PAGE, connected_id_hashes_for(:interlocks))
     entities_by_id = Entity.lookup_table_for(collapse(id_hashes))
     id_hashes.map do |id_hash|
       {
@@ -13,21 +13,16 @@ module NetworkAnalysis
 
   private
 
-  def connecting_ids
-    relationships
-      .where(root_entity_id_key => id, category_id: rel_category_ids)
-      .pluck(connecting_entity_id_key)
-  end
-
   # TODO (ag|Tue 03 Oct 2017): extract object for this?
   # ---
   # type ConnectedIdHash = { connected_id   => Integer,
   #                          connecting_ids => [Integer] }
   # ---
   # [Integer], Integer -> [ConnectedIdHash]
-  def connected_id_hashes
+  def connected_id_hashes_for(connection_type)
     Relationship
-      .where(connecting_entity_id_key => connecting_ids, category_id: rel_category_ids)
+      .where(connecting_entity_id_key => connecting_ids_for(connection_type),
+             :category_id             => relationship_categories_for(connection_type))
       .to_a
       .group_by(&root_entity_id_key)
       .tap { |grouped_ids| grouped_ids.delete(id) } # filter out root id
@@ -35,23 +30,28 @@ module NetworkAnalysis
       .sort { |a, b| b[:connecting_ids].count <=> a[:connecting_ids].count }
   end
 
+  def connecting_ids_for(connection_type)
+    relationships
+      .where(root_entity_id_key => id, category_id: relationship_categories_for(connection_type))
+      .pluck(connecting_entity_id_key)
+  end
+
   def connected_id_hash_for(connected_id, rels)
     {
       connected_id:    connected_id,
-      connecting_ids:  connecting_ids_for(rels)
+      connecting_ids:  connecting_ids_subset_for(rels)
     }
   end
 
-  def connecting_ids_for(relationships)
+  def connecting_ids_subset_for(relationships)
     relationships.map(&connecting_entity_id_key).uniq
   end
 
-  def rel_category_ids
-    # TODO (ag|03-Oct-2017):
-    # * eventually we would like to include OWNERSHIP_CATEGORY for people
-    # * as symphony does not do this and it contradicts the user-facing text
-    #   in `EntitiesHelper#entity_interlocks_header_for`, leave this unimplemented for now
-    [Relationship::POSITION_CATEGORY]
+  def relationship_categories_for(connection_type)
+    # TODO (ag|03-Oct-2017): include OWNERSHIP_CATEGORY for people interlocks?
+    {
+      interlocks: [Relationship::POSITION_CATEGORY],
+    }.fetch(connection_type)
   end
 
   def root_entity_id_key
