342add266991be915660a0333a39a2b5b9495ee1
aguestuser <aguestuser@riseup.net>
2017-10-06 17:30:05 -0400

    [#336] finalize "hops-centric" refactor of `NetworkAnalysis` module
    * move query params into constantized lookup tables (saves function
      calls and is more declarative)
    * use composite of `query` (method name) and `primary_ext` as keys to
      query param lookup table
    * join `relationships` at top of query to save SQL lookups
    * provide `sort` lambda and use in `employee_donations`

--- app/models/concerns/network_analysis.rb
+++ app/models/concerns/network_analysis.rb
@@ -1,30 +1,73 @@
 module NetworkAnalysis
 
+  # CONSTANTS
+
   NIL_LAMBDA = ->(_) { nil }
 
+  HOPS = {
+    gives_labor_to:  { category_id: Relationship::POSITION_CATEGORY, is_reverse: false },
+    gets_labor_from: { category_id: Relationship::POSITION_CATEGORY, is_reverse: true  },
+    gives_money_to:  { category_id: Relationship::DONATION_CATEGORY, is_reverse: false },
+    gets_money_from: { category_id: Relationship::DONATION_CATEGORY, is_reverse: true  }
+  }.freeze
+
+  SORTS = {
+    by_connecting_id_count: ->(a, b) { b[:connecting_ids].count <=> a[:connecting_ids].count },
+    by_stat: ->(a, b) { b[:stat] <=> a[:stat] }
+  }.freeze
+
+  STATS = {
+    sum_amounts: ->(ls) { ls.reduce(0) { |acc, link| acc + (link.relationship.amount || 0) } },
+  }.freeze
+
+  PARAMS_BY_QUERY = {
+    interlocks_person: {
+      hops: [HOPS[:gives_labor_to], HOPS[:gets_labor_from]],
+      stat: NIL_LAMBDA,
+      sort: SORTS[:by_connecting_id_count]
+    },
+    interlocks_org: {
+      hops: [HOPS[:gets_labor_from], HOPS[:gives_labor_to]],
+      stat: NIL_LAMBDA,
+      sort: SORTS[:by_connecting_id_count]
+    },
+    similar_donors_person: {
+      hops: [HOPS[:gives_money_to], HOPS[:gets_money_from]],
+      stat: NIL_LAMBDA,
+      sort: SORTS[:by_connecting_id_count]
+    },
+    employee_donations_org: {
+      hops: [HOPS[:gets_labor_from], HOPS[:gives_money_to]],
+      stat: STATS[:sum_amounts],
+      sort: SORTS[:by_stat]
+    }
+  }.freeze
+
+  # CLASS METHODS
+
+  def self.query_params_for(query, ext)
+    PARAMS_BY_QUERY["#{query}_#{ext.downcase}".to_sym]
+  end
+
+  # INSTANCE METHODS
+
   def interlocks(page = 1)
-    connected_nodes(hops_for(:interlocks, primary_ext),
-                    get_stat_for(:interlocks, primary_ext),
-                    page)
+    connected_nodes(NetworkAnalysis.query_params_for(:interlocks, primary_ext), page)
   end
 
   def similar_donors(page = 1)
-    connected_nodes(hops_for(:giving, primary_ext),
-                    get_stat_for(:giving, primary_ext),
-                    page)
+    connected_nodes(NetworkAnalysis.query_params_for(:similar_donors, primary_ext), page)
   end
 
   def employee_donations(page = 1)
-    connected_nodes(hops_for(:giving, primary_ext),
-                    get_stat_for(:giving, primary_ext),
-                    page)
+    connected_nodes(NetworkAnalysis.query_params_for(:employee_donations, primary_ext), page)
   end
 
   # type ConnectedIdHash = { connected_id   => Integer,connecting_ids => [Integer] }
   # [Integer], Integer -> [ConnectedIdHash]
   # TODO(ag|Tue 03 Oct 2017): extract object for this?
-  def connected_nodes(hops, get_stat = NIL_LAMBDA, page = 1) 
-    id_hashes = paginate(page, Entity::PER_PAGE, ids_via(hops, get_stat))
+  def connected_nodes(qps, page = 1)
+    id_hashes = paginate(page, Entity::PER_PAGE, ids_via(*qps.values_at(:hops, :stat, :sort)))
     entities_by_id = Entity.lookup_table_for(collapse(id_hashes))
     id_hashes.map do |id_hash|
       {
@@ -35,65 +78,33 @@ module NetworkAnalysis
     end
   end
 
+  # HELPERS
+
   private
 
-  def hops_for(connection_type, ext)
-    case [connection_type, ext]
-    when [:interlocks, "Person"]
-      [{ category_id: Relationship::POSITION_CATEGORY, is_reverse: false  }, # "gives-labor-to"
-       { category_id: Relationship::POSITION_CATEGORY, is_reverse: true   }] # "gets-labor-from"
-    when [:interlocks, "Org"]
-      [{ category_id: Relationship::POSITION_CATEGORY, is_reverse: true   }, # "gets-labor-from"
-       { category_id: Relationship::POSITION_CATEGORY, is_reverse: false  }] # "gives-labor-to"
-    when [:giving, "Person"]
-      [{ category_id: Relationship::DONATION_CATEGORY, is_reverse: false  }, # "gives-money-to"
-       { category_id: Relationship::DONATION_CATEGORY, is_reverse: true   }] # "gets-money-from"
-    when [:giving, "Org"]
-      [{ category_id: Relationship::POSITION_CATEGORY, is_reverse: true   }, # "gets-labor-from"
-       { category_id: Relationship::DONATION_CATEGORY, is_reverse: false  }] # "gives-money-to"
-    end
-  end
-
-  def get_stat_for(connection_type, ext)
-    case [connection_type, ext]
-    when [:giving, "Org"]
-      # TODO(ag|05-Oct-2017): Refactor this logic to avoid tons of SQL queries!
-      ->(ls) { ls.reduce(0) { |acc, link| acc + (link.relationship.amount || 0) } }
-    else
-      NIL_LAMBDA
-    end
-  end
-
-  # TODO(ag|05-Oct-2017): implement this to sort Org Giving tab by stat (summed giving amount)
-  def get_sort_for(connection_type, ext); end
-
-  def ids_via(hops, get_stat)
+  def ids_via(hops, stat, sort)
     Link
+      .joins(:relationship)
       .where(entity1_id:   first_hop_ids(hops.first),
              category_id:  hops.second[:category_id],
              is_reverse:   hops.second[:is_reverse])
       .to_a
       .group_by(&:entity2_id)
       .tap { |grouped_ids| grouped_ids.delete(id) } # filter out root id
-      .map { |connected_id, links_subset| id_hash_for(connected_id, links_subset, get_stat) }
-      .sort { |a, b| b[:connecting_ids].count <=> a[:connecting_ids].count }
-      # TODO(ag|05-Oct-2017): use parameterized sort here (but likely in SQL query...)
+      .map { |connected_id, links_subset| id_hash_for(connected_id, links_subset, stat) }
+      .sort(&sort)
   end
 
   def first_hop_ids(hop)
-    # TODO(ag|05-Oct-2017):
-    # * if we want to extend this to `next_hop_ids`, we could provide `last_hop_id` as param
-    # * then query `Link.where(entity1_id: last_hop_id)
-    # * worth doing that now, or wait (suspect the latter, curious what @aepyornis thinks...)
     links
       .where(category_id: hop[:category_id], is_reverse: hop[:is_reverse])
       .pluck(:entity2_id)
   end
 
-  def id_hash_for(connected_id, links_subset, get_stat)
+  def id_hash_for(connected_id, links_subset, stat)
     { connected_id:   connected_id,
       connecting_ids: links_subset.map(&:entity1_id).uniq,
-      stat:           get_stat.call(links_subset) }
+      stat:           stat.call(links_subset) }
   end
 
   # [ConnectedIdHash] => [Integer]
