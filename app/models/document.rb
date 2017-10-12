4e7c311e227c5cb91649813b98affafa18c2786f
ziggy <ziggy@littlesis.org>
2017-10-12 18:08:49 -0400

    [#361] Document validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,16 +1,20 @@
 class Document < ActiveRecord::Base
   has_many :references
-  validates_presence_of :url, :url_hash
+
+  validates :url, presence: true
+  validates :url_hash, presence: true, uniqueness: true
   validates :name, length: { maximum: 255 }
 
-  # before_create :trim_whitespace, :set_hash
-  
+  before_validation :trim_whitespace, :set_hash
+
   private
 
-  # def trim_whitespace
-  # end
+  def trim_whitespace
+    self.url.strip! unless url.nil?
+    self.name.strip! unless name.nil?
+  end
 
-  # def set_hash
-  # end
-
+  def set_hash
+    self.url_hash = Digest::SHA1.hexdigest(url) unless url.blank?
+  end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 describe Document, type: :model do
-  it { should have_many(:references) }
-  it { should validate_presence_of(:url) }
-  it { should validate_presence_of(:url_hash) }
-  it { should validate_length_of(:name).is_at_most(255) }
+  describe 'validations' do
+    let(:url) { Faker::Internet.unique.url }
+    subject { Document.new(url: url, name: 'a website') }
+
+    it { should have_many(:references) }
+    it { should validate_presence_of(:url) }
+    it { should validate_length_of(:name).is_at_most(255) }
+
+    it 'checks uniqueness of url_hash' do
+      subject.save!
+      new_doc_with_same_url = Document.new(url: url)
+      expect(new_doc_with_same_url.valid?).to be false
+      expect(new_doc_with_same_url.errors[:url_hash]).to eql ['has already been taken']
+    end
+  end
+
+  describe 'before validation callbacks: trims whitespace and creates url hash' do
+    let(:subject) { Document.new(url: '   https://littlesis.org  ', name: '  LittleSis  ') }
+    before { subject.valid? }
+    specify { expect(subject.url).to eql 'https://littlesis.org' }
+    specify { expect(subject.name).to eql 'LittleSis' }
+    specify { expect(subject.url_hash).to eql Digest::SHA1.hexdigest('https://littlesis.org') }
+  end
 end
