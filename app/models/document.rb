462c05489b107cc355ee47c39a912b3a9c430eb6
ziggy <ziggy@littlesis.org>
2017-10-16 10:55:41 -0400

    [#361] Document: ref type display method and constant

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,6 +7,24 @@ class Document < ActiveRecord::Base
 
   before_validation :trim_whitespace, :set_hash
 
+  REF_TYPES = {
+    1 => 'Generic',
+    2 => 'FEC Filing',
+    3 => 'Newspaper',
+    4 => 'Government Document'
+  }.freeze
+
+  def ref_types_display
+    REF_TYPES[ref_type]
+  end
+
+  # Returns the reference types as an array: [ [name, number], ... ]
+  # Removes the FEC filings option
+  # Used by the add reference modal in _reference_new.html.erb
+  def self.ref_type_options
+    REF_TYPES.except(2).map(&:reverse)
+  end
+
   private
 
   def trim_whitespace
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -15,13 +15,30 @@ describe Document, type: :model do
       expect(new_doc_with_same_url.valid?).to be false
       expect(new_doc_with_same_url.errors[:url_hash]).to eql ['has already been taken']
     end
+
+    describe 'before validation callbacks: trims whitespace and creates url hash' do
+      let(:subject) { Document.new(url: '   https://littlesis.org  ', name: '  LittleSis  ') }
+      before { subject.valid? }
+      specify { expect(subject.url).to eql 'https://littlesis.org' }
+      specify { expect(subject.name).to eql 'LittleSis' }
+      specify { expect(subject.url_hash).to eql Digest::SHA1.hexdigest('https://littlesis.org') }
+    end
   end
 
-  describe 'before validation callbacks: trims whitespace and creates url hash' do
-    let(:subject) { Document.new(url: '   https://littlesis.org  ', name: '  LittleSis  ') }
-    before { subject.valid? }
-    specify { expect(subject.url).to eql 'https://littlesis.org' }
-    specify { expect(subject.name).to eql 'LittleSis' }
-    specify { expect(subject.url_hash).to eql Digest::SHA1.hexdigest('https://littlesis.org') }
+  describe 'ref types' do
+    it 'has REF_TYPES constant' do
+      expect(Document::REF_TYPES).to be_a Hash
+    end
+
+    it '#ref_types_display returns display text of the document\'s ref type' do
+      expect(build(:document, ref_type: 1).ref_types_display).to eql 'Generic'
+      expect(build(:document, ref_type: 3).ref_types_display).to eql 'Newspaper'
+      expect(build(:document, ref_type: nil).ref_types_display).to be nil
+    end
+
+    describe 'ref_type_options' do
+      subject { Document.ref_type_options }
+      it { is_expected.to eql [['Generic', 1], ['Newspaper', 3], ['Government Document', 4]] }
+    end
   end
 end
