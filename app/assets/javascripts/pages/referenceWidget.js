94bf0389817bd951b2dc9ec98aaad6e5c1270c0f
ziggy <ziggy@littlesis.org>
2017-10-26 17:53:03 -0400

    [#375] ajax call to retrieve documents
    Widget renders after http request returns

--- app/assets/javascripts/pages/referenceWidget.js
+++ app/assets/javascripts/pages/referenceWidget.js
@@ -8,39 +8,142 @@
     root.ReferenceWidget = factory(root.jQuery, root.utility);
   }
 }(this, function ($, utility) {
+  // CONSTANTS
+  var REFERENCES_PER_PAGE = 25;
+  var REQUEST_URL = 'http://localhost:8080/references/recent';
+    
   /**
-   * ReferenceWideget
+   * Reference Widget
    * Use:  new ReferenceWidget([entity_ids], { options })
-   * @param {Array} entityIds
+   * @param {Array|Integer|String} entityIds
    * @param {Object} userOptions
    */
   function ReferenceWidget(entityIds, userOptions) {
     this.options = mergeOptions(ReferenceWidget.DEFAULT_OPTIONS, userOptions);
-    this._init();
+    this.entityIds = [].concat(entityIds).map(function(n) { return Number(n); });
+    this.documents = null;
+    this.render = this._render.bind(this);
+    this.init();
   }
 
   // STATIC VALUES
   ReferenceWidget.TYPEAHEAD_INPUT_ID = 'ref-widget-typeahead';
   ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR = '#' + ReferenceWidget.TYPEAHEAD_INPUT_ID;
-  ReferenceWidget.DEFAULT_OPTIONS = { "containerDiv": "#reference-widget-container" };
-
-  // TYPEAHEAD COMPONENTS
-  var referenceSearch = new Bloodhound({
-    local: ['cat', 'dog', 'elephant', 'cow'],
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    datumTokenizer: Bloodhound.tokenizers.whitespace
-  });
-
-  var datasource = { name: 'references', source: referenceSearch };
+  ReferenceWidget.DEFAULT_OPTIONS = {
+    "containerDiv": "#reference-widget-container",
+    "afterRender": function() {}
+  };
 
   /**
    * Initalize the ReferenceWidget 
    */
-  ReferenceWidget.prototype._init = function() {
-    $(this.options.containerDiv).append(this._typeaheadInput());
-    $(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR).typeahead(null, datasource);
+  ReferenceWidget.prototype.init = function() {
+    this.getDocs().then(this.render);
   };
   
+  /**
+   * Replaces contents of container with typeahead
+   */
+  ReferenceWidget.prototype._render = function() {
+    $(this.options.containerDiv).html(this._typeaheadInput());
+    $(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)
+      .typeahead(null, this._typeaheadConfig());
+    this.options.afterRender();
+  };
+
+  ///////////////////////////
+  // TYPEAHEAD COMPONENTS ///
+  //////////////////////////
+
+  /**
+   * Prepares Bloodhound search with provided references data
+   * @param {Array[Referencs]} data
+   * @returns {Bloodhound} 
+   */
+  function documentBloodhound(data) {
+    return new Bloodhound({
+      local: data,
+      identify: function(doc) { return doc.id; },
+      queryTokenizer: function(q) {
+	return Bloodhound.tokenizers.whitespace(q.toLowerCase());
+      },
+      datumTokenizer: function(d) {
+	return flatten(
+	  [d.name, d.url]
+	    .map(function(x) {
+	      return Bloodhound.tokenizers.whitespace(x.toLowerCase());
+	    })
+	);
+      }
+    });
+  }
+  
+  /**
+   * Provides a configuartion object for typeahead
+   * Requires `this.documents` to be set
+   * @param {Array} references
+   * @returns {Object} Configuration for typeahead 
+   */
+  ReferenceWidget.prototype._typeaheadConfig = function() {
+    if (!Array.isArray(this.documents)) {
+      throw 'Documents are missing or invalid :(';
+    }
+    
+    return {
+      name: 'references',
+      source: documentBloodhound(this.documents),
+      templates: {
+	empty: '<p>empty</p>',
+	suggestion: function(data) {
+	  return '<p><strong>' + data.name + '</strong></p>';
+	}
+      }
+    };
+  };
+  
+
+  /**
+   * Retrives recent documents via an ajax call to /references/recent
+   * Sets this.documents if successful.
+   * @returns {Promise}
+   */
+  ReferenceWidget.prototype.getDocs = function() {
+    var self = this;
+    var url = this._recentReferencesUrl();
+    
+    return new Promise(function(resolve, reject){
+      $.getJSON(url)
+	.done(function(data) {
+	  self.documents = data;
+	  resolve(data);
+	})
+	.fail(function(){
+	  reject('failed to get references from /references/recent');
+	});
+    });
+  };
+
+
+  /**
+   * Url for request
+   * @returns {String} 
+   */
+  ReferenceWidget.prototype._recentReferencesUrl = function() {
+    var params =  $.param({
+      "entity_ids":  this.entityIds,
+      "per_page": REFERENCES_PER_PAGE,
+      "exclude_type": 'fec'
+    });
+
+    return REQUEST_URL + '?' + params;
+  };
+
+
+
+  ///////////////////
+  // DOM ELEMENTS ///
+  //////////////////
+    
   /**
    * Input with id ref-widget-typeahead
    * @returns {<input>} 
@@ -53,6 +156,8 @@
     });
   };
   
+
+
   ///////////////
   // HELPERS ///
   /////////////
@@ -61,7 +166,12 @@
     return Boolean(provided) ? Object.assign({}, defaultOptions, provided) : defaultOptions;
   };
 
+  function flatten(arrays) {
+    return Array.prototype.concat.apply([], arrays);
+  }
 
-  return ReferenceWidget;
+  return ReferenceWidget;  // Return the constructor
 }));
+
+
 
--- spec/javascripts/pages/referenceWidget.spec.js
+++ spec/javascripts/pages/referenceWidget.spec.js
@@ -2,10 +2,35 @@ describe('referenceWidget', function() {
 
   const elementExists = (selector) => Boolean($(selector).length);
 
-  const testDom = '<div id="test-dom"></div>';
+  const testDom = '<div id="test-dom"><p>test dom</p></div>';
+
+  const ajaxResult = [
+    {
+      "id": 1525634,
+      "name": "Put a Tiger In Your Think Tank",
+      "url": "http://www.motherjones.com/politics/2005/05/put-tiger-your-think-tank",
+      "publication_date": null,
+      "excerpt": null
+    },
+    {
+      "id": 1535850,
+      "name": "CMD Submits Evidence of Exxon Mobil Funding ALEC%u2019s Climate Change Denial to New York Attorney G",
+      "url": "http://www.prwatch.org/news/2015/11/12977/Exxon-ALEC-climate-denial-investigation-new-york",
+      "publication_date": null,
+      "excerpt": null
+    }
+  ];
 
   beforeEach(function(){
     $('body').append(testDom);
+
+    spyOn($, 'getJSON').and.returnValue({
+      "done": function(f){
+	f(ajaxResult);
+      },
+      "fail": function(f) { f(); }
+    });
+    
   });
 
   afterEach(function(){
@@ -17,17 +42,38 @@ describe('referenceWidget', function() {
     it('is an input', function(){
       var rw = new ReferenceWidget([1,2]);
       var input = rw._typeaheadInput()[0];
-      expect(input.id).toEqual('ref-widget-typeahead');
+      expect(input.id).toEqual(ReferenceWidget.TYPEAHEAD_INPUT_ID);
     });
   });
 
   describe('initializing the widget', function(){
-    it('adds the input to the dom', function(){
-      expect(elementExists(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)).toBeFalse();
-      new ReferenceWidget([1,2], { containerDiv: '#test-dom'});
-      expect(elementExists(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)).toBeTrue();
+
+    it('sets the entityIds property', function(){
+      expect(
+	new ReferenceWidget([1,2], { containerDiv: '#test-dom'}).entityIds
+      ).toEqual([1,2]);
+
+      expect(
+	new ReferenceWidget(['1','2'], { containerDiv: '#test-dom'}).entityIds
+      ).toEqual([1,2]);
+
+      expect(
+	new ReferenceWidget('1000', { containerDiv: '#test-dom'}).entityIds
+      ).toEqual([1000]);
     });
 
+
+    it('adds the input to the dom', function(done){
+      expect(elementExists(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)).toBeFalse();
+      
+      new ReferenceWidget([1,2], {
+	containerDiv: '#test-dom',
+	afterRender: function() {
+	  expect(elementExists(ReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)).toBeTrue();
+	  done();
+	}
+      });
+    });
   });
   
   
