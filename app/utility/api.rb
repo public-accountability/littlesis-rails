8920a007b4ca364f5a2e2dbb88ceca09dfae5eea
ziggy <ziggy@littlesis.org>
2017-11-01 14:21:02 -0400

    refactor api model serializer
    * Move to namespace 'Api'
    * specify extra attributes in api.yml

---
+++ app/utility/api.rb
@@ -0,0 +1,7 @@
+module Api
+  META = {
+    copyright: 'LittleSis CC BY-SA 3.0',
+    license: 'https://creativecommons.org/licenses/by-sa/3.0/us/',
+    apiVersion: '2.0-beta'
+  }
+end
--- spec/api/api_serializer_spec.rb
+++ spec/api/api_serializer_spec.rb
@@ -1,57 +1,58 @@
 require 'rails_helper'
 
-describe 'ApiSerializer' do
+describe 'Api::Serializer' do
+
   it 'has MODEL_INFO const with common ignores' do
-    expect(ApiUtils::Serializer::MODEL_INFO['common']).to be_a Hash
-    expect(ApiUtils::Serializer::MODEL_INFO['common']['ignore']).to be_a Array
-    expect(ApiUtils::Serializer::MODEL_INFO['common']['ignore']).to include 'last_user_id'
+    expect(Api::Serializer::MODEL_INFO['common']).to be_a Hash
+    expect(Api::Serializer::MODEL_INFO['common']['ignore']).to be_a Array
+    expect(Api::Serializer::MODEL_INFO['common']['ignore']).to include 'last_user_id'
   end
 
   describe 'Entity' do
     let(:entity) { build(:corp, last_user_id: 123, id: rand(100)) }
+    subject { Api::Serializer.new(entity).attributes }
 
-    it 'removes last_user_id' do
-      expect(ApiUtils::Serializer.new(entity).attributes.key?('last_user_id')).to be false
+    context 'removes fields from attributes' do
+      ['id', 'is_deleted', 'created_at', 'last_user_id', 'merged_id', 'link_count'].each do |x|
+        it { is_expected.not_to have_key x }
+      end
     end
 
-    it 'removes id' do
-      expect(ApiUtils::Serializer.new(entity).attributes.key?('id')).to be false
+    context 'keeps other attributes' do
+      ["name", "blurb", "summary","website", "parent_id",
+       "primary_ext", "updated_at", "start_date", "end_date" ].each do |x|
+        it { is_expected.to have_key x }
+      end
     end
 
-    it 'keeps name' do
-      expect(ApiUtils::Serializer.new(entity).attributes.key?('name')).to be true
-    end
-
-    context 'extra fields with entity' do
+    context 'additional fields' do
       before(:all) do
         DatabaseCleaner.start
-        @corp = create(:entity_org, last_user_id: SfGuardUser.last.id)
+        @corp = create(:entity_org, last_user_id: 1)
         @corp.aliases.create!(name: 'other corp name')
-        @corp.extension_records.create!(definition_id: 5)
+        @corp.add_extension('Business')
       end
-
       after(:all) { DatabaseCleaner.clean }
+      subject { Api::Serializer.new(@corp).attributes }
 
-      it 'has aliases' do
-        expect(ApiUtils::Serializer.new(@corp).attributes.key?('aliases')).to be true
-        expect(ApiUtils::Serializer.new(@corp).attributes['aliases']).to include 'other corp name'
-        expect(ApiUtils::Serializer.new(@corp).attributes['aliases'].length).to eql 2
+      ['aliases', 'types', 'extensions'].each do |x|
+        it { is_expected.to have_key x }
       end
 
-      it 'has types' do
-        expect(ApiUtils::Serializer.new(@corp).attributes.key?('types')).to be true
-        expect(ApiUtils::Serializer.new(@corp).attributes['types']).to include 'Business'
-        expect(ApiUtils::Serializer.new(@corp).attributes['types'].length).to eql 2
+      it { is_expected.to include('types' => ['Organization', 'Business']) }
+      it { is_expected.to include('aliases' => ['org', 'other corp name']) }
+
+      context 'extensions' do
+        subject { Api::Serializer.new(@corp).attributes['extensions'] }
+        it { is_expected.to have_key 'Org' }
+        it { is_expected.to have_key 'Business' }
       end
     end
   end
 
   describe 'ExtensionRecord' do
     let(:er) { build(:extension_record) }
-
-    it 'serializes extension records' do
-      expect(ApiUtils::Serializer.new(er).attributes)
-        .to eql('definition_id' => 2, 'name' => 'Org', 'display_name' => 'Organization')
-    end
+    subject { Api::Serializer.new(er).attributes } 
+    specify { expect(subject).to eql('definition_id' => 2, 'name' => 'Org', 'display_name' => 'Organization') }
   end
 end
