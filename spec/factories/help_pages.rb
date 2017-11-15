c39815723e3b331eb4eae24352b876440b17c5c9
ziggy <ziggy@littlesis.org>
2017-11-15 13:20:23 -0500

    [#184] add markdown editor css to help edit pages
    + replace FactoryGirl with FactoryBot

--- spec/factories/help_pages.rb
+++ spec/factories/help_pages.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :help_page do
     markdown <<~MARKDOWN
                # editing relationships
--- spec/factories/toolkit_pages.rb
+++ spec/factories/toolkit_pages.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :toolkit_page do
     markdown <<~MARKDOWN
                # toolkit page
