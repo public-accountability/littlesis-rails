29449ffef659c5de88e9678deedf38830a3d81f8
ziggy <ziggy@littlesis.org>
2017-11-15 16:42:32 -0500

    [#383] use new donation page in nav bar

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -15,6 +15,7 @@ describe 'home/dashboard', type: :feature do
 
       expect(page).to have_selector 'ul.nav li a', text: current_user.username
       expect(page).to have_selector 'ul.nav li a', text: 'Tags'
+      expect(page).to have_selector 'ul.nav li a', text: 'Donate'
       # verifing that networks have been removed:
       expect(page).not_to have_selector 'ul.nav li a', text: 'United States'
     end
