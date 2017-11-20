60aec9affcb00094197423375d4c9dae9daedfd5
ziggy <ziggy@littlesis.org>
2017-11-20 17:01:40 -0500

    [#381] .extensions is a list of new or to-be-updated extensions

--- spec/utility/entity_merger_spec.rb
+++ spec/utility/entity_merger_spec.rb
@@ -1,16 +1,88 @@
 require 'rails_helper'
 
 describe 'Merging Entities' do
- 
-  # def entity_merge(source:, dest:)
-  # end
+  describe 'initializing' do
+    let(:source) { build(:org) }
+    let(:dest) { build(:org) }
 
-  it 'can only merge entities that have the same primary extension'
+    it 'requires both source and dest entities' do
+      expect { EntityMerger.new(source: source) }.to raise_error(ArgumentError)
+      expect { EntityMerger.new(dest: dest) }.to raise_error(ArgumentError)
+      expect { EntityMerger.new }.to raise_error(ArgumentError)
+      expect { EntityMerger.new(source: source, dest: build(:document)) }.to raise_error(ArgumentError)
+    end
+
+    it 'sets source and dest attributes' do
+      em = EntityMerger.new(source: source, dest: dest)
+      expect(em.source).to eql source
+      expect(em.dest).to eql dest
+    end
+  end
+
+  it 'can only merge entities that have the same primary extension' do
+    expect { EntityMerger.new source: build(:person), dest: build(:org) }.to raise_error(EntityMerger::ExtensionMismatchError)
+  end
+  
+  
   it 'sets the "merged_id" fields of the merged entity to be the id of the merged entity'
   it 'marks the merged entity as deleted'
 
+
   context 'extensions' do
-    it 'adds new extensions to the destination entity'
+    let(:source_person) { create(:entity_person, :with_person_name) }
+    let(:dest_person) { create(:entity_person, :with_person_name) }
+    let(:source_org) { create(:entity_org, :with_org_name) }
+    let(:dest_org) { create(:entity_org, :with_org_name) }
+    subject { EntityMerger.new(source: source_person, dest: dest_person) } 
+
+    context 'With no new extensions on the source' do
+      it '@extensions contains non-new extension' do
+        subject.merge_extensions
+        expect(subject.extensions.length).to eql 1
+        extension = subject.extensions.first
+        expect(extension).to be_a EntityMerger::Extension
+        expect(extension.new).to be false
+        expect(extension.ext_id).to eql 1
+        expect(extension.fields).to eql source_person.person.attributes.except('id', 'entity_id')
+      end
+    end
+
+    context 'when the source has a new extension with fields' do
+      before do
+        source_person.add_extension('PoliticalCandidate') # ext_id = 3
+        subject.merge_extensions
+      end
+
+      it 'has 2 extensions' do
+        expect(subject.extensions.length).to eql 2
+      end
+
+      it 'has new extension' do
+        new_ext = subject.extensions.select { |e| e.new == true }.first
+        expect(new_ext.new).to be true
+        expect(new_ext.ext_id).to eql 3
+        expect(new_ext.fields.keys).to contain_exactly('is_federal', 'is_state', 'is_local', 'pres_fec_id', 'senate_fec_id', 'house_fec_id', 'crp_id') 
+      end
+    end
+
+
+    context 'when the source has a new extension without fields' do
+      subject { EntityMerger.new(source: source_org, dest: dest_org) }
+      before do
+        source_org.add_extension('Philanthropy') # ext_id = 9
+        subject.merge_extensions
+      end
+
+      it { expect(subject.extensions.length).to eql 2 }
+
+      it 'contains new philanthroy extension' do
+        new_ext = subject.extensions.select { |e| e.new == true }.first
+        expect(new_ext.ext_id).to eql 9
+        expect(new_ext.fields).to eql({})
+      end
+    end
+
+    it 'adds new extensions to the source' 
     it 'updates fields on the destination entity if they are nil'
   end
 
