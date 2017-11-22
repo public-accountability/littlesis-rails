e2e2603ef88d91c74d246564e0883e84f6d927f5
ziggy <ziggy@littlesis.org>
2017-11-22 09:59:36 -0500

    linting/test cleanup for ny_filer_entity

--- app/models/ny_filer_entity.rb
+++ app/models/ny_filer_entity.rb
@@ -1,16 +1,16 @@
 class NyFilerEntity < ActiveRecord::Base
   belongs_to :ny_filer
   belongs_to :entity
-  
+
   validates_presence_of :ny_filer_id, :entity_id, :filer_id
 
   after_create :rematch_existing_matches
-  
+
   def rematch_existing_matches
-    NyMatch.joins(:ny_filer_entity).where('ny_filer_entities.id = ?', id).find_each do |ny_match| 
+    NyMatch.joins(:ny_filer_entity).where('ny_filer_entities.id = ?', id).find_each do |ny_match|
       ny_match.rematch
     end
   end
-  handle_asynchronously :rematch_existing_matches
 
+  handle_asynchronously :rematch_existing_matches
 end
