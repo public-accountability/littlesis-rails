5cbba882a1b78fa833a4f8458139dd48860ed0bf
Liz Starin <liz.starin@gmail.com>
2017-11-30 16:49:07 -0500

    only allows signed-in users to edit documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,23 +1,23 @@
 class DocumentsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
 
-	def edit
-		@document = Document.find(params[:id])
-	end
+  def edit
+    @document = Document.find(params[:id])
+  end
 
-	def update
-		@document = Document.find(params[:id])
-	    if @document.update_attributes(document_params)
-	      @document.save!
-	      redirect_to :back
-	    else
-	      render 'edit'
-	    end
-	end
+  def update
+    @document = Document.find(params[:id])
+    if @document.update_attributes(document_params)
+      @document.save!
+      redirect_to :back
+    else
+      render 'edit'
+    end
+  end
 
-	private
+  private
 
-		def document_params
-			params.require(:document).permit(:name, :ref_type, :publication_date, :excerpt)
-		end
-
+  def document_params
+    params.require(:document).permit(:name, :ref_type, :publication_date, :excerpt)
+  end
 end
