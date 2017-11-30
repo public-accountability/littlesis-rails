a342c5dda66bacea20cab2cba5dcfac3c321270e
ziggy <ziggy@littlesis.org>
2017-11-30 16:22:37 -0500

    [#399] add request specs for nys routes

--- app/controllers/nys_controller.rb
+++ app/controllers/nys_controller.rb
@@ -1,5 +1,8 @@
 class NysController < ApplicationController
   before_filter :authenticate_user!
+  before_action -> { check_permission 'importer' }, only: [
+    :match_donations, :unmatch_donations, :potential_contributions, :contributions
+  ]
 
   def index
   end
@@ -9,7 +12,7 @@ class NysController < ApplicationController
 
   def pacs
   end
-  
+
   def create
     check_permission 'importer'
 
@@ -31,30 +34,30 @@ class NysController < ApplicationController
   end
 
   # POST data
-  #  { payload: { 
+  #  { payload: {
   #       disclosure_ids: [int],
   #       donor_id: int }
   #   }
   #
   def match_donations
-    check_permission 'importer'
     match_params[:disclosure_ids].each do |disclosure_id|
       NyMatch.match(disclosure_id, match_params[:donor_id], current_user.id)
     end
     NyDisclosure.update_delta_flag(match_params[:disclosure_ids])
-    donor = Entity.find(match_params[:donor_id])
-    donor.touch
+    Entity.find(match_params[:donor_id]).update(last_user_id: current_user.sf_guard_user_id)
     head :accepted
   end
-  
+
+  # POST data
+  #  { payload: {
+  #       ny_match_ids: [int],
+  #   }
   def unmatch_donations
-    check_permission 'importer'
   end
 
   # search for contributions
   # /nys/potential_contributions?entity=123
   def potential_contributions
-    check_permission 'importer'
     entity = Entity.find(params.require(:entity).to_i)
     render json: NyDisclosure.potential_contributions(entity)
   end
@@ -62,7 +65,6 @@ class NysController < ApplicationController
   # Already matched donations
   # /nys/contributions?entity=123
   def contributions
-    check_permission 'importer'
     render json: NyMatch.where(donor_id: entity_id).map(&:info)
   end
 
@@ -72,7 +74,7 @@ class NysController < ApplicationController
     return :search_pacs if @entity.org?
     return :search_filers if @entity.person?
   end
-  
+
   def filter_filers(filer)
     if filer.is_matched?
       @matched << filer
@@ -94,7 +96,7 @@ class NysController < ApplicationController
   def ny_filer_ids
     params.require(:ids)
   end
-  
+
   def entity_id
     params.require(:entity)
   end
--- spec/support/request_macros.rb
+++ spec/support/request_macros.rb
@@ -3,3 +3,11 @@ module RequestExampleMacros
     JSON.parse(response.body)
   end
 end
+
+module RequestGroupMacros
+  def denies_access
+    it 'denies access' do
+      expect(response).to have_http_status(403)
+    end
+  end
+end
