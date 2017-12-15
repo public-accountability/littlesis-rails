c90c316a3a4e1809fafe539e88ae9bd135435ba5
aguestuser <aguestuser@riseup.net>
2017-12-15 18:16:06 -0500

    [#443] fix broken nav links
    * introduce new `merge` query param into:
      * `merge this entity` link on entity profile pages
      * `search` button on merge search page
      * `merge` buttons on merge search pages
      * note: the latter is not testable as it occurs in JS :(
    
    * side-effect: refine some tests
      * extract `:merge_source_person` factory
      * streamline setup a bit

--- spec/features/merging_spec.rb
+++ spec/features/merging_spec.rb
@@ -2,111 +2,154 @@ require 'rails_helper'
 
 feature 'Merging entities' do
   let(:mode) {}
-  let(:source) { create(:entity_person) }
-  let(:dest) { create(:entity_person) }
-  let(:query) { 'foobar' }
+  let(:source) { create(:merge_source_person) }
+  let(:dest) { create(:entity_person, :with_person_name) }
+  let(:dest_param) { "&dest=#{dest.id}"}
+  let(:query) { "foobar" }
+  let(:query_param) { "&query=#{query}" }
 
-  before(:each) do
-    login_as(user, scope: :user)
-    visit "/tools/merge?mode=#{mode}&source=#{source.id}&dest=#{dest.id}&query=#{query}"
-  end
+  before { login_as(user, scope: :user) }
+  after { logout(:user) }
 
-  after(:each) { logout(:user) }
+  describe "clicking on `Merge this entity` link" do
+    before { visit entity_path source }
 
-  context 'as a non-admin user' do
-    let(:user) { create(:really_basic_user) }
+    context "as a non-admin user" do
+      let(:user) { create(:really_basic_user) }
 
-    context 'searching for merge targets' do
-      let(:mode) { ToolsController::MergeModes::SEARCH }
-
-      it "allows access" do
-        expect(page).to have_http_status 200
+      it "is not possible" do
+        expect(page).not_to have_link("Merge this entity")
       end
     end
 
-    context 'requesting a merge' do
-      let(:mode) { ToolsController::MergeModes::REQUEST }
+    context "as an admin" do
+      let(:user) { create(:admin_user) }
 
-      it "allows access" do
-        expect(page).to have_http_status 200
+      it "navigates to the merge searchn page" do
+        click_link "Merge this entity"
+        successfully_visits_page tools_merge_path(mode: :search, source: source.id)
       end
-    end
-
-    context 'executing a merge' do
-      let(:mode) { ToolsController::MergeModes::EXECUTE }
-      denies_access
-    end
-
-    context 'reviewing a merge' do
-      let(:mode) { ToolsController::MergeModes::REVIEW }
-      denies_access
     end
   end
 
-  context 'as an admin' do
-    let(:user) { create(:admin_user) }
+  describe "using merge pages" do
 
-    context 'searching for merge targets' do
-      let(:mode) { ToolsController::MergeModes::SEARCH }
+    before do
+      allow(Entity::Search).to receive(:similar_entities).and_return([dest])
+      visit tools_merge_path(mode: mode,
+                             source: source.id,
+                             dest: dest.id,
+                             query: query)
+    end
 
-      it 'shows a table of entities with similar names as merge source' do
-        successfully_visits_page '/tools/merge'
-        page_has_selector 'h1', text: "Merge #{source.name} with another entity"
-        page_has_selector 'form[action="/tools/merge"]'
+    context 'as a non-admin user' do
+      let(:user) { create(:really_basic_user) }
+
+      context 'searching for merge targets' do
+        let(:mode) { ToolsController::MergeModes::SEARCH }
+        let(:dest_param) {}
+
+        it "allows access" do
+          expect(page).to have_http_status 200
+        end
+      end
+
+      context 'requesting a merge' do
+        let(:mode) { ToolsController::MergeModes::REQUEST }
+        let(:query_param) {}
+
+        it "allows access" do
+          expect(page).to have_http_status 200
+        end
+      end
+
+      context 'executing a merge' do
+        let(:mode) { ToolsController::MergeModes::EXECUTE }
+        let(:query_param) {}
+
+        denies_access
+      end
+
+      context 'reviewing a merge' do
+        let(:mode) { ToolsController::MergeModes::REVIEW }
+        let(:query_param) {}
+
+        denies_access
       end
     end
 
-    context 'requesting a merge' do
-      let(:mode) { ToolsController::MergeModes::REQUEST }
+    context 'as an admin' do
+      let(:user) { create(:admin_user) }
 
-      it "raises a custom error?"
-    end
+      context 'searching for merge targets' do
+        let(:mode) { ToolsController::MergeModes::SEARCH }
+        let(:query) { source.name }
 
-    context 'executing a merge' do
+        it "has a search bar to search for more matches" do
+          page_has_selector 'form[action="/tools/merge"]'
+          page_has_selector 'input[@value="search"]'
+          page_has_selector "input[@value='#{source.id}']"
+        end
 
-      let(:mode){ ToolsController::MergeModes::EXECUTE }
-      let(:list) { create(:list) }
-      let!(:source) do
-        person = create(:entity_person, :with_person_name)
-        person.add_extension('BusinessPerson')
-        ListEntity.create!(list_id: list.id, entity_id: person.id)
-        Relationship.create!(category_id: 1, entity: person, related: create(:entity_org))
-        person
-      end
-      let!(:dest) { create(:entity_person, :with_person_name) }
+        it 'shows a table of last search matches' do
+          page_has_selector 'h1', text: "Merge #{source.name} with another person"
+        end
 
-      scenario 'viewing the report page' do
-        page_has_selector 'h2', count: 2
-        page_has_selector 'h2', text: "It will delete "#{dest.name} (#{dest.id}) from the database"
-        page_has_selector 'h4', text: "This will make the following changes:"
-        page_has_selector '#merge-report li', count: 4
-        page_has_selector 'li', text: "Transfer 1 relationships"
-        page_has_selector '#merge-entities-form'
-        page_has_selector "input.btn[value='Merge']", count: 1
-        page_has_selector 'a.btn', text: 'Go back'
-        page_has_selector 'a.btn', text: 'Dashboard'
+        it "is impossible to test the contents of the table b/c it is in javascript"
+        # ^-- i wanted this as a sanity check to make sure the row i was trying to click
+        # `merge` in (see below) was actually there (@aguestuser)
+
+        it "is impossible to test clicking `merge` b/c the button is in javascript"
+        # ^-- i broke this by changing the URL at which the merge report page lives
+        # and thus, to which this button should submit, and and wanted to add a test
+        # to catch that breakage, but could not (@aguestuser)
       end
 
-      scenario 'clicking to merge the two entities' do
-        expect(dest.relationships.count).to eql 0
-        expect(dest.lists.count).to eql 0
-        successfully_visits_page "/tools/merge"
+      context 'requesting a merge' do
+        let(:mode) { ToolsController::MergeModes::REQUEST }
+        let(:query_param) {}
 
-        click_button "Merge"
-        successfully_visits_page entity_path(dest)
-
-        expect(dest.reload.relationships.count).to eql 1
-        expect(dest.lists.count).to eql 1
-        expect(source.reload.is_deleted).to be true
-        expect(source.merged_id).to eql dest.id
+        it "raises a custom error?"
       end
-    end
 
-    context 'reviewing a merge' do
-      let(:mode) { ToolsController::MergeModes::REVIEW }
+      context 'executing a merge' do
+        let(:mode){ ToolsController::MergeModes::EXECUTE }
+        let(:query_param) {}
+        let(:list) { create(:list) }
+        let(:source){ create(:merge_source_person) }
 
-      it "allows access" do
-        expect(page).to have_http_status 200
+        scenario 'viewing the report page' do
+          page_has_selector 'h2', count: 2
+          page_has_selector 'h2', text: "It will delete "
+          page_has_selector 'h4', text: "This will make the following changes:"
+          page_has_selector '#merge-report li', count: 4
+          page_has_selector 'li', text: "Transfer 1 relationships"
+          page_has_selector '#merge-entities-form'
+          page_has_selector "input.btn[value='Merge']", count: 1
+          page_has_selector 'a.btn', text: 'Go back'
+          page_has_selector 'a.btn', text: 'Dashboard'
+        end
+
+        scenario 'clicking to merge the two entities' do
+          expect(dest.relationships.count).to eql 0
+          expect(dest.lists.count).to eql 0
+
+          click_button "Merge"
+          successfully_visits_page entity_path(dest)
+
+          expect(dest.reload.relationships.count).to eql 1
+          expect(dest.lists.count).to eql 1
+          expect(source.reload.is_deleted).to be true
+          expect(source.merged_id).to eql dest.id
+        end
+      end
+
+      context 'reviewing a merge' do
+        let(:mode) { ToolsController::MergeModes::REVIEW }
+
+        it "allows access" do
+          expect(page).to have_http_status 200
+        end
       end
     end
   end
