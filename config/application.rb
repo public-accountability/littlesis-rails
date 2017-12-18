956e5166247d26b8ef6c90e189005ee98fb7a2e4
ziggy <ziggy@littlesis.org>
2017-12-18 09:54:20 -0500

    allow access to new help pages at /rails/new via rack rewrite

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Lilsis
       rewrite  %r{/(person|org)/([0-9]+)/[^/ ]+(/.*)?}, '/entities/$2$3'
       rewrite  %r{/(person|org)/(.*)},                  '/entities/$2'
       rewrite  %r{/(person|org)/(.*)},                  '/entities/$2'
+      rewrite  %r{/rails/help/(.*)},                    '/help/$1'
       r301     %r{/user/(.*)},                          '/users/$1'
     end
 
