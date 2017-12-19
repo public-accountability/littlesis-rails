e895d18dbaf21245173d9878ee0308f35e3cadf9
aguestuser <aguestuser@riseup.net>
2017-12-19 18:08:25 -0500

    [#443] add merge review page
    * add merge request description (needs styling)
    * extract merge execute form partial
    * add merge review form partial
    * add merge request stub form partial
    * add logic for approving/denying merge request in controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -7,13 +7,15 @@ class ToolsController < ApplicationController
     EXECUTE = 'execute'
     REQUEST = 'request'
     REVIEW  = 'review'
+    ALL = [SEARCH, EXECUTE, REQUEST, REVIEW]
   end
 
   before_action :authenticate_user!
   before_action :set_entity, only: [:bulk_relationships]
   before_action :admins_only, only: [:merge_entities!]
-  before_action :parse_merge_params, only: [:merge_entities]
-  before_action :set_source_and_dest, only: [:merge_entities!]
+  before_action :parse_merge_mode, only: [:merge_entities, :merge_entities!]
+  before_action :parse_merge_get_params, only: [:merge_entities]
+  before_action :parse_merge_post_params, only: [:merge_entities!]
 
   def bulk_relationships
   end
@@ -26,48 +28,79 @@ class ToolsController < ApplicationController
   # POST /tools/merge
   # do the merge
   def merge_entities!
-    @source.merge_with(@dest)
-    redirect_to @dest
+    case @merge_mode
+    when MergeModes::EXECUTE
+      @source.merge_with(@dest)
+      redirect_to @dest
+    when MergeModes::REVIEW
+      @merge_request.send("#{@decision}_by!".to_sym, current_user)
+      redirect_to @merge_request.source
+    end
   end
 
   private
 
-  def parse_merge_params
-    @source = Entity.find(params.require(:source).to_i)
-    @merge_mode = params.require(:mode)
+  def parse_merge_post_params
+    admins_only
     case @merge_mode
-    when MergeModes::SEARCH
-      @query = params[:query]
-      set_similar_entities
     when MergeModes::EXECUTE
-      admins_only
-      parse_merge_report_params
-    when MergeModes::REQUEST
-      parse_merge_report_params
+      set_source_and_dest
     when MergeModes::REVIEW
-      admins_only
-      parse_merge_report_params
+      @merge_request = MergeRequest.find(params.require(:request).to_i)
+      @decision = params.require(:decision)
+      raise Exceptions::NotFoundError unless %w[approved denied].include? @decision
     end
   end
 
-  def set_similar_entities
-    if @query.present?
-      similar_entities = Entity::Search.similar_entities(
-        @source, query: @query, per_page: SIMILAR_ENTITIES_PER_PAGE
-      )
-    else
-      similar_entities = @source.similar_entities(SIMILAR_ENTITIES_PER_PAGE)
-    end
+  def parse_merge_get_params
+    send("parse_merge_#{@merge_mode}_params".to_sym)
+  end
+
+  def parse_merge_mode
+    @merge_mode = params.require(:mode)
+    raise Exceptions::NotFoundError unless MergeModes::ALL.include? @merge_mode
+  end
+
+  def parse_merge_search_params
+    set_source
+    @query = params[:query]
+    similar_entities =
+      @query.present? ?
+        Entity::Search.similar_entities(@source,query: @query, per_page: SIMILAR_ENTITIES_PER_PAGE) :
+        @source.similar_entities(SIMILAR_ENTITIES_PER_PAGE)
+
     @similar_entities = similar_entities.map(&Entity::Search::SIMILAR_ENTITIES_PRESENTER)
   end
 
-  def parse_merge_report_params
-    @dest = Entity.find(params[:dest].to_i)
+  def parse_merge_execute_params
+    admins_only
+    set_source_and_dest
+    set_entity_merger
+  end
+
+  def parse_merge_request_params
+    set_source_and_dest
+    set_entity_merger
+  end
+  
+  def parse_merge_review_params
+    admins_only
+    @merge_request = MergeRequest.find(params.require(:request).to_i)
+    @source = @merge_request.source
+    @dest = @merge_request.dest
+    set_entity_merger
+  end
+
+  def set_entity_merger
     @entity_merger = EntityMerger.new(source: @source, dest: @dest).merge
   end
 
-  def set_source_and_dest
+  def set_source
     @source = Entity.find(params.require(:source).to_i)
+  end
+
+  def set_source_and_dest
+    set_source
     @dest = Entity.find(params.require(:dest).to_i)
   end
 
--- spec/features/merging_spec.rb
+++ spec/features/merging_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 feature 'Merging entities' do
   let(:mode) {}
+  let(:user) {}
+  let(:merge_request) {}
   let(:source) { create(:merge_source_person) }
   let(:dest) { create(:entity_person, :with_person_name) }
   let(:dest_param) { "&dest=#{dest.id}"}
@@ -34,12 +36,52 @@ feature 'Merging entities' do
 
   describe "using merge pages" do
 
+    def should_show_merge_report
+      page_has_selector 'h2', count: 2
+      page_has_selector 'h2', text: "It will delete "
+      page_has_selector 'h4', text: "This will make the following changes:"
+      page_has_selector '#merge-report li', count: 4
+      page_has_selector 'li', text: "Transfer 1 relationships"
+    end
+
+    def should_show_merge_form(mode)
+      page_has_selector '#merge-entities-form'
+      case mode
+      when :execute
+        page_has_selector "input.btn[value='Merge']", count: 1
+        page_has_selector 'a.btn', text: 'Go back'
+        page_has_selector 'a.btn', text: 'Dashboard'
+      when :review
+        page_has_selector "input.btn[value='Approve']", count: 1
+        page_has_selector "input.btn[value='Deny']", count: 1
+      end
+    end
+
+    def should_commit_merge
+      successfully_visits_page entity_path(dest)
+
+      expect(dest.reload.relationships.count).to eql 1
+      expect(dest.lists.count).to eql 1
+      expect(source.reload.is_deleted).to be true
+      expect(source.merged_id).to eql dest.id
+    end
+
+    def should_not_commit_merge
+      successfully_visits_page entity_path(source)
+
+      expect(dest.reload.relationships.count).to eql 0
+      expect(dest.lists.count).to eql 0
+      expect(source.reload.is_deleted).to be false
+      expect(source.merged_id).to be_nil
+    end
+
     before do
       allow(Entity::Search).to receive(:similar_entities).and_return([dest])
-      visit tools_merge_path(mode: mode,
-                             source: source.id,
-                             dest: dest.id,
-                             query: query)
+      visit tools_merge_path(mode:    mode,
+                             source:  source&.id,
+                             dest:    dest&.id,
+                             query:   query,
+                             request: merge_request&.id)
     end
 
     context 'as a non-admin user' do
@@ -118,37 +160,59 @@ feature 'Merging entities' do
         let(:list) { create(:list) }
         let(:source){ create(:merge_source_person) }
 
-        scenario 'viewing the report page' do
-          page_has_selector 'h2', count: 2
-          page_has_selector 'h2', text: "It will delete "
-          page_has_selector 'h4', text: "This will make the following changes:"
-          page_has_selector '#merge-report li', count: 4
-          page_has_selector 'li', text: "Transfer 1 relationships"
-          page_has_selector '#merge-entities-form'
-          page_has_selector "input.btn[value='Merge']", count: 1
-          page_has_selector 'a.btn', text: 'Go back'
-          page_has_selector 'a.btn', text: 'Dashboard'
-        end
-
-        scenario 'clicking to merge the two entities' do
+        before do
           expect(dest.relationships.count).to eql 0
           expect(dest.lists.count).to eql 0
+        end
+        
+        it 'shows a merge report' do
+          should_show_merge_report
+          should_show_merge_form :execute
+        end
 
+        it 'commits the merge when user clicks `Merge`' do
           click_button "Merge"
-          successfully_visits_page entity_path(dest)
-
-          expect(dest.reload.relationships.count).to eql 1
-          expect(dest.lists.count).to eql 1
-          expect(source.reload.is_deleted).to be true
-          expect(source.merged_id).to eql dest.id
+          should_commit_merge
         end
       end
 
       context 'reviewing a merge' do
         let(:mode) { ToolsController::MergeModes::REVIEW }
-
+        let(:requesting_user) { create(:really_basic_user) }
+        let(:username) { requesting_user.username }
+        let(:merge_request) { create(:merge_request,
+                                     user: requesting_user,
+                                     source: source,
+                                     dest: dest) }
+        
         it "allows access" do
           expect(page).to have_http_status 200
+        end
+
+        it "shows a merge report" do
+          should_show_merge_report
+          should_show_merge_form :review
+        end
+
+        it "shows a review description" do
+          desc = page.find("#review-description")
+          expect(desc).to have_link username, "/users/#{username}"
+          expect(desc).to have_text "requested"
+          expect(desc).to have_text LsDate.pretty_print(merge_request.created_at)
+        end
+
+        it "approves merge request when admin clicks `Approve`" do
+          click_button "Approve"
+          should_commit_merge
+          expect(merge_request.reload.status).to eql 'approved'
+          expect(merge_request.reviewer).to eql user
+        end
+
+        it "denies merge request when admin clicks `Deny`" do
+          click_button "Deny"
+          should_not_commit_merge
+          expect(merge_request.reload.status).to eql 'denied'
+          expect(merge_request.reviewer).to eql user
         end
       end
     end
