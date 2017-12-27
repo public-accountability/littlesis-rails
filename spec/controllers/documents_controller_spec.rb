f1b87c92bf2f32c01d5482466a09453c3b0cf36c
ziggy <ziggy@elephant-bird.net>
2017-12-27 10:42:56 -0500

    cleanup document controller specs

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -1,89 +1,60 @@
 require 'rails_helper'
 
-RSpec.describe DocumentsController, type: :controller do
+describe DocumentsController, type: :controller do
+  describe 'routes' do
+    it { should route(:get, '/documents/123/edit').to(action: :edit, id: '123') }
+    it { should route(:patch, '/documents/123').to(action: :update, id: '123') }
+  end
 
-	describe 'routes' do
-		it { should route(:get, '/documents/123/edit').to(action: :edit, id: '123') }
-		it { should route(:patch, '/documents/123').to(action: :update, id: '123') }
-	end
-
-	describe "GET #edit" do
-	    context 'as a logged-in user' do
-	      login_user
-	    	before do 
-	    		@doc = build(:document)
-      		expect(Document).to receive(:find).with('1').and_return(@doc)
-	    		get :edit, id: 1
-	    	end
-	      it { should render_template 'edit' }
-	    end
-
-	    context 'without logging in' do
-	    	before { get :edit, id: 1 }
-	      it { should redirect_to '/login' }
-	    end
-	end
-
-	describe "PATCH #update" do
-    def valid_params
-				{
-					id: 1, 
-					document: 
-						{
-	 					name: 'Da Googs',
-
-	          excerpt: 'google google google',
-
-	 					url: 'http://www.google.com',
-	          publication_date: '2016-01-01',
-	          ref_type: '1'
-	        }
-	      }
+  describe "GET #edit" do
+    context 'as a logged-in user' do
+      login_user
+      before do
+        @doc = build(:document)
+        expect(Document).to receive(:find).with('1').and_return(@doc)
+        get :edit, id: 1
+      end
+      it { should render_template 'edit' }
     end
 
-    def invalid_params
-			{
-				id: 1, 
-				document: {
-
-          publication_date: '1234567890'
-				}
-      }
+    context 'without logging in' do
+      before { get :edit, id: 1 }
+      it { should redirect_to '/login' }
     end
+  end
 
-		login_user
+  describe "PATCH #update" do
+    login_user
+    let(:document_params) do
+      attributes_for(:document).merge(excerpt: Faker::Lorem.sentence, publication_date: '2016-01-01', ref_type: '1')
+    end
 
     before :each do
-
-  		@doc = build(:document)
-    	expect(Document).to receive(:find).with('1').and_return(@doc)
-  		@request.env['HTTP_REFERER'] = 'http://test.com/sessions/new'
+      @doc = create(:document, document_params)
+      @request.env['HTTP_REFERER'] = 'http://test.com/sessions/new'
     end
 
-		context 'with valid params' do
-	  	before do 
-	  		patch :update, valid_params
+    context 'with valid params' do
+      before do
+        patch :update, id: @doc.id, document: document_params.merge(publication_date: '2017-01-01')
+      end
+      it { should redirect_to root_path }
 
-	  	end
-	  	it { should redirect_to root_path }
-		end
+      it 'changes the publication date' do
+        expect(@doc.reload.publication_date).to eql '2017-01-01'
+      end
+    end
 
-		context 'with invalid params' do
-	  	before do 
-	  		patch :update, invalid_params
-	  	end
-			it { should redirect_to edit_document_path(@doc) }
+    context 'with invalid params' do
+      before do
+        patch :update, id: @doc.id, document: document_params.merge(publication_date: 'THIS IS NOT A DATE')
+      end
 
-        expect(@doc.name).to eql 'Da Googs'
-	  	end
-	  	it { should redirect_to :back }
-		end
+      it { should redirect_to edit_document_path(@doc) }
 
-		context 'with invalid params' do
-	  	before { patch :update, invalid_params }
-			it { should render_template 'edit' }
-
-		end
-	end
-
+      it 'does not change the document name' do
+        expect(@doc.reload.publication_date).to eql '2016-01-01'
+      end
+    end
+  end
 end
