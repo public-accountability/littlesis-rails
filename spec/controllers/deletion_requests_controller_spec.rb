8550f48daf03d881030047adb65a09ecb6a10b19
aguestuser <aguestuser@riseup.net>
2017-12-28 18:15:35 -0500

    [#445] remove flakey controller test on before-action hook

--- spec/controllers/deletion_requests_controller_spec.rb
+++ spec/controllers/deletion_requests_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe DeletionRequestsController, type: :controller do
   it { should use_before_action(:authenticate_user!) }
-  it { should use_before_action(:set_entity) }
   it { should route(:get, '/deletion_requests/1/review').to(action: :review, id: 1) }
   it { should route(:post, '/deletion_requests/1/review').to(action: :commit_review, id: 1) }
 end
