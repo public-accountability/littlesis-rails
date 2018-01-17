a1fb2b08d0bd2b45ef04cba096e16d38293cdffd
ziggy <ziggy@littlesis.org>
2018-01-17 11:12:29 -0500

    [#478] tests for User.active_users
    * minor refactor of other UserEdits specs

--- spec/models/concerns/user_edits_spec.rb
+++ spec/models/concerns/user_edits_spec.rb
@@ -31,30 +31,63 @@ describe UserEdits do
         expect(Entity).to receive(:unscoped).and_return(entity_find)
       end
 
-      it 'returns array of <UserEdit>s' do
-        expect(subject.recent_edits_presenter.length).to eql 2
-        expect(subject.recent_edits_presenter.first).to be_a UserEdits::Edits::UserEdit
-        expect(subject.recent_edits_presenter.first.version).to eql versions.first
-        expect(subject.recent_edits_presenter.first.resource).to eql relationship
-        expect(subject.recent_edits_presenter.second.version).to eql versions.second
-        expect(subject.recent_edits_presenter.second.resource).to eql entity
-        expect(subject.recent_edits_presenter.second.action).to eql 'Update'
-        expect(subject.recent_edits_presenter.second.time).to eql versions.second.created_at.strftime('%B %e, %Y%l%p')
+      describe 'recenter_edits_presenter' do
+        subject { UserEdits::Edits.new(user).recent_edits_presenter }
+
+        it 'returns array of <UserEdit>s' do
+          expect(subject.length).to eql 2
+          expect(subject.first).to be_a UserEdits::Edits::UserEdit
+          expect(subject.first.version).to eql versions.first
+          expect(subject.first.resource).to eql relationship
+          expect(subject.second.version).to eql versions.second
+          expect(subject.second.resource).to eql entity
+          expect(subject.second.action).to eql 'Update'
+          expect(subject.second.time).to eql(versions.second.created_at.strftime('%B %e, %Y%l%p'))
+        end
       end
 
       describe '#record_lookup' do
-        it 'generates a lookup hash' do
-          expect(subject.send(:record_lookup))
-            .to eq({
-                     'Relationship' => { relationship.id => relationship },
-                     'Entity' => { entity.id => entity }
-                   })
+        let(:record_lookup) do
+          {
+            'Relationship' => { relationship.id => relationship },
+            'Entity' => { entity.id => entity }
+          }
         end
+
+        specify { expect(subject.send(:record_lookup)).to eq(record_lookup) }
       end
     end
   end # UserEdits::Edits
 
-  describe 'UserEdits.active_users' do
+  describe 'User.active_users' do
+    before(:all) { PaperTrail::Version.delete_all }
 
+    let(:user_one) { create_really_basic_user }
+    let(:user_two) { create_really_basic_user }
+    let!(:update_event) { create(:entity_version, whodunnit: user_one.id.to_s, event: 'update') }
+    let!(:destory_event) { create(:entity_version, whodunnit: user_one.id.to_s, event: 'destroy') }
+    let!(:soft_delete_event) { create(:relationship_version, whodunnit: user_one.id.to_s, event: 'soft_delete') }
+    let!(:create_event) { create(:entity_version, whodunnit: user_two.id.to_s, event: 'create') }
+
+    subject { User.active_users }
+
+    it 'returns an array of ActiveUsers, correctly sorted' do
+      expect(subject.length). to eql 2
+      expect(subject.first).to be_a UserEdits::ActiveUser
+      expect(subject.first.user).to eql user_one
+      expect(subject.second.user).to eql user_two
+    end
+
+    it 'ActiveUser contains correct edits count' do
+      expect(subject.first.edits).to eql 3
+      expect(subject.first.create_count).to be_zero
+      expect(subject.first.update_count).to eql 1
+      expect(subject.first.delete_count).to eql 2
+
+      expect(subject.second.edits).to eql 1
+      expect(subject.second.create_count).to eql 1
+      expect(subject.second.update_count).to be_zero
+      expect(subject.second.delete_count).to be_zero
+    end
   end
 end
