11697f254580f1a5396d2400726730f3b715d64a
ziggy <ziggy@littlesis.org>
2018-01-22 14:46:28 -0500

    [#496] fix order on homepage data summary
    sql lesson learned: don't depending on sorting in sub-queries.
    
    resolves #496

--- app/models/extension_record.rb
+++ app/models/extension_record.rb
@@ -8,26 +8,26 @@ class ExtensionRecord < ApplicationRecord
   # Returns nested array:
   # [ [ count, display_name ] ]
   def self.stats
-    sql = "SELECT subquery.c, display_name 
-           FROM (
-                    SELECT definition_id, count(*) as c
-                    FROM extension_record
-                    INNER JOIN entity
-                    ON entity.id = extension_record.entity_id
-                    WHERE entity.is_deleted = 0
-                    GROUP BY definition_id
-                    ORDER BY c DESC 
-                 ) as subquery
-            INNER JOIN extension_definition
-            ON subquery.definition_id = extension_definition.id"
+    sql = <<-SQL
+    SELECT subquery.c, display_name
+    FROM (
+           SELECT definition_id, count(*) as c
+           FROM extension_record
+           INNER JOIN entity ON entity.id = extension_record.entity_id
+           WHERE entity.is_deleted = 0
+           GROUP BY definition_id
+         ) as subquery
+     INNER JOIN extension_definition ON subquery.definition_id = extension_definition.id
+     ORDER BY subquery.c desc
+     SQL
     ApplicationRecord.connection.execute(sql).to_a
   end
 
-  # Returns nested array 
+  # Returns nested array
   # [ [ count, display name] ]
   def self.data_summary
-    Rails.cache.fetch('data_summary_stats', expires_in: 2.hours) do 
-      ExtensionRecord.stats.unshift( [Reference.count, 'Citation'] , [ Relationship.count, 'Relationship'  ]  )
+    Rails.cache.fetch('data_summary_stats', expires_in: 2.hours) do
+      ExtensionRecord.stats.unshift([Reference.count, 'Citation'], [Relationship.count, 'Relationship'])
     end
   end
 end
