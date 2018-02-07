842385bf843ab87546104752322a71e664b6b97b
ziggy <ziggy@littlesis.org>
2018-02-07 12:02:41 -0500

    LsHash#with_last_user accepts User and SfGuardUser as valid inputs

--- app/utility/ls_hash.rb
+++ app/utility/ls_hash.rb
@@ -1,5 +1,22 @@
 class LsHash < ActiveSupport::HashWithIndifferentAccess
-  def with_last_user(user_id)
-    merge(last_user_id: user_id.to_i)
+  def with_last_user(user_or_user_id)
+    merge(last_user_id: extract_last_user_id_from(user_or_user_id))
+  end
+
+  private
+
+  def extract_last_user_id_from(input)
+    case input
+    when String
+      input.to_i
+    when Integer
+      input
+    when User
+      input.sf_guard_user_id
+    when SfGuardUser
+      input.id
+    else
+      raise ArgumentError, "Invalid class. Provided: #{input.class}"
+    end
   end
 end
