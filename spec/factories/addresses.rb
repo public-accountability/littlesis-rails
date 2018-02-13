6b1be25845dce9be034924de643da2971104316e
ziggy <ziggy@littlesis.org>
2018-02-13 11:37:25 -0500

    fix address factory
    sometimes the Faker would return a string that's too long
    ...but not anymore!

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :address do
-    street1 { Faker::Address.street_name  }
+    street1 { Faker::Address.street_name }
     city { Faker::Address.city }
-    country_name { Faker::Address.country }
+    country_name { Faker::Address.country.slice(0, 50) }
   end
 end
