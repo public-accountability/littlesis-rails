549bb52fc5f29ab501f3e7639561966d47b0d9a8
ziggy <ziggy@littlesis.org>
2018-02-19 19:01:23 -0500

    update oligrapher-dev to dfffde2

--- app/assets/javascripts/oligrapher-dev.js
+++ app/assets/javascripts/oligrapher-dev.js
@@ -1,3 +1,3 @@
-//= require oligrapher-dev/build/oligrapher.min
+//= require oligrapher-dev/build/oligrapher
 //= require oligrapher-dev/build/LsDataConverter
 //= require oligrapher-dev/build/LsDataSource
