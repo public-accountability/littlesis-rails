b4d95adfd8b302e165d4d2529b3af4fcbebe15dc
ziggy <ziggy@littlesis.org>
2018-02-21 11:29:06 -0500

    move link description to presenter: RelationshipLabel

---
+++ app/presenters/relationship_label.rb
@@ -0,0 +1,89 @@
+# coding: utf-8
+
+# Provides a short label for a Relationship.
+# Used by Link and Oligrapher.
+class RelationshipLabel < SimpleDelegator
+  attr_reader :is_reverse
+
+  def initialize(relationship, is_reverse = false)
+    super(relationship)
+    @is_reverse = is_reverse
+  end
+
+  def label
+    return title if is_position? || is_member?
+    return humanize_contributions if is_donation?
+    return education_label if is_education?
+    text = is_reverse ? description1 : description2
+    return text if text.present?
+    default_description
+  end
+
+  private
+
+  def education_label
+    education_description = degree_abbrevation || degree || default_description
+    return "#{education_description}, #{education_field}" if education_field
+    education_description
+  end
+
+  def humanize_contributions
+    str = ""
+
+    if filings.nil? || filings.zero?
+
+      if description1 == 'NYS Campaign Contribution'
+        str << "NYS Campaign Contribution"
+      elsif description1 == "Campaign Contribution"
+        str << "Donation"
+      elsif description1.present?
+        str << description1
+      else
+        str << default_description
+      end
+
+    else
+      str << ActionController::Base.helpers.pluralize(filings, 'contribution')
+    end
+
+    unless amount.nil?
+      str << " · "
+      str << ActiveSupport::NumberHelper.number_to_currency(amount, precision: 0)
+    end
+    str
+  end
+
+  def default_description
+    case category_id
+    when 1
+      return 'Position'
+    when 2
+      return description1 if description1.present?
+      return 'Student' if is_reverse
+      return 'School' unless is_reverse
+    when 3
+      return 'Member'
+    when 4
+      return 'Relative'
+    when 5
+      return 'Donation/Grant'
+    when 6
+      return 'Service/Transaction'
+    when 7
+      return 'Lobbying'
+    when 8
+      return 'Social'
+    when 9
+      return 'Professional'
+    when 10
+      return 'Owner'
+    when 11
+      return 'Child Org' if is_reverse
+      return 'Parent Org' unless is_reverse
+    when 12
+      return 'Affiliation'
+    else
+      return ''
+    end
+  end
+end
---
+++ spec/presenters/relationship_label_spec.rb
@@ -0,0 +1,152 @@
+# coding: utf-8
+require 'rails_helper'
+
+describe RelationshipLabel do
+  let(:relationship) { build(:relationship, category_id: 1) }
+  subject { RelationshipLabel.new(relationship).label }
+
+  context 'relationship is a postiion' do
+    let(:relationship) { build(:position_relationship, description1: 'Director') }
+    it { is_expected.to eql 'Director' }
+  end
+
+  context 'relationship is a membership' do
+    let(:relationship) { build(:membership_relationship) }
+    it { is_expected.to eql 'Member' }
+  end
+
+  describe 'donation relationships' do
+    context 'campaign contribution' do
+      let(:relationship) do
+        build(:donation_relationship,
+              filings: nil, amount: 2_000_000, description1: "Campaign Contribution")
+      end
+      it { is_expected.to eql "Donation · $2,000,000" }
+    end
+
+    context 'NYS campaign contribution' do
+      let(:relationship) do
+        build(:donation_relationship,
+              filings: nil, amount: 1_000_000, description1: "NYS Campaign Contribution")
+        it { is_expected.to eql "NYS Campaign Contribution · $1,000,000" }
+      end
+    end
+
+    context 'NYS campaign contribution with 2 filings' do
+      let(:relationship) do
+        build(:donation_relationship,
+              filings: 2, amount: 1_000_000, description1: "NYS Campaign Contribution")
+        it { is_expected.to eql "2 contributions · $1,000,000" }
+      end
+    end
+
+    context 'miscellaneous donation' do
+      let(:relationship) do
+        build(:donation_relationship, filings: nil, amount: 1000, description1: nil)
+      end
+      it { is_expected.to eql "Donation/Grant · $1,000" }
+    end
+
+    context 'miscellaneous donation with custom type' do
+      let(:relationship) do
+        build(:donation_relationship,
+              filings: nil, amount: 1000, description1: 'suspicious contribution')
+      end
+      it { is_expected.to eql "suspicious contribution · $1,000" }
+    end
+  end
+
+  context 'education relationship' do
+    let(:education) { build(:education, degree_id: 2, field: nil) }
+    let(:description1) { nil }
+    let(:relationship) do
+      build(:education_relationship, education: education, description1: description1)
+    end
+
+    context 'degree abbreviation exists' do
+      it { is_expected.to eql "BA" }
+    end
+
+    context 'degree abbrevation exists with a  field' do
+      let(:education) { build(:education, degree_id: 2, field: 'Psychology') }
+      it { is_expected.to eql "BA, Psychology" }
+    end
+
+    context 'abbreviation exists' do
+      let(:education) { build(:education, degree_id: 12, field: nil) }
+      let(:relationship) do
+        build(:education_relationship, education: education, description1: description1)
+      end
+      it 'shows full degree name' do
+        expect(subject).to eql "Honorus Degree"
+      end
+    end
+
+    context 'has description1 but no field or degree' do
+      let(:education) { build(:education, degree_id: nil, field: nil) }
+      let(:description1) { 'Undergraduate' }
+      it { is_expected.to eql "Undergraduate" }
+    end
+
+    context 'shows default description' do
+      let(:education) { build(:education, degree_id: nil, field: nil) }
+      it { is_expected.to eql "School" }
+    end
+
+    context 'shows default description reversed' do
+      let(:education) { build(:education, degree_id: nil, field: nil) }
+      subject { RelationshipLabel.new(relationship, true).label }
+      it { is_expected.to eql "Student" }
+    end
+
+    context 'education is missing' do
+      let(:relationship) { build(:education_relationship, education: nil) }
+      it { is_expected.to eql "School" }
+    end
+  end
+
+  context 'social relationships' do
+    context 'description fields are filled out' do
+      let(:relationship) do
+        build(:social_relationship, description1: 'friend', description2: 'buddy')
+      end
+
+      context 'normal (not reversed)' do
+        subject { RelationshipLabel.new(relationship, false).label }
+        it { is_expected.to eql "buddy" }
+      end
+
+      context 'reversed' do
+        subject { RelationshipLabel.new(relationship, true).label }
+        it { is_expected.to eql "friend" }
+      end
+    end
+
+    context 'description fields are empty' do
+      let(:relationship) { build(:social_relationship, description1: nil, description2: nil) }
+      it { is_expected.to eql "Social" }
+    end
+  end
+
+  describe '#humanize_contributions' do
+    subject { RelationshipLabel.new(relationship).send(:humanize_contributions) }
+
+    context 'donation relationship has filings' do
+      let(:relationship) { build(:donation_relationship, filings: 3, amount: 1000) }
+      it { is_expected.to eql "3 contributions · $1,000" }
+    end
+
+    context 'filings is nil' do
+      let(:relationship) { build(:donation_relationship, filings: nil, amount: 2_000_000) }
+      it { is_expected.to eql "Donation/Grant · $2,000,000" }
+    end
+
+    context 'filing is 0' do
+      let(:relationship) do
+        build(:donation_relationship,
+              filings: 0, amount: 2_000_000, description1: 'Campaign Contribution')
+      end
+      it { is_expected.to eql "Donation · $2,000,000" }
+    end
+  end
+end
