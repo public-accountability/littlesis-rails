777ae18a4756f20214ae12f54049a2ace0e5bc97
ziggy <ziggy@littlesis.org>
2018-02-26 15:53:51 -0500

    [#549] move `EntityMatcher` to app/utility
    - remove old methods

--- app/utility/entity_matcher.rb
+++ app/utility/entity_matcher.rb
@@ -1,5 +1,26 @@
 class EntityMatcher
-  def self.by_person_name(first, last, middle = nil, suffix = nil, nick = nil, maiden = nil)
+
+  # +by_person_name+, +by_full_name+ and +by_org_name+
+  # are legacy methods used in ForbesFourHundredImporter and EntityNameAddressCsvImporter,
+  # which themselves are outdated import code that needs to be re-written.
+  # Once those classes are removed or refactored, they can be removed from this class.
+
+  def self.by_person_name
+    raise NotImplementedError
+  end
+
+  def self.by_full_name
+    raise NotImplementedError
+  end
+
+  def self.by_org_name
+    raise NotImplementedError
+  end
+end
+
+# OLD implementation:
+=begin
+def self.by_person_name(first, last, middle = nil, suffix = nil, nick = nil, maiden = nil)
     first = [first, nick] if nick
     last = [last, maiden] if maiden
     firsts = [first].concat(Person.same_first_names(first))
@@ -34,4 +55,5 @@ class EntityMatcher
     results = Entity.search "@(name,aliases) #{Riddle::Query.escape(stripped)} @primary_ext Org", per_page: 20, match_mode: :extended, with: { is_deleted: 0 }
     matches.concat(results).uniq(&:id)
   end
-end
+
+=end
