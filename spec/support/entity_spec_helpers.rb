69b2ed8c245859fd8fb333d9f0f41f222b8a08f6
ziggy <ziggy@littlesis.org>
2018-02-28 16:05:53 -0500

    Now Single Up All Lines!

--- spec/support/entity_spec_helpers.rb
+++ spec/support/entity_spec_helpers.rb
@@ -1,4 +1,28 @@
+# rubocop:disable Style/MutableConstant
+#
+# creates and builds fake entities
 module EntitySpecHelpers
+  TRAVERSES = ['Frank Traverse', 'Kit Traverse', 'Lake Traverse',
+               'Mayva Traverse', 'Reef Traverse', 'Webb Traverse']
+
+  CHUMS = ['Miles Blundell', 'Chick Counterfly', 'Lindsay Noseworth',
+           'Darby Suckling', 'Randolph St. Cosmo']
+
+  FRIENDS_OF_CHUMS = ['Lew Basnight', 'Erlys Rideout', 'Cyprian Latewood',
+                      'Yashmeen Halfcourt', 'Nikola Tesla']
+
+  BAD_VIBES = ['Scarsdale Vibe', 'Colfax Vibe', 'Dittany Vibe',
+               'Edwarda Vibe', 'Fleetwood Vibe', 'Wilshire Vibe']
+
+  ATD_CHARACTERS = TRAVERSES + CHUMS + FRIENDS_OF_CHUMS + BAD_VIBES
+
+  %i[ATD_CHARACTERS TRAVERSES CHUMS FRIENDS_OF_CHUMS BAD_VIBES].each do |set|
+    define_singleton_method(set.to_s.downcase) do |num: 3, method: :create|
+      const_get(set).sample(num).map do |person|
+        FactoryBot.send(method, :entity_person, name: person)
+      end
+    end
+  end
 
   def self.person(*names)
     first = name_for 'first'
@@ -29,3 +53,4 @@ module EntitySpecHelpers
     end
   end
 end
+# rubocop:enable Style/MutableConstant
