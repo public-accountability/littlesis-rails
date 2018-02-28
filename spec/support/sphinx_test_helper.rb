ed8e5c99e5772543c5795830558276691dfc5966
ziggy <ziggy@littlesis.org>
2018-02-28 15:29:30 -0500

    refactor sphinx test helpers

--- spec/support/sphinx_test_helper.rb
+++ spec/support/sphinx_test_helper.rb
@@ -2,6 +2,8 @@ require 'rake'
 
 module SphinxTestHelper
   def setup_sphinx(*indexes)
+    ThinkingSphinx::Deltas.suspend!
+    yield if block_given?
     Lilsis::Application.load_tasks
     Rake::Task['ts:configure'].invoke
     ThinkingSphinx::Test.init
@@ -12,5 +14,16 @@ module SphinxTestHelper
   def teardown_sphinx
     ThinkingSphinx::Test.stop
     ThinkingSphinx::Test.clear
+    yield if block_given?
+    ThinkingSphinx::Deltas.resume!
   end
+
+  def delete_entity_tables
+    Entity.delete_all
+    Link.delete_all
+    Alias.delete_all
+    Org.delete_all
+    Person.delete_all
+  end
+  
 end
