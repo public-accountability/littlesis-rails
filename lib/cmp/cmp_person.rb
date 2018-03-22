aae338146c84e8a87558ea5aeed751890cd47cde
ziggy <ziggy@littlesis.org>
2018-03-22 15:13:37 -0400

    [#495] return empty set if name data is incomplete

--- lib/cmp/cmp_person.rb
+++ lib/cmp/cmp_person.rb
@@ -6,6 +6,11 @@ module Cmp
       fullname: [:entity, :name]
     }.freeze
 
+    def import!
+    end
+
+    # utilites to find matches
+
     def cmp_relationships
       @cmp_relationships ||= Cmp::Datasets
                                .relationships
@@ -25,20 +30,13 @@ module Cmp
     end
 
     def matches
-      EntityMatcher
-        .find_matches_for_person(search_name_or_hash, associated: associated_entity_ids)
-    end
-
-    def search_name_or_hash
-      if EntityMatcher::TestCase::Person.validate_person_hash(to_person_hash)
-        to_person_hash
-      elsif fetch("lastname").present?
-        Rails.logger.debug "searching using last name #{fetch('lastname')} for #{cmpid}"
-        fetch("lastname")
-      else
-        Rails.logger.warn "invalid search for #{cmpid}"
-        raise StandardError, "Cannot find name to search for matches"
+      unless EntityMatcher::TestCase::Person.validate_person_hash(to_person_hash)
+        Rails.logger.warn "#{cmpid} is missing a first or last name!"
+        return EntityMatcher::EvaluationResultSet.new([])
       end
+
+      EntityMatcher
+        .find_matches_for_person(to_person_hash, associated: associated_entity_ids)
     end
 
     def to_person_hash
