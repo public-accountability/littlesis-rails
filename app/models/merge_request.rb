6a1be5ff56cd4320ef666d7f14f8562afe002373
ziggy <ziggy@littlesis.org>
2018-03-27 16:29:16 -0400

    [#454] update MergeRequest

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -1,7 +1,10 @@
-class MergeRequest < UserRequest
-  # fields: user_id, type, status, source_id, dest_id
+# frozen_string_literal: true
 
-  validates_presence_of %i[source_id dest_id]
+class MergeRequest < UserRequest
+  # fields: user_id, type, status, source_id, dest_id, justification
+
+  validates :source_id, presence: true
+  validates :dest_id, presence: true
 
   belongs_to :source, class_name: 'Entity', foreign_key: 'source_id'
   belongs_to :dest, class_name: 'Entity', foreign_key: 'dest_id'
