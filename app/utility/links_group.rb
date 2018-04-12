71daf17b46241343b67475c2a6b8c4f44f6f1e07
ziggy <ziggy@littlesis.org>
2018-04-12 14:25:49 -0400

    [#598] correctly sort donor relationships
    we had previously assumed that donor relationships were correctly
    sorted in the database, although that proves to be incorrect.

--- app/utility/links_group.rb
+++ app/utility/links_group.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class LinksGroup
   attr_reader :links, :count, :keyword, :heading, :category_id
-  
+
   # input: [ <Link> ] , str, str, integer
   # Assumes that the links all have the same category
   def initialize(links, keyword, heading, count = nil)
     @keyword = keyword
     @heading = heading
-    @category_id = links.empty? ? nil : links[0].category_id 
+    @category_id = links.empty? ? nil : links[0].category_id
     @links = group_by_entity(order(links))
     # In situations where the array size does not represent the total count, i.e. queries with LIMIT
     if count.present?
@@ -16,22 +18,18 @@ class LinksGroup
       # If there are 4 links with the same person (i.e. entity2 is same) then @count will be 1
       @count = @links.count
     end
-    
   end
 
   def order(links)
     # return links.sort { |a, b| b.related.links.count <=> a.related.links.count } if @category_id == 4
     return links.sort_by { |link| link.related.link_count } if @category_id == 4
     if @category_id == 5
-      # donors are sorted by amount in the database
-      return links if keyword == 'donors'
       return links.sort { |a, b| a.relationship.amount && b.relationship.amount ? b.relationship.amount <=> a.relationship.amount : -1 }
     end
-    
+
     # TODO sort by date here or do sorting in SQL
     return links.sort_by { |link| link.relationship.updated_at }
   end
-
 
   # input: [ <Link> ] 
   # output: [ [<Link>] [<Link>] ]
--- app/utility/sorted_links.rb
+++ app/utility/sorted_links.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SortedLinks
   attr_reader :staff,
               :members,
