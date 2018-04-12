d82aaba024039fc4af95db1dda5a7a5ffe07ee51
ziggy <ziggy@littlesis.org>
2018-04-12 14:01:42 -0400

    [#526] fix active & deleted scope/method in SoftDelete

--- app/models/concerns/soft_delete.rb
+++ app/models/concerns/soft_delete.rb
@@ -1,22 +1,20 @@
-require 'active_support/concern'
+# frozen_string_literal: true
 
 module SoftDelete
   extend ActiveSupport::Concern
-  
+
   included do
     default_scope -> { where(is_deleted: false) }
-    scope :active, -> { where(is_deleted: false) }
-    scope :deleted, -> { where(is_deleted: true) }
   end
-  
+
   module ClassMethods
     def active
       where(is_deleted: false)
     end
-    
+
     def deleted
-      where(is_deleted: true)
-    end    
+      unscoped.where(is_deleted: true)
+    end
   end
 
   def soft_delete
