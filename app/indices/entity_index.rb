5945d1595225e407fea1c8d3093afaf8cb85c826
ziggy <ziggy@littlesis.org>
2018-04-24 13:58:42 -0400

    [#183] remove network from note and entity indices

--- app/indices/entity_index.rb
+++ app/indices/entity_index.rb
@@ -9,6 +9,5 @@ ThinkingSphinx::Index.define :entity, :with => :active_record, :delta => Thinkin
   has is_deleted, facet: true
   has last_user_id, facet: true
   has updated_at
-  has networks.id, as: :network_ids
   has link_count
 end
