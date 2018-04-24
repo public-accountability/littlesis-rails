051fc33c3c7683ed3cb56ea187d3d60fdaaf44a6
ziggy <ziggy@littlesis.org>
2018-04-24 17:14:22 -0400

    track list entity changes with paper_trail

--- app/models/list_entity.rb
+++ app/models/list_entity.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class ListEntity < ApplicationRecord
-  self.table_name = "ls_list_entity"
+  self.table_name = 'ls_list_entity'
   include Api::Serializable
+
+  has_paper_trail on: [:create, :destroy],
+                  meta: { entity1_id: :entity_id }
 
   belongs_to :list, inverse_of: :list_entities
   belongs_to :entity, inverse_of: :list_entities
