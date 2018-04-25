30bc3449c1b9eafb30881ac8b4cd0f830112b7ed
ziggy <ziggy@littlesis.org>
2018-04-25 16:06:08 -0400

    RecordHistory subclasses only have to define `versions_sql`

--- app/utility/entity_history.rb
+++ app/utility/entity_history.rb
@@ -3,51 +3,8 @@
 # Class used to retrieve versions and edits for entities
 class EntityHistory < RecordHistory
   model_name :entity
-  delegate :id, to: :entity, prefix: true
-
-  # int, int -> Kaminari::PaginatableArray
-  # Returns paginated array of versions
-  # Each version has an extra attribute -- user -- with the User model
-  # of the user responsible for the change
-  def versions(page: 1, per_page: 15)
-    raise ArgumentError unless page.is_a?(Integer) && per_page.is_a?(Integer)
-    define_as_presenters(
-      add_users_to_versions(
-        paginate_versions(page, per_page)
-      )
-    )
-  end
 
   private
-
-  # add singleton method `as_presenters` which converts each Version to EntityVersionPresenter
-  def define_as_presenters(versions)
-    versions.tap do |vrs|
-      vrs.define_singleton_method(:as_presenters) do
-        self.map { |v| EntityVersionPresenter.new(v) }
-      end
-    end
-  end
-
-  # [Array-like] -> [Array-like]
-  # add attribute user to each <Version> which the <User> model
-  def add_users_to_versions(versions)
-    users = User.lookup_table_for versions.map(&:whodunnit).compact.uniq
-    entity_for = self.entity
-
-    versions.map do |version|
-      version.tap do |v|
-        v.singleton_class.class_exec do
-          attr_reader :user
-          attr_reader :entity
-        end
-        v.instance_exec do
-          @user = users.fetch(v.whodunnit.to_i, nil)
-          @entity = entity_for
-        end
-      end
-    end
-  end
 
   # str, str -> str
   # SQL statement to extract version for the entity
@@ -57,9 +14,9 @@ class EntityHistory < RecordHistory
     <<~SQL
       SELECT #{select}
       FROM versions
-      WHERE (item_id = #{entity_id} AND item_type = 'Entity')
-         OR (entity1_id = #{entity_id})
-         OR (entity2_id = #{entity_id})
+      WHERE (item_id = #{entity.id} AND item_type = 'Entity')
+         OR (entity1_id = #{entity.id})
+         OR (entity2_id = #{entity.id})
       #{order}
     SQL
   end
--- app/utility/record_history.rb
+++ app/utility/record_history.rb
@@ -2,12 +2,11 @@
 
 # Subclassed by +EntityHistory+ and +ListHistory+
 #
-# at least two functions must be defined by the descendent:
-#   versions and versions_sql
 #
 # Contains a few common utility functions to
 # get information from Versions table
 class RecordHistory
+  PER_PAGE = 15
   include Pagination
 
   private_class_method def self.model_name(name)
@@ -21,17 +20,70 @@ class RecordHistory
     instance_variable_set "@#{self.class.const_get(:MODEL_NAME)}", record
   end
 
-  def versions
-    raise NotImplementedError    
+  # int, int -> Kaminari::PaginatableArray
+  # Returns paginated array of versions
+  # Each version has an extra attribute -- user -- with the User model
+  # of the user responsible for the change
+  def versions(page: 1, per_page: nil)
+    per_page = self.class.const_get(:PER_PAGE) if per_page.nil?
+    define_as_presenters(
+      add_users_to_versions(
+        paginate_versions(page, per_page)
+      )
+    )
   end
 
   protected
 
+  # This must be defined with two keyword arguments:
+  #     - select
+  #     - order
+  def versions_sql
+    raise NotImplementedError
+  end
+
+  private
+
   def paginate_versions(page, per_page)
     paginate(page,
-              per_page,
-              versions_for(page: page, per_page: per_page),
-              versions_count)
+             per_page,
+             versions_for(page: page, per_page: per_page),
+             versions_count)
+  end
+
+  # add singleton method `as_presenters` which converts each Version to EntityVersionPresenter
+  def define_as_presenters(versions)
+    presenter_class = "#{self.class.const_get(:MODEL_NAME).to_s.capitalize}VersionPresenter".constantize
+    versions.tap do |vrs|
+      vrs.define_singleton_method(:as_presenters) do
+        map { |v| presenter_class.new(v) }
+      end
+    end
+  end
+
+  # [Array-like] -> [Array-like]
+  # add attribute user to each <Version> which the <User> model
+  # adds record attribute to each version
+  #   for instance, if model_name is "entity"
+  #   each version will have the attribute 'entity' with the
+  #   record this class was initialized with
+  def add_users_to_versions(versions)
+    users = User.lookup_table_for versions.map(&:whodunnit).compact.uniq
+    model_name = self.class.const_get(:MODEL_NAME)
+    model_for = send(model_name)
+
+    versions.map do |version|
+      version.tap do |v|
+        v.singleton_class.class_exec do
+          attr_reader :user
+          attr_reader model_name
+        end
+        v.instance_exec do
+          @user = users.fetch(v.whodunnit.to_i, nil)
+          instance_variable_set "@#{model_name}", model_for
+        end
+      end
+    end
   end
 
   # int, int -> Array[Version]
@@ -48,12 +100,5 @@ class RecordHistory
 
   def versions_count
     ApplicationRecord.execute_one versions_sql(select: 'COUNT(*)', order: '')
-  end
-
-  # This must be defined with two keyword arguments:
-  #     - select
-  #     - order
-  def versions_sql
-    raise NotImplementedError
   end
 end
