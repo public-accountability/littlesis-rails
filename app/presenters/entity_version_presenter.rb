20aa48161e3a8708b16e5f3e2aa5696df2edd8b2
ziggy <ziggy@littlesis.org>
2018-04-25 14:09:56 -0400

    add `VersionPresenter` for shared version view code

--- app/presenters/entity_version_presenter.rb
+++ app/presenters/entity_version_presenter.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class EntityVersionPresenter < SimpleDelegator
-  include ActionView::Helpers::UrlHelper
+class EntityVersionPresenter < VersionPresenter
   delegate :relationship_path, :entity_path, :list_path, to: "Rails.application.routes.url_helpers"
   IGNORE_FIELDS = Set.new(%w[id created_at updated_at link_count last_user_id delta])
 
@@ -41,32 +40,7 @@ class EntityVersionPresenter < SimpleDelegator
   private
 
   def time
-    "<em>#{LsDate.pretty_print(created_at)}</em>"
-  end
-
-  def user_link
-    return 'System' if user.nil?
-    link_to user.username, "/users/#{user.username}"
-  end
-
-  def create_event?
-    event == 'create'
-  end
-
-  def update_event?
-    event == 'update'
-  end
-
-  def delete_event?
-    event == 'soft_delete' || event == 'destroy'
-  end
-
-  def object
-    PaperTrail::Serializers::YAML.load(super) unless super().nil?
-  end
-
-  def object_changes
-    PaperTrail::Serializers::YAML.load(super) unless super().nil?
+    "<em>#{str_time}</em>"
   end
 
   def entity_link
@@ -103,22 +77,11 @@ class EntityVersionPresenter < SimpleDelegator
     ExtensionDefinition.display_names.fetch(definition_id)
   end
 
-  # create events store information about the fields in 'object' column
-  # update events use the' object_changes column
-  def fetch_from_object_or_changeset(field)
-    method = create_event? ? :object_changes : :object
-    Array.wrap(send(method).fetch(field)).compact.first
-  end
-
   def model
     @model ||= item_type.constantize.unscoped.find(item_id)
   end
 
   def updated_fields_text
     "updated #{'field'.pluralize(updated_fields.length)} #{updated_fields.join(',')}"
-  end
-
-  def updated_fields
-    @updated_fields ||= (object_changes.keys.to_set - IGNORE_FIELDS).to_a
   end
 end
---
+++ app/presenters/version_presenter.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# intended to be subclassed to wrap PaperTrail::Version objects
+class VersionPresenter < SimpleDelegator
+  include ActionView::Helpers::UrlHelper
+
+  protected
+
+  def str_time
+    LsDate.pretty_print(created_at)
+  end
+
+  def user_link
+    return 'System' if user.nil?
+    link_to user.username, "/users/#{user.username}"
+  end
+
+  def create_event?
+    event == 'create'
+  end
+
+  def update_event?
+    event == 'update'
+  end
+
+  def delete_event?
+    event == 'soft_delete' || event == 'destroy'
+  end
+
+  def object
+    PaperTrail::Serializers::YAML.load(super) unless super().nil?
+  end
+
+  def object_changes
+    PaperTrail::Serializers::YAML.load(super) unless super().nil?
+  end
+
+  # create events store information about the fields in 'object' column
+  # update events use the' object_changes column
+  def fetch_from_object_or_changeset(field)
+    method = create_event? ? :object_changes : :object
+    Array.wrap(send(method).fetch(field)).compact.first
+  end
+
+  def updated_fields
+    @updated_fields ||= (object_changes.keys.to_set - IGNORE_FIELDS).to_a
+  end
+end
