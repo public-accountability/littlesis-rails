1080344217698e35c86b023dfa317bde11631e31
ziggy <ziggy@littlesis.org>
2018-04-30 15:39:29 -0400

    [#616] basic ui for editing blurb in place

--- app/assets/javascripts/pages/entity.js
+++ app/assets/javascripts/pages/entity.js
@@ -12,3 +12,47 @@ entity.summaryToggle = function(){
 entity.relationshipsToggle = function(e) {
   $(e.target).closest('.relationship-section').find('.collapse').collapse('toggle');
 };
+
+
+
+/* Editable blurb */
+
+entity._submitBlurb = function(newBlurb, original) {
+  if (newBlurb !== original) {
+    // do ajax
+  }
+
+  $('#entity-blurb-text').html(newBlurb);
+  $('#entity-blurb-pencil').show();
+};
+
+entity._editableBlurbInput = function(text) {
+
+  function handleKeyup(e) {
+    if (e.keyCode === 13) {
+      entity._submitBlurb($(this).val(), text);
+    } else if (e.keyCode === 27) {
+      $('#entity-blurb-text').html(text);
+      $('#entity-blurb-pencil').show();
+    }
+  }
+
+  return $('<input>', { "val": text, "keyup": handleKeyup });
+
+};
+
+
+// utility.entityInfo
+entity.editableBlurb = function() {
+  $('#entity-blurb-pencil').click(function(x){
+    $(this).hide();
+
+    // get existing blurb text
+    var blurb = $('#entity-blurb-text').text() || '';
+
+    // replace current text with input
+    $('#entity-blurb-text').html(
+      entity._editableBlurbInput(blurb)
+    );
+  });
+};
