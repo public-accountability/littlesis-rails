4ba38d7d835abfb4655d43f9e2d1f945286fd4bc
ziggy <ziggy@littlesis.org>
2018-05-22 15:34:23 -0400

    [#519] show bulletin's on dashboard

--- app/models/dashboard_bulletin.rb
+++ app/models/dashboard_bulletin.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class DashboardBulletin < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+
+  def self.last_bulletin_updated_at
+    DashboardBulletin.limit(1).pluck('updated_at').first
+  end
 end
