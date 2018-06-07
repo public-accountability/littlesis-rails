37e984eb5511740f00dcf311a6d4fbf61d88246b
ziggy <ziggy@littlesis.org>
2018-06-07 12:36:05 -0400

    [#656] add forms to edit external links on entity edit page

--- app/models/external_link.rb
+++ app/models/external_link.rb
@@ -3,11 +3,12 @@
 class ExternalLink < ApplicationRecord
   belongs_to :entity
 
-  LINK_PLACEHOLDER = '{}'
-
   # 1 -> sec
   # 2 -> wikipedia
   enum link_type: %i[reserved sec wikipedia]
+
+  LINK_PLACEHOLDER = '{}'
+  EDITABLE_TYPES = %i[wikipedia sec].freeze
 
   validates :link_type, presence: true
   validates :entity_id, presence: true
@@ -25,6 +26,14 @@ class ExternalLink < ApplicationRecord
       'Wikipedia'
     when 'reserved'
       raise TypeError, 'Do not create ExternalLinks of type "reserved"'
+    end
+  end
+
+  # returns only editable links that can be edited
+  def self.find_or_initalize_links_for(entity)
+    EDITABLE_TYPES.map do |link_type|
+      find_or_initialize_by(link_type: link_type,
+                            entity_id: Entity.entity_id_for(entity))
     end
   end
 
