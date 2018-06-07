6df5dc19b4ac8f571b0e497d03fa57b404a256a6
ziggy <ziggy@littlesis.org>
2018-06-07 15:47:34 -0400

    [#656] add or edit external links from edit entity page

--- app/controllers/external_links_controller.rb
+++ app/controllers/external_links_controller.rb
@@ -4,11 +4,27 @@ class ExternalLinksController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    el = ExternalLink.create!(external_link_params)
+    redirect_to edit_entity_path(el.entity)
   end
 
   def update
+    el = ExternalLink.find(params[:id])
+    el.update!(external_link_params)
+    redirect_to edit_entity_path(el.entity)
   end
 
   def destroy
+    el = ExternalLink.find(params[:id])
+    el.update!(external_link_params)
+    redirect_to edit_entity_path(el.entity)
+  end
+
+  private
+
+  def external_link_params
+    params
+      .require(:external_link)
+      .permit(:link_type, :link_id, :entity_id)
   end
 end
