9cd1a7f0cba16cac601a93fc463e7e802652d7b4
ziggy <ziggy@littlesis.org>
2018-06-19 12:25:40 -0400

    narrow list of entities included in potential cmp matches

--- lib/scripts/cmp.rb
+++ lib/scripts/cmp.rb
@@ -8,12 +8,20 @@ minimum_entity_link_count = 10
 
 print CSV.generate_line(%w[cmpid cmp_full_name entity_name entity_id entity_url entity_link_count cmp_relationships])
 
+
+def include_entity?(match_values)
+  (match_values.include?('similar_first_name') || match_values.include?('same_first_name')) &&
+    match_values.include?('same_last_name') &&
+    !match_values.include?('different_middle_name')
+end
+
+
 CSV.foreach(potential_matches_csv, headers: true) do |row|
   match_values = row['match_values'].split('|')
   entity = Entity.find_by(id: row['match_id'])
   next if entity.nil?
 
-  if entity.link_count >= minimum_entity_link_count || !match_values.include?('different_middle_name')
+  if entity.link_count >= minimum_entity_link_count || include_entity?(match_values)
 
     print CSV.generate_line([
                               row['cmpid'],
