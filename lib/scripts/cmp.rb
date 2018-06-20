07682ddeffd66b5d4c094f6b3578d68c49b87b0a
ziggy <ziggy@littlesis.org>
2018-06-20 10:54:49 -0400

    record log of new entiites

--- lib/scripts/cmp.rb
+++ lib/scripts/cmp.rb
@@ -7,13 +7,20 @@ potential_matches_csv = Rails.root.join('data', 'potential_cmp_matches.csv')
 
 print CSV.generate_line(%w[cmpid cmp_full_name entity_name entity_id entity_url entity_link_count cmp_relationships match_values])
 
-def skip_entity?(entity, match_values)
+def create_entity?(entity, match_values)
   return true if match_values.include?('different_middle_name')
   return true if CmpEntity.exists?(entity_id: entity.id)
-  return false if entity.link_count >= 10
-  (match_values.include?('similar_first_name') || match_values.include?('same_first_name')) && match_values.include?('same_last_name')
+  if (match_values.include?('similar_first_name') || match_values.include?('same_first_name')) && match_values.include?('same_last_name')
+    return false if entity.link_count >= 10
+    if match_values.include?('same_middle_name')
+      return false
+    else
+      return true
+    end
+  end
 end
 
+create_log = File.open(Rails.root.join('data', 'cmp_ids_to_create.csv').to_s, 'w')
 
 CSV.foreach(potential_matches_csv, headers: true) do |row|
   if row['match_values'].nil?
@@ -25,10 +32,11 @@ CSV.foreach(potential_matches_csv, headers: true) do |row|
   entity = Entity.find_by(id: row['match_id'])
   next if entity.nil?
 
-  if skip_entity?(entity, match_values)
+  if create_entity?(entity, match_values)
     # create new entity:
-    Cmp::Datasets.people[row['cmpid']].import!
-    Cmp::Datasets.people[row['cmpid']].clear_matches
+    create_log.puts row['cmpid']
+    # Cmp::Datasets.people[row['cmpid']].import!
+    # Cmp::Datasets.people[row['cmpid']].clear_matches
   else
     csv_line = CSV.generate_line([
                                    row['cmpid'],
@@ -47,3 +55,5 @@ CSV.foreach(potential_matches_csv, headers: true) do |row|
     print csv_line
   end
 end
+
+create_log.close
