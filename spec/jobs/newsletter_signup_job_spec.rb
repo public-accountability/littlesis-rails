86231fe256a152c6001312142abb025367c0a44f
ziggy <ziggy@littlesis.org>
2018-06-25 11:14:18 -0400

    refactor NewsletterSignupJob to handle pai signups
    add tests for `NewsletterSignupJob`

--- spec/jobs/newsletter_signup_job_spec.rb
+++ spec/jobs/newsletter_signup_job_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe NewsletterSignupJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe NewsletterSignupJob, type: :job do
+  include ActiveJob::TestHelper
+  let(:email_address) { Faker::Internet.email }
+  let(:user) { create_basic_user }
+
+  it 'creates a job' do
+    expect { NewsletterSignupJob.perform_later(email_address) }
+      .to have_enqueued_job.on_queue('default')
+  end
+
+  context 'submitting a User' do
+    it 'signups the user using #signup' do
+      expect(ActionNetwork).to receive(:signup).with(user).once
+      perform_enqueued_jobs { NewsletterSignupJob.perform_later(user) }
+    end
+  end
+
+  context 'Newsletter Signup' do
+    it 'signups the user using #add_email_to_newsletter' do
+      expect(ActionNetwork).to receive(:add_email_to_newsletter).with(email_address).once
+      perform_enqueued_jobs { NewsletterSignupJob.perform_later(email_address, 'newsletter') }
+    end
+  end
+
+  context 'PAI Signup' do
+    it 'signups the user using #add_email_to_pai' do
+      expect(ActionNetwork).to receive(:add_email_to_pai).with(email_address).once
+      perform_enqueued_jobs { NewsletterSignupJob.perform_later(email_address, 'pai') }
+    end
+  end
+
+  context 'errors' do
+    it 'rejects bad types' do
+      expect { perform_enqueued_jobs { NewsletterSignupJob.perform_later(5) } }
+        .to raise_error(TypeError)
+    end
+
+    it 'rejects invalid signup types' do
+      expect { perform_enqueued_jobs { NewsletterSignupJob.perform_later(email_address, 'alice-the-cat-fan-club') } }
+        .to raise_error(ArgumentError)
+    end
+  end
 end
