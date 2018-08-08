5723d651b39ca2e99b8bc14aa96588e10c8d3844
ziggy <ziggy@littlesis.org>
2018-08-08 15:51:19 +0000

    refactor `Chat` and add tests

--- app/utility/chat.rb
+++ app/utility/chat.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'net/http'
 
 class Chat
@@ -5,21 +7,26 @@ class Chat
   USERNAME = APP_CONFIG['chat']['admin_username']
   PASSWORD = APP_CONFIG['chat']['admin_password']
 
+  class RocketChatApiRequestFailedError < StandardError
+  end
+
   def initialize
     @admin_token = nil
     @admin_id = nil
   end
 
   def create_user(user)
-    return nil unless user.chatid.blank?
+    return nil if user.chatid.present?
     res = post '/api/v1/users.create', user_payload(user), auth_headers
-    if res && res.fetch('success') == true
-      mongo_chat_id = res['user']['_id']
-      user.update(chatid: mongo_chat_id)
-      Rails.logger.info "Created chat account for user #{user.id}. Mongo id: #{mongo_chat_id}"
-    else
-      Rails.logger.warn "Failed to create chat account for user #{user.id}"
-    end
+    res_success?(res)
+    mongo_chat_id = res['user']['_id']
+    user.update!(chatid: mongo_chat_id)
+  end
+
+  def login_token(mongo_id)
+    res = post '/api/v1/users.createToken', { userId: mongo_id }, auth_headers
+    res_success?(res)
+    { 'loginToken' => res['data']['authToken'] }
   end
 
   def info
@@ -44,9 +51,35 @@ class Chat
 
   ##  CLASS METHODS  ##
 
-  #  Str (Mongo ID of user) => Hash
-  # Creates new iframe token for the user, updates the mongo record, and returns a hash with the token
+  def self.admin_init
+    @chat.admin_logout if @chat.present?
+    @chat = new
+    @chat.admin_login
+  end
+
+  def self.create_user(user)
+    api_request { @chat.create_user(user) }
+  end
+
   def self.login_token(mongo_id)
+    api_request { @chat.login_token(mongo_id) }
+  end
+
+  def self.api_request(&block)
+    admin_init if @chat.blank?
+    begin
+      return block.call
+    rescue RocketChatApiRequestFailedError
+      Rails.logger.debug 'RocketChat API request failed. Re-trying'
+      admin_init
+      return block.call
+    end
+  end
+
+  #  Str (Mongo ID of user) => Hash
+  # Creates new iframe token for the user, updates the mongo record,
+  # and returns a hash with the token
+  def self.iframe_login_token(mongo_id)
     return nil if mongo_id.blank?
     mongo = mongo_client
     users = mongo['users']
@@ -64,7 +97,7 @@ class Chat
   private
 
   def auth_headers
-    {'X-Auth-Token' => @admin_token, 'X-User-Id' => @admin_id }
+    { 'X-Auth-Token' => @admin_token, 'X-User-Id' => @admin_id }
   end
 
   def user_payload(user)
@@ -100,6 +133,11 @@ class Chat
   # str -> <URI>
   def path_to_uri(path)
     URI("#{API_URL}#{path}")
+  end
+
+  def res_success?(res)
+    return true if res.present? && res.fetch('status', '') == 'success'
+    raise RocketChatApiRequestFailedError
   end
 
   # <NetResponce> -> json | nil
