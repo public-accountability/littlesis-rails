469a5261b8a6d49e17f5229dabf6a56a31bbf654
ziggy <ziggy@littlesis.org>
2018-08-14 21:45:02 +0000

    set bootsnap 'disable_trace' to false

--- config/boot.rb
+++ config/boot.rb
@@ -15,7 +15,7 @@ Bootsnap.setup(
   development_mode:     env == 'development', # Current working environment, e.g. RACK_ENV, RAILS_ENV, etc
   load_path_cache:      true,                 # Optimize the LOAD_PATH with a cache
   autoload_paths_cache: true,                 # Optimize ActiveSupport autoloads with cache
-  disable_trace:        true,                 # (Alpha) Set `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
+  disable_trace:        false,                # (Alpha) Set `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
   compile_cache_iseq:   true,                 # Compile Ruby code into ISeq cache, breaks coverage reporting.
   compile_cache_yaml:   false                 # Compile YAML into a cache
 )
