265a255f9e55e7e2d1a23af9eea647d1af3b7d2b
ziggy <ziggy@littlesis.org>
2018-08-22 14:21:06 +0000

    add module method Pagination.paginate

--- app/models/concerns/pagination.rb
+++ app/models/concerns/pagination.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 module Pagination
-  # count is optionalwe can normally infer it from records,
-  # but expose it as param in case SQL lookups are separate
   def paginate(page, per, records, count = nil)
+    Pagination.paginate(page, per, records, count)
+  end
+
+  # count is optional. we can normally infer it from records,
+  # but expose it as param in case SQL lookups are separate
+  def self.paginate(page, per, records, count = nil)
     Kaminari
       .paginate_array(records, total_count: count || records.count)
       .page(page)
