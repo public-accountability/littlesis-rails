c9887677cb788acf2bd90cc120012a6614d907a3
ziggy <ziggy@littlesis.org>
2018-09-18 19:06:17 +0000

    rubocop linting

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -18,7 +18,10 @@ module NavbarHelper
     class_name = dropdown ? 'nav-link dropdown-toggle' : 'nav-link'
     options = { 'class' => class_name, 'href' => href, 'id' => "navbar-header-#{text}" }
     if dropdown
-      options.merge!('role' => 'button', 'data-toggle' => 'dropdown','aria-haspopup' => 'true', 'aria-expanded' => 'false')
+      options.merge!('role' => 'button',
+                     'data-toggle' => 'dropdown',
+                     'aria-haspopup' => 'true',
+                     'aria-expanded' => 'false')
     end
     content_tag 'a', text, options
   end
