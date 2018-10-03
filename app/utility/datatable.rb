6a6ac6b2db512ecef64d9fa588ae194e48211765
ziggy <ziggy@littlesis.org>
2018-10-03 16:00:24 +0000

    Datatable::Response

--- app/utility/datatable.rb
+++ app/utility/datatable.rb
@@ -1,51 +1,63 @@
 # frozen_string_literal: true
 
 ##
-# Used to handle server-side rendering
-# of a datatable in Javascript.
+# module for datatable server-side processing
+#
 module Datatable
-
+  # Process datatable params into an object
+  # that can be used to query the database
+  #
+  # Example: Datatable::Request.new(params)
+  #
+  # see https://datatables.net/manual/server-side
+  # for the documentation on the format of a datatables request
   class Request
-    attr_reader :draw, :start, :length
+    attr_reader :draw, :start, :length, :search
 
     def initialize(params)
       %w[draw start length].each do |var|
         instance_variable_set "@#{var}", params.fetch(var).to_i
       end
 
+      @search = params.dig('search', 'value').presence
+
       freeze
     end
   end
 
+  # Generates datatables api response
+  # Use `.for` to create new classes
+  # for each model where a datatable
+  # Example:
+  #
+  #   + Response.for(:NyFiler).new(request)
+  #
   class Response
-  end
-
-  # Superclass for Datatable
-  # use .create to create new classes
-  # for each model where a datatabl
-  class Base
     class_attribute :model
+    attr_reader :json
+    define_method(:as_json) { @json }
 
-    def self.create(klass)
+    def self.for(klass)
       Class.new(self) do
-        self.model = klass
+        self.model = klass.to_s.constantize
       end
     end
 
-    def initalize(start:, length:, search: nil)
-      @model = self.class.model.to_s.constanize
-      @limit = start.to_i
-      @offset = length.to_i
-      @total_records = @model.count
+    def initalize(request)
+      @model = self.class.model
+      @request = request
+      @json = create_json
+      freeze
+    end
 
-      if search
-      else
-        @model.limit(@limit).offset(@offset)
-      end
+    private
+
+    def create_json
+      # @total_records = @model.count
+      # @model.limit(request.limit).offset(request.offset)
+      # {
+      #   draw: @request.draw,
+      # }
     end
   end
-
-  # NyFiler = Base.create(:NyFiler)
-  
-  
 end
--- spec/utility/datatable_spec.rb
+++ spec/utility/datatable_spec.rb
@@ -1,16 +1,49 @@
 require 'rails_helper'
 
+# rubocop:disable RSpec/NestedGroups
+
 describe Datatable do
   describe Datatable::Request do
     describe 'initialize' do
       subject(:request) { Datatable::Request.new(params) }
 
-      let(:params) { { 'draw' => '10', 'start' => '100', 'length' => '50' } }
+      let(:search) { nil }
+      let(:params) { { 'draw' => '10', 'start' => '100', 'length' => '50', 'search' => search } }
 
       specify { expect(request.draw).to eq 10 }
       specify { expect(request.start).to eq 100 }
       specify { expect(request.length).to eq 50 }
       specify { expect(request.frozen?).to be true }
+      specify { expect(request.search).to be nil }
+
+      context 'with search term' do
+        let(:search) { { 'value' => 'alice' } }
+
+        specify { expect(request.search).to eq 'alice' }
+      end
+
+      context 'with empty seach term' do
+        let(:search) { { 'value' => '' } }
+
+        specify { expect(request.search).to be nil }
+      end
+    end
+  end
+
+  describe Datatable::Response do
+    describe '.for' do
+      context 'when called with :NyFiler' do
+        subject { Datatable::Response.for(:NyFiler) }
+
+        it { is_expected.to be_a Class }
+        assert_attribute :superclass, Datatable::Response
+      end
+    end
+
+    describe 'handling request' do
+      
     end
   end
 end
+
+# rubocop:enable RSpec/NestedGroups
