f6f89ed94cd0c596b8ea58a4cde0a21a42e870c3
ziggy <ziggy@littlesis.org>
2018-10-08 17:21:20 +0000

    lint ny_filer_entity_spec

--- spec/models/ny_filer_entity_spec.rb
+++ spec/models/ny_filer_entity_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 describe NyFilerEntity, type: :model do
-  it { should belong_to(:ny_filer) }
-  it { should belong_to(:entity) }
-  it { should validate_presence_of(:ny_filer_id) }
-  it { should validate_presence_of(:entity_id) }
-  it { should validate_presence_of(:filer_id) }
-  it { should callback(:rematch_existing_matches).after(:create) }
+  it { is_expected.to belong_to(:ny_filer) }
+  it { is_expected.to belong_to(:entity) }
+  it { is_expected.to validate_presence_of(:ny_filer_id) }
+  it { is_expected.to validate_presence_of(:entity_id) }
+  it { is_expected.to validate_presence_of(:filer_id) }
+  it { is_expected.to callback(:rematch_existing_matches).after(:create) }
 
-  describe "associations" do
+  describe 'associations' do
     let(:elected) { create(:entity_person, name: 'Elected Representative') }
     let(:ny_filer) { create(:ny_filer, filer_id: 'A1') }
     let(:filer_entity) { NyFilerEntity.create!(entity: elected, ny_filer: ny_filer, filer_id: 'A1') }
 
-    it "belongs to entity" do
+    it 'belongs to entity' do
       expect(filer_entity.entity).to eql elected
-      expect(elected.ny_filer_entities.length).to eql 1
+      expect(elected.ny_filer_entities.length).to eq 1
       expect(elected.ny_filer_entities[0].id).to eq filer_entity.id
     end
 
-    it "belongs to ny_filer" do
+    it 'belongs to ny_filer' do
       expect(filer_entity.ny_filer).to eql ny_filer
-      expect(ny_filer.entities.length).to eql 1
+      expect(ny_filer.entities.length).to eq 1
       expect(ny_filer.ny_filer_entity.present?).to be true
       expect(ny_filer.entities[0].id).to eql elected.id
       expect(ny_filer.ny_filer_entity.id).to eql filer_entity.id
@@ -37,7 +37,8 @@ describe NyFilerEntity, type: :model do
       ny_disclosure = create(:ny_disclosure, ny_filer: ny_filer)
       NyMatch.create(ny_disclosure: ny_disclosure, donor_id: donor.id)
       expect(NyMatch.last.recip_id).to be nil
-      expect{NyFilerEntity.last.rematch_existing_matches_without_delay}.to change{Relationship.count}.by(1)
+      expect { NyFilerEntity.last.rematch_existing_matches_without_delay }
+        .to change { Relationship.count }.by(1)
       expect(NyMatch.last.recip_id).to eql elected.id
     end
   end
