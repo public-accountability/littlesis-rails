eed1e6ac48202e9de7de3b17017f2bb646f0ca70
ziggy <ziggy@littlesis.org>
2018-10-09 17:16:41 +0000

    lint requests/nys_spec.rb

--- spec/requests/nys_spec.rb
+++ spec/requests/nys_spec.rb
@@ -2,54 +2,62 @@ require 'rails_helper'
 
 describe 'NYS requests' do
   let(:user) { create_importer }
+
   before { login_as(user, scope: :user) }
-  after(:each) { logout(:user) }
+
+  after { logout(:user) }
 
   context 'as a basic user' do
     let(:user) { create_really_basic_user }
 
-    context 'match_donations' do
+    describe 'match_donations' do
       before do
-        post '/nys/match_donations', params: { payload: { disclosure_ids: [1,2,3], donor_id: 1 } }
+        post '/nys/match_donations', params: { payload: { disclosure_ids: [1, 2, 3], donor_id: 1 } }
       end
+
       denies_access
     end
 
-    context 'unmatch donations' do
+    describe 'unmatch donations' do
       before { post '/nys/unmatch_donations', params: { payload: { ny_match_ids: [1, 2, 3] } } }
+
       denies_access
     end
 
-    context 'potential contributions' do
+    describe 'potential contributions' do
       before { get '/nys/potential_contributions', params: { entity: 123 } }
+
       denies_access
     end
 
-    context 'contributions' do
+    describe 'contributions' do
       before { get '/nys/contributions', params: { entity: 123 } }
+
       denies_access
     end
   end
 
   describe 'match_donations' do
     let(:donor) { create(:entity_person).tap { |e| e.update_column(:updated_at, 1.year.ago) } }
-    let(:disclosures) { Array.new(2) { create(:ny_disclosure)} }
+    let(:disclosures) { Array.new(2) { create(:ny_disclosure) } }
     let(:match_donations) do
       proc do
         post '/nys/match_donations', params: { payload: { disclosure_ids: disclosures.map(&:id), donor_id: donor.id } }
       end
     end
 
-    specify { expect(&match_donations).to change { NyMatch.count }.by(2) }
+    specify { expect(&match_donations).to change(NyMatch, :count).by(2) }
     specify { expect(&match_donations).to change { donor.reload.updated_at } }
 
     specify do
       expect(&match_donations).to change { donor.reload.last_user_id }.to(user.sf_guard_user_id)
     end
 
-    context 'response' do
-      before { match_donations.call }
+    describe 'response' do
       subject { response }
+
+      before { match_donations.call }
+
       it { is_expected.to have_http_status(:accepted) }
     end
   end
@@ -64,12 +72,14 @@ describe 'NYS requests' do
       end
     end
 
-    specify { expect(&unmatch_donations).to change { NyMatch.count }.by(-2) }
+    specify { expect(&unmatch_donations).to change(NyMatch, :count).by(-2) }
 
-    context 'response' do
-      before { unmatch_donations.call }
+    describe 'response' do
       subject { response }
-      it { is_expected.to have_http_status(200) }
+
+      before { unmatch_donations.call }
+
+      it { is_expected.to have_http_status(:ok) }
     end
   end
 end
