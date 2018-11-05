b0d268bed3558d4a48108107b4423ec46cfa3aaa
ziggy <ziggy@littlesis.org>
2018-11-05 19:24:44 +0000

    lint new_entity_spec

--- spec/features/new_entity_spec.rb
+++ spec/features/new_entity_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
-feature '/entities/new', type: :feature do
+describe '/entities/new', type: :feature do
   let(:user) { create_basic_user }
-  before(:each) { login_as(user, scope: :user) }
-  after(:each) { logout(:user) }
 
-  context 'linked to add entity with page a name' do
+  before { login_as(user, scope: :user) }
+
+  after { logout(:user) }
+
+  describe 'linked to add entity with page a name' do
     let(:url) { '/entities/new?name=exxon' }
+
     before { visit url }
 
-    scenario 'adding new entity' do
+    it 'adding new entity' do
       successfully_visits_page url
       expect(find('#entity_name').value).to eql 'exxon'
     end
   end
-
 end
