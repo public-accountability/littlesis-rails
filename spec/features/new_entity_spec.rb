b830fc2791591550042b8d3db8acdcc5b2529cc2
ziggy <ziggy@littlesis.org>
2018-11-05 20:24:03 +0000

    add feature test for creating an new entity

--- spec/features/new_entity_spec.rb
+++ spec/features/new_entity_spec.rb
@@ -12,9 +12,30 @@ describe '/entities/new', type: :feature do
 
     before { visit url }
 
-    it 'adding new entity' do
+    it 'has name field already filled out' do
       successfully_visits_page url
       expect(find('#entity_name').value).to eql 'exxon'
+    end
+  end
+
+  describe 'creating a new entity' do
+    before { visit '/entities/new' }
+
+    let(:name) { Faker::Name.name }
+    let(:blurb) { Faker::Shakespeare.hamlet_quote }
+
+    it 'can create a new person' do
+      successfully_visits_page '/entities/new'
+
+      fill_in 'entity_name', with: name
+      fill_in 'entity_blurb', with: blurb
+      choose 'Person'
+      click_button 'Add'
+
+      expect(page.status_code).to eq 200
+      expect(Entity.last.name).to eql name
+      expect(page.current_path).to include '/person/'
+      expect(page.current_path).to include '/edit'
     end
   end
 end
