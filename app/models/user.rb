4295ebeb1058b7c8a1d34625d90a10ecc12f633d
ziggy <ziggy@littlesis.org>
2018-11-05 15:59:18 +0000

    remove `Note`, note functionality from `User`, and note index

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,10 +36,6 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users, inverse_of: :users
   has_many :campaigns, through: :groups, inverse_of: :users
 
-  has_many :notes, foreign_key: "new_user_id", inverse_of: :user
-
-  has_many :note_users, inverse_of: :user, dependent: :destroy
-  has_many :received_notes, class_name: "Note", through: :note_users, source: :note, inverse_of: :recipients
   has_many :network_maps, primary_key: "sf_guard_user_id"
 
   has_many :lists, foreign_key: "creator_user_id", inverse_of: :user
@@ -73,28 +69,6 @@ class User < ApplicationRecord
   def legacy_created_at
   	return created_at if sf_guard_user.nil?
   	sf_guard_user.created_at
-  end
-
-  def notes_with_replies
-		Note.with_joins
-	    .where("note.new_user_id = ? OR users.id = ?", id, id)
-  end
-
-  def notes_with_replies_visible_to_user(user)
-  	return notes_with_replies.public if user.nil?
-  	notes_with_replies.where("note.is_private = ? OR users.id = ?", false, user.id)
-  end
-
-  def notes_visible_to_user(user)
-  	return notes.public.order("note.created_at DESC") if user.nil?
-  	notes.with_joins
-  		.where("note.is_private = ? OR users.id = ?", false, user.id)
-  end
-
-  def received_notes_visible_to_user(user)
-  	return received_notes.public.order("note.created_at DESC") if user.nil?
-  	received_notes.with_joins
-  		.where("note.is_private = ? OR users.id = ?", false, user.id)
   end
 
   def show_full_name?
