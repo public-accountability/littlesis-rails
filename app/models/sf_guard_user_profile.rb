e90e2ea7a153e853ec4caad4a7bc0b38bb8920b9
ziggy <ziggy@elephant-bird.net>
2018-11-20 11:46:04 -0500

    remove bio from admin user overview page

--- app/models/sf_guard_user_profile.rb
+++ app/models/sf_guard_user_profile.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class SfGuardUserProfile < ApplicationRecord
-  include SingularTable	
+  include SingularTable
+
+  belongs_to :sf_guard_user, inverse_of: :sf_guard_user_profile, foreign_key: 'user_id'
 end
