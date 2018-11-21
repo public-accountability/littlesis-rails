7e92abdd132fc79931a7b42f2e34632dc021c12d
ziggy <ziggy@elephant-bird.net>
2018-11-21 11:38:17 -0500

    replace references to sf profile in mailers
    + add test for user_mailer
    + refactor notification mailer specs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
-class UserMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome to LittleSis!')
   end
-
 end
