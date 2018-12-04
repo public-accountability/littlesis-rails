095764c85b00b1f60553be261c0d74b48be6d95c
ziggy <ziggy@littlesis.org>
2018-12-04 12:54:45 -0500

    DashboardBulletin#color

--- app/models/dashboard_bulletin.rb
+++ app/models/dashboard_bulletin.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class DashboardBulletin < ApplicationRecord
-  DEFAULT_COLOR = 'rgba(0, 0, 0, 0.03)'.html_safe.freeze
+  DEFAULT_COLOR = 'rgba(0, 0, 0, 0.03)'
 
   default_scope { order(created_at: :desc) }
+
+  def color
+    self[:color].presence || DEFAULT_COLOR
+  end
 
   def self.last_bulletin_updated_at
     DashboardBulletin.reorder('updated_at desc').limit(1).pluck('updated_at').first
--- spec/models/dashboard_bulletin_spec.rb
+++ spec/models/dashboard_bulletin_spec.rb
@@ -5,4 +5,24 @@ describe DashboardBulletin, type: :model do
   it { is_expected.to have_db_column(:title) }
   it { is_expected.to have_db_column(:color).of_type(:string) }
   it { is_expected.to have_db_index(:created_at) }
+
+  describe '#color' do
+    context 'with non-empty color field' do
+      subject(:bulletin) { DashboardBulletin.new(color: '#ccc') }
+
+      specify { expect(bulletin.color).to eq '#ccc' }
+    end
+
+    context 'with empty color field' do
+      subject(:bulletin) { DashboardBulletin.new }
+
+      specify { expect(bulletin.color).to eq 'rgba(0, 0, 0, 0.03)' }
+    end
+
+    context 'with blank string as color field' do
+      subject(:bulletin) { DashboardBulletin.new(color: '') }
+
+      specify { expect(bulletin.color).to eq 'rgba(0, 0, 0, 0.03)' }
+    end
+  end
 end
