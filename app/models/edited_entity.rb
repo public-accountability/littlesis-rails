404d8dce4eeff9245b5418e0b2dcb2345bcbf436
ziggy <ziggy@littlesis.org>
2019-01-02 16:30:13 -0500

    test unique constraint on entity_id/version_id

--- app/models/edited_entity.rb
+++ app/models/edited_entity.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class EditedEntity < ApplicationRecord
-  belongs_to :entity
-  belongs_to :version, class_name: 'PaperTrail::Version', foreign_key: 'version_id'
-  belongs_to :user
+  belongs_to :entity, optional: true
+  belongs_to :version, class_name: 'PaperTrail::Version', foreign_key: 'version_id', optional: true
+  belongs_to :user, optional: true
 
   validates :entity_id, presence: true
   validates :version_id, presence: true
--- spec/models/edited_entity_spec.rb
+++ spec/models/edited_entity_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 describe EditedEntity, type: :model do
+  let(:entity_id) { Faker::Number.unique.number(8).to_i }
+  let(:version_id) { Faker::Number.unique.number(8).to_i }
+  let(:user_id) { Faker::Number.unique.number(4).to_i }
+  let(:created_at) { Faker::Date.backward(100) }
+
   it { is_expected.to have_db_column(:user_id).of_type(:integer) }
   it { is_expected.to have_db_column(:version_id).of_type(:integer) }
   it { is_expected.to have_db_column(:entity_id).of_type(:integer) }
@@ -14,4 +19,31 @@ describe EditedEntity, type: :model do
   it { is_expected.to validate_presence_of(:version_id) }
   it { is_expected.to validate_presence_of(:created_at) }
   it { is_expected.not_to validate_presence_of(:user_id) }
+
+  describe 'unique verison_id/entity_id' do
+    before do
+      EditedEntity.create!(entity_id: entity_id,
+                           version_id: version_id,
+                           user_id: user_id,
+                           created_at: created_at)
+    end
+
+    it 'raises error when encouters duplicate version_id/entity_id combo' do
+      expect do
+        EditedEntity.create!(entity_id: entity_id,
+                             version_id: version_id,
+                             user_id: rand(10_000),
+                             created_at: Time.current)
+      end.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+
+    it 'permits creations when version id is different' do
+      expect do
+        EditedEntity.create!(entity_id: entity_id,
+                             version_id: Faker::Number.unique.number(8).to_i,
+                             user_id: rand(10_000),
+                             created_at: Time.current)
+      end.not_to raise_error
+    end
+  end
 end
