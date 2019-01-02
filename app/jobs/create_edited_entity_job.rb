9535c7dce522b53e961592ff8f5589029a40839c
ziggy <ziggy@littlesis.org>
2019-01-02 16:58:11 -0500

    revise create edited entity job to accept `PaperTrail::Version`

--- app/jobs/create_edited_entity_job.rb
+++ app/jobs/create_edited_entity_job.rb
@@ -3,7 +3,18 @@
 class CreateEditedEntityJob < ApplicationJob
   queue_as :default
 
-  def perform(attributes)
-    EditedEntity.create!(attributes)
+  def perform(version)
+    TypeCheck.check version, PaperTrail::Version
+    return if version.entity1_id.nil?
+
+    base_attributes = { user_id: version.whodunnit&.to_i,
+                        version_id: version.id,
+                        created_at: version.created_at }
+
+    EditedEntity.create! base_attributes.merge(entity_id: version.entity1_id)
+
+    if version.entity2_id.present? && (version.entity2_id != version.entity1_id)
+      EditedEntity.create! base_attributes.merge(entity_id: version.entity2_id)
+    end
   end
 end
--- spec/jobs/create_edited_entity_job_spec.rb
+++ spec/jobs/create_edited_entity_job_spec.rb
@@ -3,15 +3,39 @@ require 'rails_helper'
 describe CreateEditedEntityJob, type: :job do
   include ActiveJob::TestHelper
 
-  let(:attributes) do
-    { entity_id: rand(10_000),
-      version_id: rand(10_000),
-      user_id: rand(10_000),
-      created_at: Time.current }
+  let(:entity_version) { create(:entity_version) }
+  let(:relationship_version) { create(:relationship_version) }
+
+  it 'Entity Version: calls EditedEntity.create! with correct attributes' do
+    expect(EditedEntity).to receive(:create!)
+                              .with(user_id: entity_version.whodunnit.to_i,
+                                    version_id: entity_version.id,
+                                    entity_id: entity_version.entity1_id,
+                                    created_at: entity_version.created_at)
+                              .once
+
+    perform_enqueued_jobs do
+      CreateEditedEntityJob.perform_later(entity_version)
+    end
   end
 
-  it 'calls EditedEntity.create! with correct attributes' do
-    expect(EditedEntity).to receive(:create!).with(attributes).once
-    perform_enqueued_jobs { EditedEntity.create!(attributes) }
+  it 'Relationship verison: creates two Edited Entities' do
+    expect(EditedEntity).to receive(:create!)
+                              .with(user_id: relationship_version.whodunnit.to_i,
+                                    version_id: relationship_version.id,
+                                    entity_id: relationship_version.entity1_id,
+                                    created_at: relationship_version.created_at)
+                              .once
+
+    expect(EditedEntity).to receive(:create!)
+                              .with(user_id: relationship_version.whodunnit.to_i,
+                                    version_id: relationship_version.id,
+                                    entity_id: relationship_version.entity2_id,
+                                    created_at: relationship_version.created_at)
+                              .once
+
+    perform_enqueued_jobs do
+      CreateEditedEntityJob.perform_later(relationship_version)
+    end
   end
 end
