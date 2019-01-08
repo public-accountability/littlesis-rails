d220fb6ee4de30328d9578a197a2e3e285f1b276
ziggy <ziggy@littlesis.org>
2019-01-08 13:32:33 -0500

    update development helper script: create_example_user.rb

--- lib/scripts/create_example_user.rb
+++ lib/scripts/create_example_user.rb
@@ -4,17 +4,13 @@ unless Rails.env.development?
   raise "#{__FILE__} should only be run in the development environment"
 end
 
-
-
 email = Faker::Internet.email
 password = 'password'
 
 sf_user = FactoryBot.create(:sf_guard_user)
 user = FactoryBot.create(:user, sf_guard_user_id: sf_user.id, email: email)
 user.reset_password(password, password)
-SfGuardUserPermission.create!(permission_id: 2, user_id: sf_user.id)
-SfGuardUserPermission.create!(permission_id: 3, user_id: sf_user.id)
-SfGuardUserPermission.create!(permission_id: 6, user_id: sf_user.id)
+user.add_ability!(:edit, :list)
 
 ColorPrinter.print_blue <<-MSG
 
