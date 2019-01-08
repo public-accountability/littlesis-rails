ec3bb9a72ae4136a44f037d4656a7c7ada0a248f
ziggy <ziggy@littlesis.org>
2019-01-08 15:57:16 -0500

    last entity presenter uses EditedEntity table

--- spec/presenters/entity_last_editor_presenter_spec.rb
+++ spec/presenters/entity_last_editor_presenter_spec.rb
@@ -7,33 +7,32 @@ describe EntityLastEditorPresenter do
 
   let(:person_time) { Time.current }
   let(:version_time) { Time.current }
+  let(:last_edited_at) { version_time }
+
   let(:user1) { build(:user_with_id) }
   let(:user2) { build(:user_with_id) }
-
-  let(:version) do
-    build(:entity_version, created_at: version_time, whodunnit: user1.id.to_s)
-  end
 
   let(:person) do
     build(:person, updated_at: person_time, last_user_id: user2.sf_guard_user_id)
   end
 
-  before do
-    allow(person).to receive(:last_user).and_return(double(user: user2))
-    allow(EntityHistory).to receive(:new)
-                              .with(person)
-                              .and_return(double(:versions => double(:at => version)))
+  let(:edited_enity) do
+    build(:edited_entity, user: user1, created_at: last_edited_at)
   end
 
-  context 'when version is lastest edit' do
+  before do
+    allow(person).to receive(:last_user).and_return(double(user: user2))
+
+    allow(EditedEntity).to receive(:order)
+                             .with(created_at: :desc)
+                             .and_return(double(:find_by => edited_enity))
+  end
+
+  context 'when edited entity is lastest edit' do
     let(:person_time) { 1.hour.ago }
 
-    before do
-      expect(User).to receive(:find_by).with(id: user1.id.to_s).and_return(user1)
-    end
-
     specify { expect(subject.last_editor).to eq user1 }
-    specify { expect(subject.last_edited_at.change(usec: 0)).to eq version_time.change(usec: 0) }
+    specify { expect(subject.last_edited_at.change(usec: 0)).to eq last_edited_at.change(usec: 0) }
 
     describe 'last_edited_link' do
       subject { EntityLastEditorPresenter.new(person).html }
@@ -51,30 +50,23 @@ describe EntityLastEditorPresenter do
     end
   end
 
-  context 'when version is the lastest edit, and user is missing' do
+  context 'when edited entity is the lastest edit, and user is missing' do
     let(:person_time) { 1.hour.ago }
 
-    before do
-      expect(User).to receive(:find_by).with(id: user1.id.to_s).and_return(nil)
+    let(:edited_enity) do
+      build(:edited_entity, user: nil, created_at: last_edited_at)
     end
 
     specify { expect(subject.last_editor).to eq User.system_user }
   end
 
-  context 'when version is at same time as updated_at' do
-    before do
-      expect(User).to receive(:find_by).with(id: user1.id.to_s).and_return(user1)
-    end
-
+  context 'when edited entity is at same time as updated_at' do
     specify { expect(subject.last_editor).to eq user1 }
     specify { expect(subject.last_edited_at.change(usec: 0)).to eq version_time.change(usec: 0) }
   end
 
-  context 'when updated_at is latest edit' do
+  context 'when updated_at is for latest edit' do
     let(:version_time) { 1.hour.ago }
-
-    before { expect(User).not_to receive(:find_by) }
-
     specify { expect(subject.last_editor).to eq User.system_user }
     specify { expect(subject.last_edited_at.change(usec: 0)).to eq person_time.change(usec: 0) }
   end
