01f638efdaca060b1eba3b2ba9200a1d35336e33
ziggy <ziggy@littlesis.org>
2019-01-22 14:14:03 -0500

    lint extension_definition_spec.rb

--- spec/models/extension_definition_spec.rb
+++ spec/models/extension_definition_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
-describe ExtensionDefinition, type: :model  do
-  it { should have_many(:extension_records) }
-  it { should have_many(:entities) }
+describe ExtensionDefinition, type: :model do
+  it { is_expected.to have_many(:extension_records) }
+  it { is_expected.to have_many(:entities) }
 
-  it 'should have ID constants' do
-    expect(ExtensionDefinition::PERSON_ID).to eql 1
-    expect(ExtensionDefinition::ORG_ID).to eql 2
+  describe 'ID constants' do
+    specify { expect(ExtensionDefinition::PERSON_ID).to eq 1 }
+    specify { expect(ExtensionDefinition::ORG_ID).to eq 2 }
   end
 
   describe 'person types & org types' do
     it 'returns all person types' do
-      expect(ExtensionDefinition).to receive(:where).with(parent_id: 1).once.and_return(double(order: {name: :asc}))
+      expect(ExtensionDefinition).to receive(:where)
+                                       .with(parent_id: 1)
+                                       .once
+                                       .and_return(double(order: { name: :asc }))
       ExtensionDefinition.person_types
     end
 
     it 'returns all org types' do
-      expect(ExtensionDefinition).to receive(:where).with(parent_id: 2).once.and_return(double(order: {name: :asc}))
+      expect(ExtensionDefinition).to receive(:where)
+                                       .with(parent_id: 2).once
+                                       .and_return(double(order: { name: :asc }))
       ExtensionDefinition.org_types
     end
   end
@@ -37,7 +42,7 @@ describe ExtensionDefinition, type: :model  do
     it 'returns a memozined hash map' do
       expect(ExtensionDefinition.display_names).to be_a Hash
       expect(ExtensionDefinition.display_names.keys.to_set).to eql ExtensionDefinition.all.map(&:id).to_set
-      expect(ExtensionDefinition.display_names.fetch(26)).to eql  "Political Party"
+      expect(ExtensionDefinition.display_names.fetch(26)).to eq 'Political Party'
     end
   end
 end
