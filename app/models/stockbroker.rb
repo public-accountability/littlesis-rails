9722c1df17859c77b4ade4af8914748d9b2cf752
ziggy <ziggy@littlesis.org>
2019-01-22 15:49:23 -0500

    add has_paper_trail to `Stockbroker`

--- app/models/stockbroker.rb
+++ app/models/stockbroker.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Stockbroker < ApplicationRecord
+  has_paper_trail on: [:update],
+                  meta: { entity1_id: :entity_id }
+
   belongs_to :entity, inverse_of: :stockbroker
 end
