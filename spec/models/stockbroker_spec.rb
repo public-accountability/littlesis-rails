17c25001e5c2100b4cd04f8e9f763d1f8793ddcc
ziggy <ziggy@littlesis.org>
2019-01-22 15:31:32 -0500

    add tests for creating stockbrokers

--- spec/models/stockbroker_spec.rb
+++ spec/models/stockbroker_spec.rb
@@ -6,4 +6,26 @@ describe Stockbroker, type: :model do
   it { is_expected.to have_db_column(:entity_id).of_type(:integer) }
   it { is_expected.to have_db_column(:crd_number).of_type(:integer) }
   it { is_expected.to belong_to(:entity) }
+
+  describe 'creating a stockbroker' do
+    let(:crd_number) { rand(10_000) }
+
+    it 'can be added to an org' do
+      entity = create(:entity_org)
+      expect { entity.add_extension('Stockbroker') }
+        .to change(Stockbroker, :count).by(1)
+    end
+
+    it 'can be added to an person' do
+      entity = create(:entity_person)
+      expect { entity.add_extension('Stockbroker') }
+        .to change(Stockbroker, :count).by(1)
+    end
+
+    it 'can have crd number' do
+      entity = create(:entity_person)
+      entity.add_extension('Stockbroker', crd_number: crd_number)
+      expect(Stockbroker.last.crd_number).to eq crd_number
+    end
+  end
 end
