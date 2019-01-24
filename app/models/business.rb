223cd37c698a63ea56cf77d037841cdfb5406f37
ziggy <ziggy@littlesis.org>
2019-01-24 12:21:04 -0500

    Business#with_crd_number

--- app/models/business.rb
+++ app/models/business.rb
@@ -7,4 +7,8 @@ class Business < ApplicationRecord
                   meta: { entity1_id: :entity_id }
 
   belongs_to :entity, inverse_of: :business
+
+  def self.with_crd_number
+    where(arel_table[:crd_number].not_eq(nil))
+  end
 end
