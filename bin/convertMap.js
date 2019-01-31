11082a837973153f256cb9ee04d1d912ac2a8980
ziggy <ziggy@littlesis.org>
2019-01-31 13:17:05 -0500

    add yarn testing to travis
    * remove unused configuration files and outdated examples
    * cache bundler & yarn

--- bin/convertMap.js
+++
@@ -1,4 +0,0 @@
-var mapData = require('../tmp/mapData.json');
-var LsDataConverter = require('../vendor/assets/javascripts/oli2/LsDataConverter.js');
-var graphData = LsDataConverter.convertMapData(mapData);
-console.log(JSON.stringify(graphData));
