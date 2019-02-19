ce755afd9c2497f163a9e5dbcc9b75811977c8d0
ziggy <ziggy@littlesis.org>
2019-02-19 18:10:34 -0500

    request spec for cropping an image

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -55,6 +55,51 @@ describe Images do
       post "/images/deny_deletion/#{image_deletion_request.id}"
       expect(response).to have_http_status :forbidden
     end
+
+    describe 'cropping an image' do
+      let(:person) { create(:entity_person) }
+      let(:image) { create(:image, is_featured: true, entity: person, width: 1200, height: 900) } 
+
+      let(:params) do
+        { 'crop' => {
+            'type' => 'original',
+            'ratio' => 2.0,
+            'x' => 100,
+            'y' => 100,
+            'w' => 300,
+            'h' => 125
+          } }
+      end
+
+      let(:crop_request) do
+        lambda do
+          post "/images/#{image.id}/crop", params: params, as: :json
+        end
+      end
+
+      before do
+        test_image_path = Rails.root.join('spec', 'testdata', '1200x900.png').to_s
+        FileUtils.mkdir_p image.image_file('original').pathname.dirname
+        FileUtils.cp(test_image_path, image.image_file('original').path)
+      end
+
+      it 'returns json with redirect to url' do
+        crop_request.call
+        expect(response).to have_http_status :created
+        expect(json).to eq({ 'url' => "#{Routes.entity_path(person)}/images" })
+      end
+
+      it 'replaces images' do
+        entity = Entity.find(person.id)
+        expect(entity.images.count).to eq 1
+        expect(entity.featured_image.width).to eq 1200
+        expect(entity.featured_image.height).to eq 900
+        crop_request.call
+        expect(entity.reload.images.count).to eq 1
+        expect(entity.featured_image.width).to eq 600
+        expect(entity.featured_image.height).to eq 250
+      end
+    end
   end
 
   context 'as an admin' do
