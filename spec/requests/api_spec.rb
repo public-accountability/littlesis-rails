aeda5712524d62fb9f7c20f25f92ee0084a1960b
ziggy <ziggy@littlesis.org>
2019-02-21 12:43:50 -0500

    signed in users can make API requests without a token

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -15,6 +15,31 @@ describe Api, :pagination_helper do
 
   let(:meta) { Api::META }
 
+  describe 'access control' do
+    let(:api_request) do
+      -> { get api_entity_path(create(:entity_person)) }
+    end
+
+    context 'when no api token provided' do
+      before { api_request.call }
+
+      specify { expect(response).to have_http_status :unauthorized }
+    end
+
+    context 'when no api token provided, but user is logged in' do
+      let(:user) { create_really_basic_user }
+
+      before do
+        login_as(user, :scope => :user)
+        api_request.call
+      end
+
+      after { logout(:user) }
+
+      specify { expect(response).to have_http_status :ok }
+    end
+  end
+
   describe 'entities' do
     let(:lawyer) do
       create(:entity_person).tap { |e| e.add_extension('Lawyer') }
