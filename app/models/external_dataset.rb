6433d610f6e8333fccabd1dc72a3dd80f1354810
ziggy <ziggy@littlesis.org>
2019-02-25 14:47:07 -0500

    validate dataset name and use JSON as serializer

--- app/models/external_dataset.rb
+++ app/models/external_dataset.rb
@@ -1,25 +1,9 @@
 # frozen_string_literal: true
 
-=begin
-
-id         |
-updated_at |
-created_at |
-name       | string         | name of the dataset
-row_data   | longtext(json) | contains the actual "data" as json object
-row_id     | 
-matched    | boolean        | if the row has been matched yet 
-match_data | longtext(json) | pre-calculated match data
-
-
-
-=end
-
-
 class ExternalDataset < ApplicationRecord
+  DATASETS = %w[iapd].freeze
+  validates :name, inclusion: { in: DATASETS }
 
-  
+  serialize :row_data, JSON
+  serialize :match_data, JSON
 end
-
-
-
