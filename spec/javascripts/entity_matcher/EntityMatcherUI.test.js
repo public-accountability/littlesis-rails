4c90e2a372ae6c7ebb6a3b8a5873789206cfdce6
ziggy <ziggy@littlesis.org>
2019-02-28 12:10:04 -0500

    mock api function call with jest; use `test.todo`

--- spec/javascripts/entity_matcher/EntityMatcherUI.test.js
+++ spec/javascripts/entity_matcher/EntityMatcherUI.test.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+jest.mock('packs/entity_matcher/api_client',
+	  () => ({ retriveDatasetRow: jest.fn() }));
+
+import { retriveDatasetRow } from 'packs/entity_matcher/api_client';
+
 import EntityMatcherUI from 'packs/entity_matcher/EntityMatcherUI';
+
 
 describe('EntityMatcherUI', () => {
   const testDatasetFields = ["one", "two"];
+  
+  retriveDatasetRow.mockImplementation( () => {
+    return Promise.resolve({"one": "foo", "two": "bar"});
+  });
+
   const wrapper = shallow(<EntityMatcherUI datasetFields={testDatasetFields} itemId={2}/>);
 
   describe('mounting', () => {
-    it('sets default state');
-    it('calls loadItemInfo after mounting');
+    test.todo('sets default state');
+    test.todo('calls loadItemInfo after mounting');
   });
 
   describe('updateState', () => {
-    it('recursively merges objects');
-    it('can be called with two arguments: key, value');
+    test.todo('recursively merges objects');
+    test.todo('can be called with two arguments: key, value');
   });
 
   describe('Loading itemInfo', () => {
-    it('shows loading spinner while it is loaded');
-    it('hides loading spinner after it is loading');
-    it('shows itemInfo after loading');
-    it('shows LoadingError if error occurs');
+    test.todo('shows loading spinner while it is loaded');
+    test.todo('hides loading spinner after it is loading');
+    test.todo('shows itemInfo after loading');
+    test.todo('shows LoadingError if error occurs');
   });
 
 });
