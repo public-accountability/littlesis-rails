2a2cc4c8dd4db3bad0ddc82738a70e0719e29619
ziggy <ziggy@littlesis.org>
2019-03-05 17:37:39 -0500

    add entity matcher fixtures file

--- spec/javascripts/entity_matcher/PotentialMatches.test.js
+++ spec/javascripts/entity_matcher/PotentialMatches.test.js
@@ -7,33 +7,10 @@ import PotentialMatchesHeader from 'packs/entity_matcher/components/PotentialMat
 import PotentialMatchesSearch from 'packs/entity_matcher/components/PotentialMatchesSearch';
 import PotentialMatchesList from 'packs/entity_matcher/components/PotentialMatchesList';
 
-describe('PotentialMatches', () => {
+import { exampleMatch } from './fixtures';
 
-  const exampleMatch = {
-    "entity": {
-      "id": 109722,
-      "name": "Joy Pollock",
-      "blurb": null,
-      "summary": null,
-      "website": null,
-      "parent_id": null,
-      "primary_ext": "Person",
-      "created_at": "2012-12-09T14:05:18.000Z",
-      "updated_at": "2012-12-09T14:05:18.000Z",
-      "start_date": null,
-      "end_date": null,
-      "is_current": null,
-      "is_deleted": false,
-      "merged_id": null,
-      "link_count": 1,
-      "url": "http://localhost:8080/person/109722-Joy_Pollock",
-      "image_url": "/images/system/anon.png"
-    },
-    "values": [ "same_last_name", "mismatched_middle_name", "similar_last_name", "similar_first_name", "common_last_name" ],
-    "ranking": 63,
-    "automatch": false
-  };
-  
+describe('PotentialMatches', () => {
+    
   test('has div with correct id', () => {
     const wrapper = shallow(<PotentialMatches />);
     expect(wrapper.find('div#potential-matches').exists()).toBe(true);
