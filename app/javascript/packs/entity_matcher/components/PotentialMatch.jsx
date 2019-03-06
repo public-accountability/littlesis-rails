aea42cce43b71e2c06eba06dad6474e3d5ced193
ziggy <ziggy@littlesis.org>
2019-03-06 10:40:47 -0500

    simple entity card style

--- app/javascript/packs/entity_matcher/components/PotentialMatch.jsx
+++ app/javascript/packs/entity_matcher/components/PotentialMatch.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Image = ({entity}) => {
-  return <img
-           src={entity.image_url}
-           alt={`Image of LittleSis entity: ${entity.name}`}
-           className="potential-match-image"
-         />;
+  return <div className="potential-match-entity-image-wrapper">
+           <img
+             src={entity.image_url}
+             alt={`Image of LittleSis entity: ${entity.name}`}
+           />
+         </div>;
 
 };
 
 const Entity = ({entity}) => {
 
   return <div className="potential-match-entity">
-           <a className="potential-match-entity-link" href={entity.url} ><h1>{entity.name}</h1></a>
+           <a href={entity.url} ><h1>{entity.name}</h1></a>
            <p className="potential-match-entity-blurb">{entity.blurb}</p>
          </div>;
   
