d0dd152a16112c5681d2f7c08e24528786b4fc83
ziggy <ziggy@littlesis.org>
2019-03-11 11:45:31 -0400

    ExternalDatasetService::Iapd is now a module

--- app/services/external_dataset_service.rb
+++ app/services/external_dataset_service.rb
@@ -2,24 +2,25 @@
 
 module ExternalDatasetService
   class InvalidMatchError < Exceptions::LittleSisError; end
-  
+
   # For IAPD it:
   #   * adds business or business person extension
   #   * adds crd number
-  class Iapd < SimpleDelegator
-    def validate_match!(entity_or_entity_id)
-      entity = Entity.entity_for(entity_or_entity_id)
+  module Iapd
+    def self.validate_match!(entity:, external_dataset:)
+      entity = Entity.entity_for(entity)
       extension = entity.org? ? :business : :business_person
 
       if entity.public_send(extension)&.crd_number&.present?
-        raise InvalidMatchError, "Entity #{entity.id} already has a crd_number. Cannot match row#{id}."
+        msg = "Entity #{entity.id} already has a crd_number. Cannot match row#{external_dataset.id}"
+        raise InvalidMatchError, msg
       end
     end
 
-    def match(entity_or_entity_id)
+    def self.match(entity_or_entity_id)
     end
 
-    def unmatch
+    def self.unmatch
     end
   end
 
--- spec/services/external_dataset_service_spec.rb
+++ spec/services/external_dataset_service_spec.rb
@@ -7,31 +7,36 @@ describe ExternalDatasetService do
   describe 'Iapd' do
     describe '#validate_match!' do
       let(:external_dataset) { build(:external_dataset, entity_id: nil) }
-      let(:external_dataset_service) { ExternalDatasetService::Iapd.new(external_dataset) }
+      # let(:external_dataset_service) { ExternalDatasetService::Iapd.new(external_dataset) }
       let(:crd_number) { Faker::Number.unique.number(5).to_i }
       let(:person) { create(:entity_person) }
       let(:org) { create(:entity_org) }
 
       it 'raises error if person has crd number' do
         person.add_extension('BusinessPerson', crd_number: crd_number)
-        expect { external_dataset_service.validate_match!(person) }
+        expect { ExternalDatasetService::Iapd.validate_match!(entity: person, external_dataset: external_dataset) }
           .to raise_error(ExternalDatasetService::InvalidMatchError)
       end
 
       it 'rasies error if org has a crd number' do
         org.add_extension('Business', crd_number: crd_number)
-        expect { external_dataset_service.validate_match!(org) }
+        expect { ExternalDatasetService::Iapd.validate_match!(entity: org, external_dataset: external_dataset) }
           .to raise_error(ExternalDatasetService::InvalidMatchError)
       end
-      
+
       it 'ok if business does not have a crd number' do
         org.add_extension('Business')
-        expect { external_dataset_service.validate_match!(org) }.not_to raise_error
+        expect { ExternalDatasetService::Iapd.validate_match!(entity: org, external_dataset: external_dataset) }
+          .not_to raise_error
       end
 
       it 'ok if person does not have a crd number' do
-        expect { external_dataset_service.validate_match!(person) }.not_to raise_error
+        expect { ExternalDatasetService::Iapd.validate_match!(entity: person, external_dataset: external_dataset) }
+          .not_to raise_error
       end
+    end
+
+    describe '#match' do
     end
   end
 end
