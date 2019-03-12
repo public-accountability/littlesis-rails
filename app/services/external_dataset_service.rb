62df99b5b0995269c69829ca0e52a80dae70576e
ziggy <ziggy@littlesis.org>
2019-03-12 10:20:15 -0400

    test for matching iapd dataset

--- app/services/external_dataset_service.rb
+++ app/services/external_dataset_service.rb
@@ -17,10 +17,32 @@ module ExternalDatasetService
       end
     end
 
-    def self.match(entity_or_entity_id)
+    def self.match(entity:, external_dataset:)
+      entity = Entity.entity_for(entity)
+      extension = entity.org? ? 'Business' : 'BusinessPerson'
+      crd_number = external_dataset.row_data['OwnerID']
+
+      ApplicationRecord.transaction do
+        if crd_number?(crd_number)
+          if entity.has_extension?(extension)
+            entity.merge_extension extension, crd_number: crd_number.to_i
+          else
+            entity.add_extension extension, crd_number: crd_number.to_i
+          end
+        else
+          entity.add_extension extension
+        end
+        external_dataset.update! entity_id: entity.id
+      end
     end
 
     def self.unmatch
+    end
+
+    def self.crd_number?(crd)
+      return false if crd.blank? || crd.include?('-')
+
+      /\A\d+\z/.match?(crd)
     end
   end
 
--- spec/services/external_dataset_service_spec.rb
+++ spec/services/external_dataset_service_spec.rb
@@ -31,12 +31,81 @@ describe ExternalDatasetService do
       end
 
       it 'ok if person does not have a crd number' do
+        person
         expect { ExternalDatasetService::Iapd.validate_match!(entity: person, external_dataset: external_dataset) }
           .not_to raise_error
       end
     end
 
+    describe 'crd_number?' do
+      it 'returns false for blank strings' do
+        expect(ExternalDatasetService::Iapd.crd_number?('')).to be false
+      end
+
+      it 'returns false for spring with "-"' do
+        not_a_crd_number = '23-2696041'
+        expect(ExternalDatasetService::Iapd.crd_number?(not_a_crd_number)).to be false
+      end
+
+      it 'returns false for random strings' do
+        expect(ExternalDatasetService::Iapd.crd_number?('foobar')).to be false
+      end
+
+      it 'returns true for integers' do
+        expect(ExternalDatasetService::Iapd.crd_number?('2786221')).to be true
+      end
+    end
+
     describe '#match' do
+      let(:person) { create(:entity_person) }
+      let(:org) { create(:entity_org) }
+      let(:owner_id) { nil }
+      let(:primary_ext) { :person }
+      let(:external_dataset) do
+        create(:external_dataset, primary_ext: primary_ext, entity_id: nil, row_data: { 'OwnerID' => owner_id })
+      end
+
+      context 'with empty owner id' do
+        it 'add entity_id to external_dataset' do
+          expect { ExternalDatasetService::Iapd.match(entity: person, external_dataset: external_dataset) }
+            .to change { external_dataset.reload.entity_id }.from(nil).to(person.id)
+        end
+
+        it 'creates business person' do
+          expect { ExternalDatasetService::Iapd.match(entity: person, external_dataset: external_dataset) }
+            .to change(BusinessPerson, :count).by(1)
+        end
+
+        it 'does not create crd_number if empty' do
+          ExternalDatasetService::Iapd.match(entity: person, external_dataset: external_dataset)
+          expect(person.reload.business_person.crd_number).to be nil
+        end
+      end
+
+      context 'with owner id' do
+        let(:owner_id) { Faker::Number.unique.number(5) }
+
+        it 'creates business person' do
+          expect { ExternalDatasetService::Iapd.match(entity: person, external_dataset: external_dataset) }
+            .to change(BusinessPerson, :count).by(1)
+        end
+
+        it 'add crd number to business person' do
+          person.add_extension('BusinessPerson')
+          expect(person.business_person.crd_number).to be nil
+          ExternalDatasetService::Iapd.match(entity: person, external_dataset: external_dataset)
+          expect(person.reload.business_person.crd_number).to eq owner_id.to_i
+        end
+      end
+
+      context 'when entity is an org' do
+        let(:primary_ext) { :org }
+
+        it 'creates a business' do
+          expect { ExternalDatasetService::Iapd.match(entity: org, external_dataset: external_dataset) }
+            .to change(Business, :count).by(1)
+        end
+      end
     end
   end
 end
