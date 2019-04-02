5484540a0a5bfd3c4ea900bafc411c6b4a169eae
ziggy <ziggy@littlesis.org>
2019-04-02 14:46:34 -0400

    lint merge_controller_spec

--- spec/controllers/merge_controller_spec.rb
+++ spec/controllers/merge_controller_spec.rb
@@ -2,55 +2,56 @@ require 'rails_helper'
 
 describe MergeController, type: :controller do
   describe "routes" do
-    it { should route(:get, '/merge').to(action: :merge) }
-    it { should route(:post, '/merge').to(action: :merge!) }
-    it { should route(:get, '/merge/redundant').to(action: :redundant_merge_review) }
+    it { is_expected.to route(:get, '/merge').to(action: :merge) }
+    it { is_expected.to route(:post, '/merge').to(action: :merge!) }
+    it { is_expected.to route(:get, '/merge/redundant').to(action: :redundant_merge_review) }
   end
 
   describe '#merge' do
     # TODO: consider deleting?
     let(:entity) { build(:org) }
+
     login_admin
 
-    context 'search mode' do
+    describe 'search mode' do
       before do
         expect(Entity).to receive(:find).with(entity.id).and_return(entity)
         expect(entity).to receive(:similar_entities).with(75).and_return([])
         get :merge, params: { mode: 'search', source: entity.id }
       end
 
-      it { should respond_with(200) }
-      it { should render_template(:merge) }
+      it { is_expected.to respond_with(200) }
+      it { is_expected.to render_template(:merge) }
       specify { expect(assigns(:merge_mode)).to eql 'search' }
     end
 
-    context 'search mode with query' do
+    describe 'search mode with query' do
       before do
         expect(Entity).to receive(:find).with(entity.id).and_return(entity)
-        expect(Entity::Search).to receive(:similar_entities)
-                                    .with(entity, query: 'query', per_page: 75)
-                                    .and_return([])
+        expect(SimilarEntitiesService).to receive(:new)
+                                            .with(entity, query: 'query', per_page: 75)
+                                            .and_return(double(:similar_entities => []))
         get :merge, params: { mode: 'search', source: entity.id, query: 'query' }
       end
 
-      it { should respond_with(200) }
-      it { should render_template(:merge) }
+      it { is_expected.to respond_with(200) }
+      it { is_expected.to render_template(:merge) }
       specify { expect(assigns(:merge_mode)).to eql 'search' }
       specify { expect(assigns(:query)).to eql 'query' }
     end
 
-    context 'merge mode with query' do
+    describe 'merge mode with query' do
       let(:source) { build(:org) }
       let(:dest) { build(:org) }
 
       before do
         expect(Entity).to receive(:find).with(source.id).and_return(source)
         expect(Entity).to receive(:find).with(dest.id).and_return(dest)
-        get :merge, params: { mode: 'execute', source: source.id, dest: dest.id } 
+        get :merge, params: { mode: 'execute', source: source.id, dest: dest.id }
       end
 
-      it { should respond_with(200) }
-      it { should render_template(:merge) }
+      it { is_expected.to respond_with(200) }
+      it { is_expected.to render_template(:merge) }
       specify { expect(assigns(:merge_mode)).to eql 'execute' }
       specify { expect(assigns(:entity_merger)).to be_a EntityMerger }
     end
