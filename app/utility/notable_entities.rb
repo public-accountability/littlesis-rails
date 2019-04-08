da02ab79db56d0ac6b9143dad0fed1cd7bb1c0a2
ziggy <ziggy@littlesis.org>
2019-04-08 11:05:23 -0400

    add a few more notable entities

--- app/utility/notable_entities.rb
+++ app/utility/notable_entities.rb
@@ -4,7 +4,10 @@ NotableEntities = {
   :house_of_reps => 12_884,
   :senate => 12_885,
   :democratic_party => 12_886,
-  :republican_party => 12_901
+  :republican_party => 12_901,
+  :trump => 15_108,
+  :exxon => 2,
+  :cuomo => 36_930
 }.with_indifferent_access
 
 NotableEntities.singleton_class.define_method(:get) do |entity|
