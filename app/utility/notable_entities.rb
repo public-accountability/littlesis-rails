1ae16d22bb7156fe9d6111f1d836338cb49024f7
ziggy <ziggy@littlesis.org>
2019-04-08 10:58:39 -0400

    NotableEntities.get returns <Entity>

--- app/utility/notable_entities.rb
+++ app/utility/notable_entities.rb
@@ -5,4 +5,10 @@ NotableEntities = {
   :senate => 12_885,
   :democratic_party => 12_886,
   :republican_party => 12_901
-}.with_indifferent_access.freeze
+}.with_indifferent_access
+
+NotableEntities.singleton_class.define_method(:get) do |entity|
+  Entity.find fetch(entity)
+end
+
+NotableEntities.freeze
--- spec/utility/notable_entities_spec.rb
+++ spec/utility/notable_entities_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
-describe NotableEntities do
+describe "NotableEntities" do
   specify { expect(NotableEntities).to be_a ActiveSupport::HashWithIndifferentAccess }
   specify { expect(NotableEntities.fetch(:senate)).to eq 12_885 }
   specify { expect(NotableEntities.fetch('democratic_party')).to eq 12_886 }
   specify { expect(NotableEntities.keys).to be_a Array }
+  specify { expect(NotableEntities.frozen?).to be true }
+
+  describe 'get' do
+    it 'retrives Entity active record object' do
+      expect(Entity).to receive(:find).with(12_884).once
+      NotableEntities.get(:house_of_reps)
+    end
+  end
 end
