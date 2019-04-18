c4843a98e631205a0b8a8afe4603cbd9975a73d9
ziggy <ziggy@littlesis.org>
2019-04-18 13:37:34 -0400

    use owner_type to determine primary_ext for iapd owner

--- app/models/external_dataset.rb
+++ app/models/external_dataset.rb
@@ -2,7 +2,18 @@
 
 class ExternalDataset < ApplicationRecord
   IapdAdvisor = Struct.new(:crd_number, :name, :data)
-  IapdOwner = Struct.new(:owner_key, :name, :data)
+  IapdOwner = Struct.new(:owner_key, :name, :data) do
+    def owner_type
+      owner_types = data.map { |d| d['owner_type'] }.uniq
+      if owner_types.length != 1 && owner_types.include?('I')
+        Rails.logger.warn "Conflicting owner type in Iapd dataset. Ownerkey = #{owner_key}"
+        nil
+      else
+        owner_types.first == 'I' ? :person : :org
+      end
+    end
+  end
+
   DATASETS = %w[iapd].freeze
 
   validates :name, inclusion: { in: DATASETS }
