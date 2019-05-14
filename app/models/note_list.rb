b5017138241eeb231faa0fc85831e200392d1aa2
ziggy <ziggy@littlesis.org>
2019-05-14 10:18:38 -0400

    remove note models

--- app/models/note_list.rb
+++
@@ -1,4 +0,0 @@
-class NoteList < ApplicationRecord
-  belongs_to :note, inverse_of: :note_lists
-  belongs_to :list
-end
--- app/models/note_user.rb
+++
@@ -1,4 +0,0 @@
-class NoteUser < ApplicationRecord
-	belongs_to :note
-	belongs_to :user
-end
