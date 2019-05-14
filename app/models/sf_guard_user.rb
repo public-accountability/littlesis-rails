91bf23cfe7fca148341ae5338a3f27fb88c0ea60
ziggy <ziggy@littlesis.org>
2019-05-14 14:34:36 -0400

    remove group_permissions from sf_guard_user

--- app/models/sf_guard_user.rb
+++ app/models/sf_guard_user.rb
@@ -2,8 +2,6 @@ class SfGuardUser < ApplicationRecord
   include SingularTable
   include SoftDelete
 
-  # has_many :notes, foreign_key: "user_id", inverse_of: :sf_guard_user
-
   has_one :user, inverse_of: :sf_guard_user
   has_one :sf_guard_user_profile, inverse_of: :sf_guard_user, foreign_key: "user_id"
   accepts_nested_attributes_for :sf_guard_user_profile
@@ -15,19 +13,15 @@ class SfGuardUser < ApplicationRecord
 
   has_many :network_maps, foreign_key: 'sf_user_id', inverse_of: :sf_guard_user
 
-	def permissions
-		(direct_permissions + group_permissions).uniq
-	end
+  def permissions
+    direct_permissions
+  end
 
-	def direct_permissions
-		sf_guard_permissions.pluck(:name).uniq
-	end
+  def direct_permissions
+    sf_guard_permissions.pluck(:name).uniq
+  end
 
-	def group_permissions
-		sf_guard_groups_permissions.pluck(:name).uniq
-	end
-
-	def has_permission(name)
-		permissions.include?(name)
-	end
+  def has_permission(name)
+    permissions.include?(name)
+  end
 end
