71ecec71e0d58d3209d76bd16b627f17b910eddc
ziggy <ziggy@littlesis.org>
2019-06-18 16:08:23 -0400

    move method state_abbr to AddressState

--- app/models/address_state.rb
+++ app/models/address_state.rb
@@ -7,8 +7,66 @@ class AddressState < ApplicationRecord
 
   def self.abbreviation_map
     return @abbreviation_map if defined?(@abbreviation_map)
+
     @abbreviation_map = all.each_with_object({}) do |state, hash|
       hash[state.abbreviation] = state.name
     end
   end
+
+  def self.state_abbr(name)
+    STATE_ABBR.key(name)
+  end
+
+  STATE_ABBR = {
+    'AL' => 'Alabama',
+    'AK' => 'Alaska',
+    'AZ' => 'Arizona',
+    'AR' => 'Arkansas',
+    'CA' => 'California',
+    'CO' => 'Colorado',
+    'CT' => 'Connecticut',
+    'DE' => 'Delaware',
+    'FL' => 'Florida',
+    'GA' => 'Georgia',
+    'HI' => 'Hawaii',
+    'ID' => 'Idaho',
+    'IL' => 'Illinois',
+    'IN' => 'Indiana',
+    'IA' => 'Iowa',
+    'KS' => 'Kansas',
+    'KY' => 'Kentucky',
+    'LA' => 'Louisiana',
+    'ME' => 'Maine',
+    'MD' => 'Maryland',
+    'MA' => 'Massachusetts',
+    'MI' => 'Michigan',
+    'MN' => 'Minnesota',
+    'MS' => 'Mississippi',
+    'MO' => 'Missouri',
+    'MT' => 'Montana',
+    'NE' => 'Nebraska',
+    'NV' => 'Nevada',
+    'NH' => 'New Hampshire',
+    'NJ' => 'New Jersey',
+    'NM' => 'New Mexico',
+    'NY' => 'New York',
+    'NC' => 'North Carolina',
+    'ND' => 'North Dakota',
+    'OH' => 'Ohio',
+    'OK' => 'Oklahoma',
+    'OR' => 'Oregon',
+    'PA' => 'Pennsylvania',
+    'RI' => 'Rhode Island',
+    'SC' => 'South Carolina',
+    'SD' => 'South Dakota',
+    'TN' => 'Tennessee',
+    'TX' => 'Texas',
+    'UT' => 'Utah',
+    'VT' => 'Vermont',
+    'VA' => 'Virginia',
+    'WA' => 'Washington',
+    'WV' => 'West Virginia',
+    'WI' => 'Wisconsin',
+    'WY' => 'Wyoming'
+  }.freeze
 end
--- spec/models/address_state_spec.rb
+++ spec/models/address_state_spec.rb
@@ -3,4 +3,9 @@ describe AddressState do
     expect(AddressState.abbreviation_map).to be_a Hash
     expect(AddressState.abbreviation_map.count).to eql 59
   end
+
+  specify do
+    expect(AddressState.state_abbr('Colorado')).to eq 'CO'
+    expect(AddressState.state_abbr('Canada')).to eq nil
+  end
 end
