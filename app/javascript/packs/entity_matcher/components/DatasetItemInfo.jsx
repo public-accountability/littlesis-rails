1ef0e7e818d2bb43f74d46a12f81de652030e1da
ziggy <ziggy@littlesis.org>
2019-07-01 16:06:43 -0400

    skip schedule sorting for adivsors

--- app/javascript/packs/entity_matcher/components/DatasetItemInfo.jsx
+++ app/javascript/packs/entity_matcher/components/DatasetItemInfo.jsx
@@ -25,9 +25,10 @@ const DatasetItemPresenter = ({title, value}) => {
   output: Array[ Array[String, String|Number] ]
 */
 const dataToKeyValues= (rowData) => {
-  const latestRecord = orderBy(rowData.data, ['filename', 'schedule'], ['desc', 'asc'])[0];
 
   if (rowData['class'].includes('IapdOwner')) {
+    let latestRecord = orderBy(rowData.data, ['filename', 'schedule'], ['desc', 'asc'])[0];
+
     return [
       ["Dataset", "IAPD Owner"],
       ["Name", formatIapdOwnerName(rowData.name, isOwnerPerson(rowData))],
@@ -40,6 +41,8 @@ const dataToKeyValues= (rowData) => {
   }
   
   if (rowData['class'].includes('IapdAdvisor')) {
+    let latestRecord = orderBy(rowData.data, ['filename'], ['desc'])[0];
+    
     return [
       ["Dataset", "IAPD Advisor"],
       ["Name", capitalizeWords(rowData.name)],
