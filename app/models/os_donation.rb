93ca104b0b5127a5573f085a3458f5b68eeeea52
ziggy <ziggy@littlesis.org>
2019-07-03 15:50:15 -0400

    refactor and lint OsDonation

--- app/models/os_donation.rb
+++ app/models/os_donation.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class OsDonation < ApplicationRecord
-  has_paper_trail  :on => [:update, :destroy]  # don't track create events
-  validates_uniqueness_of :fec_cycle_id
+  has_paper_trail :on => [:update, :destroy] # don't track create events
+  validates :fec_cycle_id, uniqueness: true
 
   has_one :os_match
-  
+
   def create_fec_cycle_id
-    unless self.cycle.nil? || self.fectransid.nil?
-      self.fec_cycle_id = self.cycle + "_" + self.fectransid
+    if cycle.present? && fectransid.present?
+      self.fec_cycle_id = "#{cycle}_#{fectransid}"
     end
   end
 
   def reference_name
-    "FEC Filing " + microfilm.to_s
+    "FEC Filing #{microfilm}"
   end
 
   def reference_source
@@ -20,10 +22,9 @@ class OsDonation < ApplicationRecord
 
   def reference_url
     if microfilm.nil?
-      "http://www.fec.gov/finance/disclosure/advindsea.shtml"
+      'http://www.fec.gov/finance/disclosure/advindsea.shtml'
     else
-      "http://docquery.fec.gov/cgi-bin/fecimg/?" + microfilm
+      "http://docquery.fec.gov/cgi-bin/fecimg/?#{microfilm}"
     end
   end
-
 end
