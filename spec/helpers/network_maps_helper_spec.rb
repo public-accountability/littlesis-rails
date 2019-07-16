0dd0fba8a8e666dc9f98047a899c3bf1b6e1e882
ziggy <ziggy@littlesis.org>
2019-07-16 16:58:26 -0400

    setting `@oligrapher_version` changes js script tag

--- spec/helpers/network_maps_helper_spec.rb
+++ spec/helpers/network_maps_helper_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-
 describe NetworkMapsHelper do
   describe 'oligrapher_js_tags' do
     specify do
       expect(helper.oligrapher_js_tags)
         .to eq '<script src="/js/oligrapher/oligrapher-0.0.1.js"></script><script src="/js/oligrapher/oligrapher_littlesis_bridge-0.0.1.js"></script>'
+    end
+
+    specify do
+      assign(:oligrapher_version, '0.0.9')
+      expect(helper.oligrapher_js_tags)
+        .to eq '<script src="/js/oligrapher/oligrapher-0.0.9.js"></script><script src="/js/oligrapher/oligrapher_littlesis_bridge-0.0.9.js"></script>'
     end
   end
 
