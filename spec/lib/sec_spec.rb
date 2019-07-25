9b3d213c27f35d327b6d98c7b5e50bc1ecdfa8a6
ziggy <ziggy@littlesis.org>
2019-07-25 11:09:41 -0400

    remove scraping version to get sec filings

--- spec/lib/sec_spec.rb
+++ spec/lib/sec_spec.rb
@@ -1,40 +1,15 @@
 describe Sec do
-  describe Sec::Filings do
-    let(:xml) do
-      File.read Rails.root.join('spec', 'testdata', 'gs.xml')
-    end
-    
-    describe 'get_xml' do
-      let(:params) do
-        { 'action' => 'getcompany',
-          'output' => 'xml',
-          'start' => 0,
-          'count' => 100,
-          'CIK' => Sec::CIKS.fetch('GS') }
-      end
-
-      it 'calls makes GET request to sec.gov' do
-        expect(HTTParty).to receive(:get)
-                              .with('http://www.sec.gov/cgi-bin/browse-edgar', query: params)
-                              .once
-                              .and_return(double(body: xml))
-
-        Sec::Filings.get_xml(Sec::CIKS['GS'])
-      end
+  describe 'verify_cik!' do
+    specify do
+      expect { Sec.verify_cik!('') }.to raise_error(Sec::InvalidCikNumber)
     end
 
-    describe 'parse_filings_xml' do
-      specify do
-        expect(Sec::Filings.parse_filings_xml(xml).length).to eq 100
-      end
+    specify do
+      expect { Sec.verify_cik!('123') }.to raise_error(Sec::InvalidCikNumber)
+    end
 
-      specify do
-        expect(Sec::Filings.parse_filings_xml(xml).first)
-          .to eq Sec::Filings::Filing.new(date: '2019-07-23',
-                                          href: 'https://www.sec.gov/Archives/edgar/data/886982/000156459019025565/0001564590-19-025565-index.htm',
-                                          form_name: 'Prospectus [Rule 424(b)(2)]',
-                                          type: '424B2')
-      end
+    specify do
+      expect { Sec.verify_cik!('0000886982') }.not_to raise_error
     end
   end
 end
