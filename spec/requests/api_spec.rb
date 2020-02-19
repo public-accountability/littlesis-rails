241c91c86cd2d631f952cb6759d96a844f11e428
ziggy <ziggy@littlesis.org>
2020-02-19 16:14:54 -0500

    api tokens are no longer required for api requests

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -1,42 +1,5 @@
 describe Api, :pagination_helper do
-  before(:all) do
-    @user = create_really_basic_user
-    @api_token = @user.create_api_token!
-    @auth_header = { 'Littlesis-Api-Token': @api_token.token }
-  end
-
-  after(:all) do
-    @user.sf_guard_user.delete
-    @user.delete
-    @api_token.delete
-  end
-
   let(:meta) { Api::META }
-
-  describe 'access control' do
-    let(:api_request) do
-      -> { get api_entity_path(create(:entity_person)) }
-    end
-
-    context 'when no api token provided' do
-      before { api_request.call }
-
-      specify { expect(response).to have_http_status :unauthorized }
-    end
-
-    context 'when no api token provided, but user is logged in' do
-      let(:user) { create_really_basic_user }
-
-      before do
-        login_as(user, :scope => :user)
-        api_request.call
-      end
-
-      after { logout(:user) }
-
-      specify { expect(response).to have_http_status :ok }
-    end
-  end
 
   describe 'entities' do
     let(:lawyer) do
@@ -275,7 +238,7 @@ describe Api, :pagination_helper do
       end
     end
 
-    
+
   end
 
   describe '/entities/search?q=NAME' do
