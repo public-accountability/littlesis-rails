f8fbc18f5483eb103d7b6c767e279a9c103cf956
ziggy <ziggy@littlesis.org>
2020-02-27 10:51:34 -0500

    rewrite OligrapherGraphData tests

--- spec/utility/oligrapher_graph_data_spec.rb
+++ spec/utility/oligrapher_graph_data_spec.rb
@@ -1,39 +1,41 @@
 # frozen_string_literal: true
 
 describe OligrapherGraphData do
-  let(:foo_bar_hash) { { 'foo' => 'bar' } }
-  let(:foo_bar_json) { "{\"foo\":\"bar\"}" }
+  let(:nodes_hash) do
+    { 'nodes' => { '123' => { 'id' => '123', 'color' => '#ccc' } } }
+  end
+
+  let(:nodes_json) { JSON.generate(nodes_hash) }
 
   describe 'init' do
     it 'creates new hash if nil' do
-      expect(OligrapherGraphData.new(nil).hash).to eq({})
+      expect(OligrapherGraphData.new(nil).hash).to eq({ nodes: {}, edges: {}, captions: {} }.with_indifferent_access)
     end
 
     it 'parses json' do
-      expect(OligrapherGraphData.new(foo_bar_json).hash).to eq foo_bar_hash
+      expect(OligrapherGraphData.new(nodes_json).hash).to eq nodes_hash.merge(edges: {}, captions: {}).with_indifferent_access
     end
 
     it 'stores hash' do
-      expect(OligrapherGraphData.new(foo_bar_hash).hash).to eq foo_bar_hash
+      expect(OligrapherGraphData.new(nodes_hash).hash).to eq nodes_hash.merge(edges: {}, captions: {}).with_indifferent_access
     end
   end
 
   describe 'dump' do
     it 'converts hash to json via to_json' do
-      expect(OligrapherGraphData.new(foo_bar_hash).to_json)
-        .to eq foo_bar_json
+      expect(OligrapherGraphData.new(nodes_hash).to_json)
+        .to eq JSON.generate(nodes_hash.merge(edges: {}, captions: {}))
     end
   end
 
   describe '==' do
     specify do
-      expect(OligrapherGraphData.new(foo_bar_hash) == OligrapherGraphData.new(foo_bar_json))
+      expect(OligrapherGraphData.new(nodes_hash) == OligrapherGraphData.new(nodes_json))
         .to be true
     end
 
     specify do
-      expect(OligrapherGraphData.new(foo_bar_hash) == OligrapherGraphData.new("{\"foo\":\"baz\"}"))
-        .to be false
+      expect(OligrapherGraphData.new == OligrapherGraphData.new(nodes_hash)).to be false
     end
   end
 
@@ -43,12 +45,12 @@ describe OligrapherGraphData do
     end
 
     it 'parses string' do
-      expect(OligrapherGraphData.load(foo_bar_json)).to eq OligrapherGraphData.new(foo_bar_hash)
+      expect(OligrapherGraphData.load(nodes_json)).to eq OligrapherGraphData.new(nodes_hash)
     end
 
     it 'duplicate OligrapherGraphData' do
-      expect(OligrapherGraphData.load(OligrapherGraphData.new(foo_bar_hash)))
-        .to eq OligrapherGraphData.new(foo_bar_hash)
+      expect(OligrapherGraphData.load(OligrapherGraphData.new(nodes_hash)))
+        .to eq OligrapherGraphData.new(nodes_hash)
     end
   end
 end
