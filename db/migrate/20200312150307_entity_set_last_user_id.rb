4d47e042cc5efdaa446feb3e3f13facb6454b9dd
ziggy <ziggy@elephant-bird.net>
2020-03-30 14:57:44 -0400

    make entity set last user id migration irreversible

--- db/migrate/20200312150307_entity_set_last_user_id.rb
+++ db/migrate/20200312150307_entity_set_last_user_id.rb
@@ -5,27 +5,14 @@ class EntitySetLastUserId < ActiveRecord::Migration[6.0]
 
       if user.nil?
         Rails.logger.warn "Couldn't find User with sf_guard_user_id of #{entity.last_user_id}; setting entity #{entity.id} last_user_id to 1"
-        user_id = 1
+        entity.update_columns(last_user_id: 1)
       else
-        user_id = user.id
+        entity.update_columns(last_user_id: user.id)
       end
-
-      user_id = 1 if user_id.nil?
-
-      entity.update_columns(last_user_id: user_id)
     end
-
-    # make sure all last_user_ids are set to actual user ids
-    user_ids = User.all.map(&:id).uniq
-    Entity.unscoped
-          .where.not(last_user_id: user_ids)
-          .update_all(last_user_id: 1)
   end
 
   def down
-    Entity.unscoped.find_each do |entity|
-      sf_user_id = User.find(entity.last_user_id)&.sf_guard_user_id || 1
-      entity.update_columns(last_user_id: sf_user_id)
-    end
+    raise ActiveRecord::IrreversibleMigration
   end
 end
