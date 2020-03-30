6484b4eb9d69a362e6c0da432f2c3a246714c091
ziggy <ziggy@elephant-bird.net>
2020-03-30 17:52:08 -0400

    update list migrations and run all remaining migrations

--- db/migrate/20200318202544_list_set_last_user_id.rb
+++ db/migrate/20200318202544_list_set_last_user_id.rb
@@ -3,7 +3,7 @@ class ListSetLastUserId < ActiveRecord::Migration[6.0]
     List.unscoped.find_each do |list|
       user = User.find_by(sf_guard_user_id: list.last_user_id)
 
-      if user.nil?
+      if user.nil? || list.last_user_id.nil?
         Rails.logger.warn "Couldn't find User with sf_guard_user_id of #{list.last_user_id}; setting list #{list.id} last_user_id to 1"
         user_id = 1
       else
@@ -14,12 +14,6 @@ class ListSetLastUserId < ActiveRecord::Migration[6.0]
 
       list.update_columns(last_user_id: user_id)
     end
-
-    # make sure all last_user_ids are set to actual user ids
-    user_ids = User.all.map(&:id).uniq
-    List.unscoped
-          .where.not(last_user_id: user_ids)
-          .update_all(last_user_id: 1)
   end
 
   def down
--- db/migrate/20200318202545_list_entity_set_last_user_id.rb
+++ db/migrate/20200318202545_list_entity_set_last_user_id.rb
@@ -3,7 +3,7 @@ class ListEntitySetLastUserId < ActiveRecord::Migration[6.0]
     ListEntity.unscoped.find_each do |le|
       user = User.find_by(sf_guard_user_id: le.last_user_id)
 
-      if user.nil?
+      if user.nil? || le.last_user_id.nil?
         Rails.logger.warn "Couldn't find User with sf_guard_user_id of #{le.last_user_id}; setting list entity #{le.id} last_user_id to 1"
         user_id = 1
       else
@@ -14,12 +14,6 @@ class ListEntitySetLastUserId < ActiveRecord::Migration[6.0]
 
       le.update_columns(last_user_id: user_id)
     end
-
-    # make sure all last_user_ids are set to actual user ids
-    user_ids = User.all.map(&:id).uniq
-    ListEntity.unscoped
-          .where.not(last_user_id: user_ids)
-          .update_all(last_user_id: 1)
   end
 
   def down
