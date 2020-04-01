1c41012aa4c11d5c188c772e5cefe96ea9e09ac7
robjlucas <rob@newleftreview.org>
2020-04-01 13:12:13 +0100

    Basic lint of entity.js

--- app/assets/javascripts/pages/entity.js
+++ app/assets/javascripts/pages/entity.js
@@ -1,64 +1,62 @@
-var entity = {}; 
+var entity = {} 
 
 // Toggles visibility of entity summary
 entity.summaryToggle = function(){
-  $('.summary-excerpt').toggle();
-  $('.summary-full').toggle();
-  $('.summary-show-more').toggle();
-  $('.summary-show-less').toggle();
-};
+  $('.summary-excerpt').toggle()
+  $('.summary-full').toggle()
+  $('.summary-show-more').toggle()
+  $('.summary-show-less').toggle()
+}
 
 // Toggles visibility of a related entity's additional relationships on a profile page
 entity.relationshipsToggle = function(e) {
-  $(e.target).closest('.relationship-section').find('.collapse').collapse('toggle');
-};
-
-
+  $(e.target).closest('.relationship-section').find('.collapse').collapse('toggle')
+}
 
 /* Editable blurb */
 
 entity._submitBlurb = function(newBlurb, original) {
   if (newBlurb !== original) {
-    api.addBlurbToEntity(newBlurb, utility.entityInfo('entityid'));
+    api.addBlurbToEntity(newBlurb, utility.entityInfo('entityid'))
   }
 
-  $('#entity-blurb-text').html(newBlurb);
-  $('#entity-blurb-pencil').show();
-};
+  $('#entity-blurb-text').html(newBlurb)
+  $('#entity-blurb-pencil').show()
+}
 
 entity._editableBlurbInput = function(text) {
   function handleKeyup(e) {
     if ((e.keyCode || e.which) === 13) {
-      entity._submitBlurb($(this).val(), text);
+      entity._submitBlurb($(this).val(), text)
     } else if ( (e.keyCode || e.which) === 27) {
-      $('#entity-blurb-text').html(text);
-      $('#entity-blurb-pencil').show();
+      $('#entity-blurb-text').html(text)
+      $('#entity-blurb-pencil').show()
     }
   }
 
-  return $('<input>', { "val": text, "keyup": handleKeyup });
+  return $('<input>', { "val": text, "keyup": handleKeyup })
 
-};
+}
 
 
 entity.editableBlurb = function() {
   $("#editable-blurb").hover(
     function() {
-      $("#entity-blurb-pencil").show();
+      $("#entity-blurb-pencil").show()
     },
     function() {
-      $("#entity-blurb-pencil").hide();
+      $("#entity-blurb-pencil").hide()
     }
-  );
-  $('#entity-blurb-pencil').click(function(x){
-    $(this).hide();
+  )
+  $('#entity-blurb-pencil').click(function(){
+    $(this).hide()
 
     // get existing blurb text
-    var blurb = $('#entity-blurb-text').text() || '';
+    var blurb = $('#entity-blurb-text').text() || ''
 
     // replace current text with input
     $('#entity-blurb-text').html(
       entity._editableBlurbInput(blurb)
-    );
-  });
-};
+    )
+  })
+}
