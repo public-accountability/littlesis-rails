fa4704d34972fff795d300dab1b3468e6372fb5d
ziggy <ziggy@elephant-bird.net>
2020-04-09 11:35:30 -0400

    remove legacy variables from lilsis.yml.sample

--- config/initializers/carrier_wave.rb
+++
@@ -1,8 +0,0 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     :provider               => 'AWS',
-#     :aws_access_key_id      => (Rails.env.development? or Rails.env.test?) ? '' : Lilsis::Application.config.aws_key,
-#     :aws_secret_access_key  => (Rails.env.development? or Rails.env.test?) ? '' : Lilsis::Application.config.aws_secret,
-#   }
-#   config.fog_directory  = Lilsis::Application.config.aws_s3_bucket
-# end
