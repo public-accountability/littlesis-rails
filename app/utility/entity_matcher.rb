f15df54f03ca5e1a6f2b8cdff00563b05a1475b5
ziggy <ziggy@elephant-bird.net>
2020-04-14 13:57:57 -0400

    find_matches_for use TestCase.org and TestCase.person

--- app/utility/entity_matcher.rb
+++ app/utility/entity_matcher.rb
@@ -8,10 +8,10 @@ require_relative 'entity_matcher/evaluation'
 module EntityMatcher
   # String, kwargs --> Array[EvaluateResult]
   def self.find_matches_for_person(name, **kwargs)
-    test_case = TestCase::Person.new(name, **kwargs)
+    test_case = TestCase.person(name, **kwargs)
 
     search_results = Search.by_name(test_case.last).map do |entity|
-      EntityMatcher::Evaluation::Person.new(test_case, TestCase::Person.new(entity)).result
+      EntityMatcher::Evaluation::Person.new(test_case, TestCase.person(entity)).result
     end
 
     EvaluationResultSet.new search_results
@@ -19,10 +19,10 @@ module EntityMatcher
 
   # String, kwargs --> Array[EvaluateResult]
   def self.find_matches_for_org(name, **kwargs)
-    test_case = TestCase::Org.new(name, **kwargs)
+    test_case = TestCase.org(name, **kwargs)
 
     search_results = Search.by_org(name).map do |entity|
-      EntityMatcher::Evaluation::Org.new(test_case, TestCase::Org.new(entity)).result
+      EntityMatcher::Evaluation::Org.new(test_case, TestCase.org(entity)).result
     end
 
     EvaluationResultSet.new search_results
