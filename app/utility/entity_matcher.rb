cfdb7cb57ff28f6d1716735c8aed5d77804e14ef
ziggy <ziggy@elephant-bird.net>
2020-04-14 13:30:49 -0400

    refactor find_matches functions

--- app/utility/entity_matcher.rb
+++ app/utility/entity_matcher.rb
@@ -11,7 +11,7 @@ module EntityMatcher
     test_case = TestCase::Person.new(name, **kwargs)
 
     search_results = Search.by_name(test_case.last).map do |entity|
-      evaluate_people test_case, TestCase::Person.new(entity)
+      EntityMatcher::Evaluation::Person.new(test_case, TestCase::Person.new(entity)).result
     end
 
     EvaluationResultSet.new search_results
@@ -22,17 +22,9 @@ module EntityMatcher
     test_case = TestCase::Org.new(name, **kwargs)
 
     search_results = Search.by_org(name).map do |entity|
-      evaluate_orgs test_case, TestCase::Org.new(entity)
+      EntityMatcher::Evaluation::Org.new(test_case, TestCase::Org.new(entity)).result
     end
 
     EvaluationResultSet.new search_results
-  end
-
-  def self.evaluate_people(test_case, match)
-    EntityMatcher::Evaluation::Person.new(test_case, match).result
-  end
-
-  def self.evaluate_orgs(test_case, match)
-    EntityMatcher::Evaluation::Org.new(test_case, match).result
   end
 end
