1e1f4f8d4bf51c4ff46cdb59cafedcc1d72c26cd
ziggy <ziggy@elephant-bird.net>
2020-04-16 16:10:34 -0400

    new iapd importer using external_data

--- app/models/external_data.rb
+++ app/models/external_data.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ExternalData < ApplicationRecord
+  enum dataset: { reserved: 0,
+                  iapd_advisors: 1,
+                  iapd_owners: 2 }
+
+  serialize :data
+
+  def setup_data_column
+    return self if data.present?
+
+    case dataset
+    when 'iapd_advisors', 'iapd_owners'
+      self.data = []
+    end
+
+    self
+  end
 end
