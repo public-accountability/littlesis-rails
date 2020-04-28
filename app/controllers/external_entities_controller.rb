befc92673d7f10561f4759c9c7f304af091b228d
ziggy <ziggy@elephant-bird.net>
2020-04-28 15:07:50 -0400

    setup entity matcher page

--- app/controllers/external_entities_controller.rb
+++ app/controllers/external_entities_controller.rb
@@ -2,12 +2,22 @@
 
 class ExternalEntitiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_external_entity
 
   # GET /external_entities/:id
   def show
+    if @external_entity.matched?
+      render 'already_matched'
+    end
   end
 
   # PATCH /external_entities/:id
   def update
+  end
+
+  private
+
+  def set_external_entity
+    @external_entity = ExternalEntity.find(params.fetch(:id))
   end
 end
--- spec/controllers/external_entities_controller_spec.rb
+++ spec/controllers/external_entities_controller_spec.rb
@@ -1,4 +1,14 @@
 describe ExternalEntitiesController, type: :controller do
   it { is_expected.to route(:get, '/external_entities/123').to(action: :show, id: 123) }
   it { is_expected.to route(:patch, '/external_entities/123').to(action: :update, id: 123) }
+
+  describe 'show' do
+    it 'renders already_matched page when entity if entity is matched' do
+      external_entity = build(:external_entity, id: rand(1000), entity_id: rand(1000))
+      expect(ExternalEntity).to receive(:find).once.and_return(external_entity)
+      allow(controller).to receive(:authenticate_user!)
+      get :show, params: { id: external_entity.id }
+      expect(response).to render_template 'already_matched'
+    end
+  end
 end
