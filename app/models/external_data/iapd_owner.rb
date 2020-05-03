e89476b1d6d1b1285b2fe5ae949e8c1cf7736227
ziggy <ziggy@elephant-bird.net>
2020-05-03 19:31:28 -0400

    ExternalData::IapdOwner

---
+++ app/models/external_data/iapd_owner.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# The ExternalData "data" column for iapd owners is an array of hashes.
+# Each hash represents an owner relationship dervied from an SEC filing.
+# Each filing has multiple owners and it is common for a single person
+# to have both a schedule A (direct relationship) and schedule B (indirect ownership)
+# on the same filing.
+class ExternalData
+  class IapdOwner
+    # extend Forwardable
+
+    def initialize(data) # input is the "data" field from ExternalData
+      @data = data
+      @primary_ext = primary_extenion(@data)
+    end
+
+    def person?
+      @primary_ext == 'Person'
+    end
+
+    def org?
+      !person?
+    end
+
+    # def filing_ids
+      # @data.map { |x| x.fetch('filing_id') }.uniq
+    # end
+
+    def associated_advisors
+    end
+
+    private
+
+    def primary_extenion(data)
+      owner_types = data.map { |d| d['owner_type'] }.uniq
+
+      if owner_types.length != 1 && owner_types.include?('I')
+        raise Exceptions::LittleSisError,
+              "Conflicting owner type in Iapd dataset. Ownerkey = #{owner_key}"
+      else
+        owner_types.first == 'I' ? 'Person' : 'Org'
+      end
+    end
+  end
+end
--- spec/models/external_data_spec.rb
+++ spec/models/external_data_spec.rb
@@ -2,4 +2,14 @@ describe ExternalData, type: :model do
   it { is_expected.to have_db_column(:dataset).of_type(:integer) }
   it { is_expected.to have_db_column(:dataset_id).of_type(:string) }
   it { is_expected.to have_db_column(:data).of_type(:text) }
+
+  describe ExternalData::IapdOwner do
+    specify 'person owner' do
+      owner = ExternalData::IapdOwner.new([
+                                            { 'owner_type' => 'I' }
+                                          ])
+      expect(owner.send(:instance_variable_get, :@primary_ext)).to eq 'Person'
+      expect(owner.person?).to be true
+    end
+  end
 end
