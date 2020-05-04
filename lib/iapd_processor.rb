f4d745f1c44b96f1124c14dd87232393659ce858
ziggy <ziggy@elephant-bird.net>
2020-05-04 11:18:13 -0400

    refactor process_owner and process_advisor

--- lib/iapd_processor.rb
+++ lib/iapd_processor.rb
@@ -22,12 +22,7 @@ module IapdProcessor
     # If the entity is not matched, this will automatically match the entity when there is
     # already a crd number associated with an existing entity
     unless external_entity.matched?
-      crd_number = iapd_advisor.dataset_id
-      external_link = ExternalLink.crd.find_by(link_id: crd_number)
-
-      if external_link.present?
-        external_entity.update!(entity_id: external_link.entity_id)
-      end
+      match_by_crd_number(crd_number: iapd_advisor.dataset_id, external_entity: external_entity)
     end
   end
 
@@ -39,11 +34,21 @@ module IapdProcessor
                                             primary_ext: data.primary_ext)
 
     unless external_entity.matched?
-      # For Iapd Owners, the dataset id is not always the crd number.
-      if ExternalLink.crd_number?(iapd_owner.dataset_id)
-        external_link = ExternalLink.crd.find_by(link_id: iapd_owner.dataset_id)
-        external_entity.update!(entity_id: external_link.entity_id) if external_link.present?
-      end
+      match_by_crd_number(crd_number: iapd_owner.dataset_id, external_entity: external_entity)
     end
+
+    create_owner_relationships(iapd_owner)
+  end
+
+  def match_by_crd_number(crd_number:, external_entity:)
+    # For Iapd Owners, the dataset id is not always the crd number
+    if ExternalLink.crd_number?(crd_number)
+      external_link = ExternalLink.crd.find_by(link_id: crd_number)
+      external_entity.update!(entity_id: external_link.entity_id) if external_link.present?
+    end
+  end
+
+  def create_owner_relationships(iapd_owner)
+    # data = ExternalData::IapdOwner.new(iapd_owner.data)
   end
 end
