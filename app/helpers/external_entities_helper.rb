5f573373f2963b1394ac73a602be192ea541e59a
ziggy <ziggy@elephant-bird.net>
2020-05-26 12:14:58 -0400

    move datatables column description to partial

--- app/helpers/external_entities_helper.rb
+++ app/helpers/external_entities_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ExternalEntitiesHelper
+  def render_columns_js(dataset)
+    render partial: "external_entities/dataset_columns/#{dataset}", formats: [:js]
+  end
+
   def external_entities_tab(id, active = false)
     class_name = active ? 'tab-pane active' : 'tab-pane'
     content_tag(:div, id: id, class: class_name, role: 'tabpanel') { yield }
