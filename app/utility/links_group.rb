f3f8f401c45d56ad0c661b19aeb2de5e21e67f74
Rob Lucas <rob@newleftreview.org>
2020-06-18 12:06:13 +0100

    Refactor LinksGroup utility

--- app/utility/links_group.rb
+++ app/utility/links_group.rb
@@ -21,22 +21,22 @@ class LinksGroup
   end
 
   def order(links)
-    # return links.sort { |a, b| b.related.links.count <=> a.related.links.count } if @category_id == 4
-    return links.sort_by { |link| link.related.link_count } if @category_id == 4
-    if @category_id == 5
-      return links.sort { |a, b| a.relationship.amount && b.relationship.amount ? b.relationship.amount <=> a.relationship.amount : -1 }
+    case @category_id
+    when 4
+      sort_by_related_link_count(links)
+    when 5
+      sort_by_amount(links)
+    else
+      sorted_current_links(links) + sorted_ended_links(links)
     end
-
-    # TODO sort by date here or do sorting in SQL
-    return sorted_current_links(links) + sorted_ended_links(links)
   end
 
-  # input: [ <Link> ] 
+  # input: [ <Link> ]
   # output: [ [<Link>] [<Link>] ]
   # This groups entities by the 'other' entity so that relationships with the
   # same entity are grouped together
   def group_by_entity(links)
-    links.group_by { |l| l.entity2_id }.values
+    links.group_by(&:entity2_id).values
   end
 
   def sorted_current_links(links)
@@ -49,5 +49,21 @@ class LinksGroup
     links
       .select { |l| l.relationship.end_date.present? }
       .sort_by { |l| l.relationship.start_date || l.relationship.updated_at.to_s }.reverse
+  end
+
+  def sort_by_related_link_count(links)
+    links.sort_by do |link|
+      link.related.link_count
+    end
+  end
+
+  def sort_by_amount(links)
+    links.sort do |a, b|
+      if a.relationship.amount && b.relationship.amount
+        b.relationship.amount <=> a.relationship.amount
+      else
+        -1
+      end
+    end
   end
 end
