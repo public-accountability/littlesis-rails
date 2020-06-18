e518bdd6b690ec07b69d21d0428b6c79dbd50328
ziggy <ziggy@elephant-bird.net>
2020-06-18 10:13:58 -0400

    refactor relationship_deletion_spec

--- spec/features/relationship_deletion_spec.rb
+++ spec/features/relationship_deletion_spec.rb
@@ -1,40 +1,40 @@
 describe 'deleting relationships' do
-  let(:person) { create(:entity_person) }
-  let(:org) { create(:entity_org) }
-  let(:relationship_params) do
-    { entity: person, related: org, category_id: 1 }
-  end
-
   let(:user1) { create_basic_user }
   let(:user2) { create_basic_user }
 
-  before do
-    with_versioning_for(user1) do
-      @relationship = Relationship.create!(relationship_params)
+  describe 'relationship history page' do
+    let(:person) { create(:entity_person) }
+    let(:org) { create(:entity_org) }
+    let(:relationship) do
+      with_versioning_for(user1) do
+        Relationship.create!(entity: person, related: org, category_id: 1)
+      end
     end
-  end
 
-  it 'shows correct user on entity page' do
-    visit entity_path(person)
-    expect(find('#entity-edited-history').text).to include user1.username
-  end
+    before { relationship }
 
-  context 'when logged in as user2' do
-    before { login_as(user2, :scope => :user) }
-
-    after { logout(:user) }
-
-    it 'shows correct user on entity page after deleting' do
+    it 'shows correct user on entity page' do
       visit entity_path(person)
       expect(find('#entity-edited-history').text).to include user1.username
-      with_versioning_for(user2) do
-        @relationship.current_user = user2
-        @relationship.soft_delete
+    end
+
+    context 'when logged in as user2' do
+      before { login_as(user2, scope: :user) }
+
+      after { logout(:user) }
+
+      it 'shows correct user on entity page after deleting' do
+        visit entity_path(person)
+        expect(find('#entity-edited-history').text).to include user1.username
+        with_versioning_for(user2) do
+          relationship.current_user = user2
+          relationship.soft_delete
+        end
+        visit entity_path(person)
+        expect(find('#entity-edited-history').text).to include user2.username
+        visit entity_path(org)
+        expect(find('#entity-edited-history').text).to include user2.username
       end
-      visit entity_path(person)
-      expect(find('#entity-edited-history').text).to include user2.username
-      visit entity_path(org)
-      expect(find('#entity-edited-history').text).to include user2.username
     end
   end
 end
