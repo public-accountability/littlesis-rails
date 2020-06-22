6c655cda8256f640c3a06271a9ddfe0dc26796eb
ziggy <ziggy@elephant-bird.net>
2020-06-22 14:03:03 -0400

    use uniq ids for tab links

--- app/helpers/external_entities_helper.rb
+++ app/helpers/external_entities_helper.rb
@@ -5,7 +5,7 @@ module ExternalEntitiesHelper
     render partial: "external_entities/dataset_columns/#{dataset}", formats: [:js]
   end
 
-  def external_entities_tab(id, active = false)
+  def external_entities_tab(id, active: false)
     class_name = active ? 'tab-pane active' : 'tab-pane'
     content_tag(:div, id: id, class: class_name, role: 'tabpanel') { yield }
   end
