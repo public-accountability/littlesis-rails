d415812e6aed076058a7749d69c849f3ea3b7b0e
ziggy <ziggy@elephant-bird.net>
2020-07-07 13:32:29 -0400

    remove default_url_options from development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,10 +3,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.routes.default_url_options[:host] = 'localhost:8080'
-  config.routes.default_url_options[:protocol] = 'http'
-
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
