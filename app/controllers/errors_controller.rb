c1ff664dfbfe19f3cc90a5a7389b6e488a67a4e0
ziggy <ziggy@elephant-bird.net>
2020-07-09 16:06:40 -0400

    lint ErrorsController

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,6 @@
-class ErrorsController < ApplicationController
+# frozen_string_literal: true
 
+class ErrorsController < ApplicationController
   PLACEHOLDERS = {
     description: "If submitting a bug report, describe with as much detail as you can what you were doing that caused the error.",
     reproduce: "How do you reproduce this bug. For example: \n 1) Navigate to the page /bug_report\n 2) Fill out the text box labeled description\n 3) Click submit button",
@@ -7,16 +8,22 @@ class ErrorsController < ApplicationController
   }.freeze
 
   THANK_YOU_NOTICE = 'Thank you for submitting a bug report and helping to improve LittleSis!'
+  YOU_ARE_SPAM = "Please don't submit spam to LittleSis. Contact us if you think this is an error."
 
   def bug_report
   end
 
   def file_bug_report
-    NotificationMailer.bug_report_email(bug_report_params).deliver_later
     if user_signed_in?
+      NotificationMailer.bug_report_email(bug_report_params).deliver_later
       redirect_to home_dashboard_path, notice: THANK_YOU_NOTICE
     else
-      flash.now[:notice] = THANK_YOU_NOTICE
+      if SpamDetector.bug_report?(params)
+        NotificationMailer.bug_report_email(bug_report_params).deliver_later
+        flash.now[:notice] = THANK_YOU_NOTICE
+      else
+        flash.now[:notice] = YOU_ARE_SPAM
+      end
       render 'bug_report'
     end
   end
