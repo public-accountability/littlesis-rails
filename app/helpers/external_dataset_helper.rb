8ba7151601792d3a1f03a1e05c79f9c7ba99c5e5
ziggy <ziggy@elephant-bird.net>
2020-07-29 10:17:32 -0400

    move dataset_columns directory to new location

--- app/helpers/external_dataset_helper.rb
+++ app/helpers/external_dataset_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 module ExternalDatasetHelper
+  def render_columns_js(dataset)
+    render partial: "datasets/dataset_columns/#{dataset}", formats: [:js]
+  end
+
   def external_dataset_iapd_subtitle(flow)
     dataset_name = flow.gsub('owners', 'executives')
 
-    content_tag(:h2, class: 'mt-1') do 
+    content_tag(:h2, class: 'mt-1') do
       content_tag(:b, 'Dataset: ') + content_tag(:span, "IAPD #{dataset_name}")
     end
   end
