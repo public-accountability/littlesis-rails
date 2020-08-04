99d281d5331ffc793bfeb2051e3ef3e39a81c366
ziggy <ziggy@elephant-bird.net>
2020-08-04 13:56:04 -0400

    improved nys disclosure index

--- app/indices/external_data_index.rb
+++ app/indices/external_data_index.rb
@@ -3,8 +3,16 @@ ThinkingSphinx::Index.define(
 ) do
   where 'dataset = 4'
   has "JSON_VALUE(data, '$.TRANSACTION_CODE')", :as => :transaction_code, :type => :string
-  has "FORMAT(STR_TO_DATE(JSON_VALUE(data, '$.DATE1_10'),  '%m/%d/%Y'), '%y-%m-%d')", :as => :date, :type => :string
-  has "ROUND(CAST(JSON_VALUE(data, '$.TRANSACTION_CODE') as SIGNED))", :as => :amount, :type => :integer
+
+  has "(CASE WHEN JSON_VALUE(data, '$.DATE1_10') = '' THEN NULL ELSE DATE_FORMAT(STR_TO_DATE(JSON_VALUE(data, '$.DATE1_10'),  '%m/%d/%Y'), '%Y-%m-%d') END)",
+      :as => :date, :type => :string
+
+  has(<<-SQL, :as => :amount, :type => :bigint)
+    CASE
+      WHEN JSON_VALUE(data, '$.AMOUNT_70') = ''  THEN NULL
+      ELSE CAST(ROUND(CAST(JSON_VALUE(data, '$.AMOUNT_70') as FLOAT)) as SIGNED)
+    END
+  SQL
 
   indexes <<~SQL
     TRIM(
