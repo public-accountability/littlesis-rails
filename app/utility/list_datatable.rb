f8efdad931b3ea3e17d426ca90f32d659fa59436
Rob Lucas <rob@newleftreview.org>
2020-08-13 20:46:51 +0100

    Cache list datatable data

--- app/utility/list_datatable.rb
+++ app/utility/list_datatable.rb
@@ -53,13 +53,13 @@ class ListDatatable
     list_entities = ListEntity.includes(entity: [:extension_definitions, :os_categories]).where(list_id: @list.id, entity: { is_deleted: false })
     @total_entities = list_entities.count
 
-    @data = list_entities.map do |le|
-      entity = le.entity
-      @types = @types.concat(entity.types)
-      @industries = @industries.concat(entity.industries)
-      interlock_ids = interlocks? ? @top_interlocks.select { |l| l[1].include?(entity.id) }.map { |l| l[0] }.uniq.join(',') : nil
-      list_interlock_ids = lists? ? @top_list_interlocks.select { |l| l[1].include?(entity.id) }.map { |l| l[0] }.uniq.join(',') : nil
-      list_entity_data(le, interlock_ids, list_interlock_ids)
+    @data = Rails.cache.fetch(cache_key, expires_in: 2.weeks) do
+      list_entities.map do |le|
+        entity = le.entity
+        @types = @types.concat(entity.types)
+        @industries = @industries.concat(entity.industries)
+        list_entity_data(le, extract_interlock_ids(entity), extract_list_interlock_ids(entity))
+      end
     end
   end
 
@@ -110,5 +110,27 @@ class ListDatatable
   # entities with a rank of 1. Instead they should come at the end of the data table.
   def default_sort_position(entity)
     entity.rank.presence || @total_entities + 1
+  end
+
+  def cache_key
+    "list_datatables/#{@list.id}/#{@list.updated_at}"
+  end
+
+  def extract_interlock_ids(entity)
+    return unless interlocks?
+
+    @top_interlocks
+      .select { |l| l[1].include?(entity.id) }
+      .map { |l| l[0] }
+      .uniq.join(',')
+  end
+
+  def extract_list_interlock_ids(entity)
+    return unless lists?
+
+    @top_list_interlocks
+      .select { |l| l[1].include?(entity.id) }
+      .map { |l| l[0] }
+      .uniq.join(',')
   end
 end
