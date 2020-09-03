bc5b8210928d619038efa99b5d5af11daa94fd7c
Rob Lucas <rob@newleftreview.org>
2020-09-03 13:15:03 +0100

    Move image deletion request actions into deletion requests namespace

--- app/controllers/deletion_requests/base_controller.rb
+++ app/controllers/deletion_requests/base_controller.rb
@@ -4,22 +4,20 @@ module DeletionRequests
   class BaseController < ApplicationController
     before_action :authenticate_user!
     before_action :admins_only, only: [:review, :commit_review]
-    before_action :set_deletion_request, only: [:review, :commit_review]
+    before_action :set_deletion_request, only: [:show, :review, :commit_review]
     before_action :set_decision, only: :commit_review
 
-    # GET /deletion_requests/new
+    def show; end
+
     def new; end
 
-    # POST /deletion_requests
     def create
       NotificationMailer.deletion_request_email(@deletion_request).deliver_later
       redirect_to home_dashboard_path, notice: 'Deletion request sent to admins.'
     end
 
-    # GET /deletion_requests/:id/review
     def review; end
 
-    # POST /deletion_reqeusts/:id/review
     def commit_review
       @deletion_request.send("#{@decision}_by!".to_sym, current_user)
       redirect_to home_dashboard_path, notice: "Deletion request #{@decision}"
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,33 +1,8 @@
 # frozen_string_literal: true
 
 class ImagesController < ApplicationController
-  ADMIN_ACTIONS = %i[deletion_request approve_deletion deny_deletion].freeze
-
   before_action :authenticate_user!
-  before_action -> { check_permission('admin') }, only: ADMIN_ACTIONS
-  before_action :set_image_deletion_request, only: ADMIN_ACTIONS
-  before_action :set_image, only: %i[request_deletion crop update]
-
-  def request_deletion
-    unless ImageDeletionRequest.exists?(image: @image)
-      ImageDeletionRequest.create!(new_image_deletion_request_params)
-    end
-    redirect_back fallback_location: home_dashboard_path,
-                  notice: 'Request is pending'
-  end
-
-  def deletion_request
-  end
-
-  def approve_deletion
-    @image_deletion_request.approved_by!(current_user)
-    redirect_to home_dashboard_path, notice: 'Image deletion request approved'
-  end
-
-  def deny_deletion
-    @image_deletion_request.denied_by!(current_user)
-    redirect_to home_dashboard_path, notice: 'Image deletion request denied'
-  end
+  before_action :set_image, only: %i[crop update]
 
   def crop
     if request.post?
@@ -52,23 +27,12 @@ class ImagesController < ApplicationController
     @image = Image.find(params[:id])
   end
 
-  def set_image_deletion_request
-    @image_deletion_request = ImageDeletionRequest.find(params.require('image_deletion_request_id'))
-  end
-
   def crop_image_params
     params
       .require(:crop)
       .permit(:type, :ratio, :x, :y, :w, :h)
       .to_h
       .symbolize_keys
-  end
-
-  def new_image_deletion_request_params
-    { user: current_user,
-      image: @image,
-      entity_id: params.fetch('entity_id', nil),
-      justification: params.require('justification') }
   end
 
   def image_update_params
