4938765c8a8a441275be66a8de7ad5473c5a7838
Rob Lucas <rob@newleftreview.org>
2020-09-10 15:24:01 +0100

    Set up Chromedriver-based tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,8 +38,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.server = :webrick
-
 RSpec.configure do |config|
   config.expect_with :rspec
   config.mock_with :rspec
