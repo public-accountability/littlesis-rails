c3f5fc7ee8893c4ab06cac5cd816b92c05602844
ziggy <ziggy@littlesis.org>
2020-09-17 16:16:34 -0400

    set Capybara.app_host

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -16,3 +16,4 @@ Capybara.ignore_hidden_elements = false
 Capybara.javascript_driver = :headless_chrome
 Capybara.server = :puma, {Silent: true}  # Remove crummy test output
 Capybara.default_max_wait_time = ENV["CI"] ? 15 : 5
+Capybara.app_host = "#{Lilsis::Application.default_url_options[:protocol]}://#{Lilsis::Application.default_url_options[:host]}"
