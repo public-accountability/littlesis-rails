31fd0b9eb8b9501b8b0a92b798d98af6c59aba13
ziggy <ziggy@littlesis.org>
2020-09-21 17:55:26 -0400

    fix constants in ControllerMacros

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,14 +2,14 @@
 
 module ControllerMacros
   def login_admin
-    before(:each) do
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in RspecExampleHelpers.create_admin_user
+      sign_in RspecHelpers::ExampleMacros.create_admin_user
     end
   end
 
   def login_user(abilities = [:edit])
-    before(:each) do
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       user = FactoryBot.create(:user)
       create(:user_profile, user: user)
@@ -19,21 +19,21 @@ module ControllerMacros
   end
 
   def login_basic_user
-    before(:each) do
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in RspecExampleHelpers.create_basic_user
+      sign_in RspecHelpers::ExampleMacros.create_basic_user
     end
   end
 
   def login_restricted_user
-    before(:each) do
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in RspecExampleHelpers.create_restricted_user
+      sign_in RspecHelpers::ExampleMacros.create_restricted_user
     end
-  end  
+  end
 
   def login_user_without_permissions
-    before(:each) do
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       user = FactoryBot.create(:user)
       create(:user_profile, user: user)
