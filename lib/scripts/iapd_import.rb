e4c74b92247b2edcb89a2132c447f6b9323a9af9
ziggy <ziggy@littlesis.org>
2020-09-24 08:13:41 -0400

    fix iapd import script

--- lib/scripts/iapd_import.rb
+++ lib/scripts/iapd_import.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require Rails.root.join('lib/iapd_importer.rb').to_s
-require Rails.root.join('lib/iapd_processor.rb').to_s
+require Rails.root.join('lib/importers/iapd_importer.rb').to_s
 
-ColorPrinter.print_blue "Importing IAPD data"
 IapdImporter.run
-ColorPrinter.print_blue "Processing IAPD data"
-IapdProcessor.run
