5af67e5089285f7c646e49d6bd8e68d1cc7b4186
ziggy <ziggy@littlesis.org>
2020-10-07 17:30:40 -0400

    rewrite DevelopmentDb

--- lib/development_db.rb
+++ lib/development_db.rb
@@ -3,231 +3,103 @@
 # rubocop:disable Metrics/LineLength
 
 class DevelopmentDb
-  def initialize(path)
-    @out_path = path
-    @db = Rails.configuration.database_configuration['production']
+  # Groupings of tables in our database
+  # :system tables where only the table structure is dumped (a CREATE TABLE statement but without any data)
+  #     These either contain production data unneccessary for development (i.e. delayed_jobs) or are very large (i.e. external_data)
+  # :public is the core LittleSis dataset, although there is still another processing step before it can be distruputed
+  TABLES = Struct.new(:all, :system, :open_secrets, :nys_campaign_finance, :external_data, :legacy, :users, :structure, :development, :public)
+
+  def self.tables
+    @tables ||= TABLES.new.tap do |tables|
+      tables.all = ActiveRecord::Base.connection.tables.freeze
+      tables.system = %w[api_tokens delayed_jobs edited_entities object_tag sphinx_index user_requests versions web_requests user_permissions permission_passes].freeze
+      tables.open_secrets = %w[os_matches os_candidates os_committees os_donations os_entity_category os_entity_donor].freeze
+      tables.nys_campaign_finance = %w[ny_disclosures ny_filer_entities ny_filers unmatched_ny_filers ny_matches].freeze
+      tables.external_data = %w[external_data external_entities external_relationships].freeze
+      tables.legacy = %w[modification modification_field reference reference_excerpt].freeze
+      tables.users = %w[user_profiles users].freeze
+      tables.structure = (tables.system + tables.open_secrets + tables.nys_campaign_finance + tables.external_data).freeze
+      tables.development = (tables.all - tables.structure - tables.users - tables.legacy).freeze
+      tables.public = %w[alias business business_industry business_person common_names degree documents donation education
+                         elected_representative entity extension_definition extension_record external_links family generic
+                         government_body hierarchy link lobbyist membership org ownership person political_candidate
+                         political_fundraising position professional public_company references relationship relationship_category
+                         school social taggings tags transaction].freeze
+    end.freeze
+  end
+
+  delegate :tables, :to => :class
+
+  def initialize(type, debug: false)
+    @type = type
+    @debug = debug
+
+    unless %i[development public open_secrets external_data].include?(@type)
+      raise Exceptions::LittleSisError, 'Invalid DevelopmentDb Type'
+    end
+
+    @dbconfig = Rails.configuration.database_configuration.fetch(Rails.env)
   end
 
   def run
-    create_clean_tables
-    structure_only
-    cleaned
-    full_tables
-    limit
+    case @type
+    when :development
+      dump_user_tables
+      execute mysqldump(tables.development)
+      execute mysqldump(tables.structure, no_data: true)
+    when :open_secrets
+      execute mysqldump(tables.open_secrets)
+    when :external_data
+      execute mysqldump(tables.external_data)
+    when :public
+      execute mysqldump(tables.public)
+    end
   end
 
-  def open_secrets
-    cmd = "mysqldump -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} --skip-comments --single-transaction #{@db['database']} #{OPEN_SECRETS.join(' ')} > #{@out_path}"
-    `#{cmd}`
+  private
+
+  def execute(cmd)
+    cmd += " >> #{filepath}"
+
+    if @debug
+      puts cmd
+    else
+      system cmd, exception: true
+    end
   end
 
-  def create_clean_tables
-    cmd = "mysql -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} #{@db['database']} < #{Rails.root.join('lib', 'sql', 'clean_users.sql')}"
-    `#{cmd}`
+  def mysqldump(tables, no_data: false)
+    cmd = "mysqldump -u #{@dbconfig['username']} -p#{@dbconfig['password']} -h #{@dbconfig['host']}"
+    cmd += ' --single-transaction --skip-lock-tables --quick'
+    cmd += ' --no-data' if no_data
+    cmd += " #{@dbconfig['database']}"
+    cmd += " #{Array.wrap(tables).join(' ')}"
+    cmd
   end
 
-  def full_tables
-    cmd = "mysqldump -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} --skip-comments --single-transaction #{@db['database']} #{FULL_DUMP.join(' ')} >> #{@out_path}"
-    `#{cmd}`
+  def dump_user_tables
+    Utility.execute_sql_file Rails.root.join('lib', 'sql', 'clean_users.sql') # creates clean_users and clean_user_profiles
+
+    tables.users.each do |table|
+      clean_name = "clean_#{table}"
+      # hack to export "clean_user_profiles" as "user_profiles"
+      sed = "sed 's/#{clean_name}/#{table}/g'"
+      execute " #{mysqldump(clean_name)} | #{sed}"
+    end
   end
 
-  def structure_only
-    cmd = "mysqldump -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} --no-data --single-transaction #{@db['database']} #{STRUCTURE.join(' ')} >> #{@out_path}"
-    `#{cmd}`
+  def filepath
+    case @type
+    when :development
+      Rails.root.join('data', "development_db#{LsDate.today}.sql")
+    when :open_secrets
+      Rails.root.join('data', "open_secrets#{LsDate.today}.sql")
+    when :external_data
+      Rails.root.join('data', "external_data#{LsDate.today}.sql")
+    when :public
+      Rails.root.join('data', "public_data_raw#{LsDate.today}.sql")
+    end
   end
-
-  def limit
-    where = '--where="1 limit 5000"'
-    cmd = "mysqldump -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} --skip-comments --single-transaction #{where} #{@db['database']} #{LIMIT.join(' ')} >> #{@out_path}"
-    `#{cmd}`
-  end
-
-  def cleaned
-    CLEANED.each { |table| dump_clean_table(table) }
-  end
-
-  def dump_clean_table(clean_name)
-    normal_name = clean_name.gsub('clean_', '')
-    sed = "sed 's/#{clean_name}/#{normal_name}/g'"
-    cmd = "mysqldump -u #{@db['username']} -p#{@db['password']} -h #{@db['host']} --skip-comments --single-transaction #{@db['database']} #{clean_name} | #{sed} >> #{@out_path}"
-    `#{cmd}`
-  end
-
-  # All tables can be grouped in to 4 categories for export
-  #  1) Tables who'se entire contents can be dumped (i.e. entities)
-  #  2) Table where just the structure needs to exported (i.e. versions)
-  #  3) Tables containing senstive information that must be cleaned first (i.e. users)
-  #  4) Large tables that can be dumpt with LIMIT in order to reduce the export size (i.e. os_donations)
-
-  FULL_DUMP = %w[address
-                 address_category
-                 address_country
-                 address_state
-                 alias
-                 article
-                 article_entities
-                 article_entity
-                 article_source
-                 articles
-                 business
-                 business_industry
-                 business_person
-                 candidate_district
-                 cmp_entities
-                 cmp_relationships
-                 common_names
-                 couple
-                 custom_key
-                 dashboard_bulletins
-                 degree
-                 documents
-                 donation
-                 education
-                 elected_representative
-                 email
-                 entity
-                 entity_fields
-                 extension_definition
-                 extension_record
-                 external_links
-                 family
-                 fields
-                 generic
-                 government_body
-                 help_pages
-                 hierarchy
-                 image
-                 industries
-                 industry
-                 link
-                 lobby_filing
-                 lobby_filing_lobby_issue
-                 lobby_filing_lobbyist
-                 lobby_filing_relationship
-                 lobby_issue
-                 lobbying
-                 lobbyist
-                 ls_list
-                 ls_list_entity
-                 membership
-                 network_map
-                 ny_filer_entities
-                 org
-                 os_category
-                 ownership
-                 pages
-                 person
-                 phone
-                 political_candidate
-                 political_district
-                 political_fundraising
-                 political_fundraising_type
-                 position
-                 professional
-                 public_company
-                 references
-                 relationship
-                 relationship_category
-                 representative
-                 representative_district
-                 schema_migrations
-                 school
-                 social
-                 tag
-                 tags
-                 taggings
-                 toolkit_pages
-                 transaction
-                 user_permissions].freeze
-
-  STRUCTURE = %w[api_request
-                 api_user
-                 api_tokens
-                 chat_user
-                 delayed_jobs
-                 edited_entities
-                 external_data
-                 external_entities
-                 external_relationships
-                 map_annotations
-                 ny_disclosures
-                 ny_filers
-                 ny_matches
-                 object_tag
-                 sphinx_index
-                 unmatched_ny_filers
-                 user_requests
-                 versions
-                 web_requests].freeze
-
-  NON_CLEANED_IGNORED = %w[user_profiles users].freeze
-
-  CLEANED = %w[clean_user_profiles clean_users].freeze
-
-  LIMIT = %w[modification
-             modification_field
-             os_candidates
-             os_committees
-             os_donations
-             os_entity_category
-             os_entity_donor
-             os_matches
-             reference
-             reference_excerpt].freeze
-
-  OPEN_SECRETS = %w[os_candidates os_committees os_donations os_entity_category os_entity_donor os_matches].freeze
-
-  # these tables need to be cleaned afterwards:
-  # entity, alias, relationship, extension_record,
-  # link, taggings, taggings
-  # see: clean_public_data.sql
-  PUBLIC_DATA = %w[
-    alias
-    business
-    business_industry
-    business_person
-    candidate_district
-    couple
-    degree
-    donation
-    education
-    elected_representative
-    entity
-    extension_definition
-    extension_record
-    external_links
-    family
-    generic
-    government_body
-    hierarchy
-    industries
-    industry
-    link
-    lobbyist
-    membership
-    org
-    ownership
-    person
-    political_candidate
-    political_district
-    political_fundraising
-    political_fundraising_type
-    position
-    professional
-    public_company
-    relationship_category
-    representative
-    representative_district
-    school
-    social
-    tags
-    taggings
-    transaction
-    entity
-    relationship
-    references
-    documents
-  ].freeze
 end
 
 # rubocop:enable Metrics/LineLength
