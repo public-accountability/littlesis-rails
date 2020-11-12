32c982609a914a7f57d67cb4f519133fde806ffc
ziggy <ziggy@elephant-bird.net>
2020-11-12 14:21:04 -0500

    remove NyDisclosure tests and fix FECController test

--- spec/controllers/fec_controller_spec.rb
+++ spec/controllers/fec_controller_spec.rb
@@ -10,7 +10,7 @@ describe FECController, type: :controller do
     end
 
     it do
-      is_expected.to route(:post, '/fec/donor_match').to(action: :donor_match)
+      is_expected.to route(:post, '/fec/entities/123/donor_match').to(action: :donor_match, id: 123)
     end
 
     it do
