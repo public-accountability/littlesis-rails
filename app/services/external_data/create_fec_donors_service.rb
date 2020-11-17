0c7a49c36925b3951acfeeb6934f3b3afcfbb8c3
ziggy <ziggy@elephant-bird.net>
2020-11-17 12:46:33 -0500

    don't run in parallel in the test env

--- app/services/external_data/create_fec_donors_service.rb
+++ app/services/external_data/create_fec_donors_service.rb
@@ -13,21 +13,29 @@ class ExternalData
         end
       end
 
-      ApplicationRecord.connection.reconnect!
+      ApplicationRecord.connection.reconnect! unless Rails.env.test?
 
       ExternalData.fec_donor.find_in_batches(batch_size: BATCH_SIZE) do |batch|
         parallel_in_groups(batch) do |donors|
-          donors.each(&:update_fec_donor_data!)
+          donors.each do |donor|
+            donor.update_fec_donor_data!
+          end
         end
       end
 
-      ApplicationRecord.connection.reconnect!
+      ApplicationRecord.connection.reconnect! unless Rails.env.test?
     end
 
-    def self.parallel_in_groups(batch)
-      Parallel.each(batch.in_groups(THREAD_COUNT), in_threads: THREAD_COUNT) do |batch_part|
-        ApplicationRecord.connection_pool.with_connection do
-          yield batch_part.compact
+    if Rails.env.test?
+      def self.parallel_in_groups(batch)
+        yield batch
+      end
+    else
+      def self.parallel_in_groups(batch)
+        Parallel.each(batch.in_groups(THREAD_COUNT), in_threads: THREAD_COUNT) do |batch_part|
+          ApplicationRecord.connection_pool.with_connection do
+            yield batch_part.compact
+          end
         end
       end
     end
