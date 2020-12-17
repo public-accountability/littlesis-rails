62008572f46d45f5afefa27b1a821ba560e2f7da
Rob Lucas <rob@newleftreview.org>
2020-12-17 13:42:40 +0000

    Move polyfill.js to Webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import './polyfill'
+import 'node-fetch'
 import jquery from 'jquery'
 import 'popper.js'
 import 'bootstrap'
