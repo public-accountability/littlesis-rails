514ee53e9d97c3aa1fcdca9ffc01fdbcce844a25
Rob Lucas <rob@newleftreview.org>
2021-01-25 11:49:41 +0000

    Refactor home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,3 +1,6 @@
+require 'email_spec'
+require 'email_spec/rspec'
+
 describe HomeController, type: :controller do
   describe 'routes' do
     it { is_expected.to route(:get, '/flag').to(action: :flag) }
@@ -24,49 +27,63 @@ describe HomeController, type: :controller do
 
     describe 'POST' do
       context 'when no email provided' do
-
         before do
-          expect(NotificationMailer).not_to receive(:flag_email)
           post :flag, params: { url: 'http://url', message: 'hey' }
         end
 
         it { is_expected.to set_flash.now[:alert] }
         it { is_expected.to render_template('flag') }
+
+        it "doesn't send an email" do
+          expect { last_email_sent }.to raise_error(RuntimeError, 'No email has been sent!')
+        end
       end
 
       context 'when no message provided' do
         before do
-          expect(NotificationMailer).not_to receive(:flag_email)
           post :flag, params: { url: 'http://url', email: 'test@example.com' }
         end
+
         it { is_expected.to set_flash.now[:alert] }
         it { is_expected.to render_template('flag') }
+
+        it "doesn't send an email" do
+          expect { last_email_sent }.to raise_error(RuntimeError, 'No email has been sent!')
+        end
       end
 
-      context 'when all required params provided' do
+      context 'when all required params provided', :run_jobs do
         let(:params) { { 'url' => 'http://url', 'email' => 'test@example.com', 'message' => 'hey' } }
 
         before do
-          expect(NotificationMailer)
-            .to receive(:flag_email).with(params).and_return(double(deliver_later: nil))
           post :flag, params: { url: 'http://url', email: 'test@example.com', message: 'hey' }
         end
 
         it { is_expected.not_to set_flash.now[:alert] }
         it { is_expected.to set_flash.now[:notice] }
         it { is_expected.to render_template('flag') }
+
+        it 'sends an email' do
+          expect(last_email_sent).to have_subject('Flag for Review')
+        end
       end
     end
   end
 
   describe 'pai_signup_ip_limit' do
     let(:ip) { Faker::Internet.ip_v6_address }
+    let(:logger) { instance_double(ActiveSupport::Logger) }
+
+    before do
+      allow(Rails).to receive(:logger).and_return(logger)
+      allow(logger).to receive(:warn)
+    end
 
     it 'denies access after 4 requests' do
       4.times { controller.send(:pai_signup_ip_limit, ip) }
       expect(Rails.cache.read("pai_signup_request_count_for_#{ip}")).to eq 4
-      expect(Rails.logger).to receive(:warn).with("#{ip} has submitted too many requests this hour!").once
       expect { controller.send(:pai_signup_ip_limit, ip) }.to raise_error(Exceptions::PermissionError)
+      expect(logger).to have_received(:warn).with("#{ip} has submitted too many requests this hour!").once
     end
   end
 end
