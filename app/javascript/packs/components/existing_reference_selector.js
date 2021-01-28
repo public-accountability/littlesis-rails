d2235095f41800a2d6969eb0459070221abbaed4
ziggy <ziggy@littlesis.org>
2021-01-28 13:46:55 -0500

    fix relationship references

--- app/javascript/packs/components/existing_reference_selector.js
+++ app/javascript/packs/components/existing_reference_selector.js
@@ -1,246 +1,230 @@
+import $ from 'jquery'
+import Bloodhound from 'bloodhound-js'
+import mustache from 'mustache'
+
+// CONSTANTS
+const REFERENCES_PER_PAGE = 75
+const REQUEST_URL = '/references/recent'
+const TYPEAHEAD_OPTIONS = {
+  minLength: 0,
+  highlight: true,
+  limit: 8
+}
+
 /**
- * Widget to select an existing reference
+ * Reference Widget
+ * Use:  new ReferenceWidget([entity_ids], { options })
+ * @param {Array|Integer|String} entityIds
+ * @param {Object} userOptions
  */
-(function (root, factory) {
-  if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('../common/utility'));
-  } else {
-    root.ExistingReferenceWidget = factory(root.jQuery, root.utility);
-  }
-}(this, function (utility) {
-  // CONSTANTS
-  var REFERENCES_PER_PAGE = 75;
-  var REQUEST_URL = '/references/recent';
-  var TYPEAHEAD_OPTIONS = {
-    minLength: 0,
-    highlight: true,
-    limit: 8
-  };
+function ExistingReferenceWidget(entityIds, userOptions) {
+  this.options = mergeOptions(ExistingReferenceWidget.DEFAULT_OPTIONS, userOptions)
+  this.entityIds = [].concat(entityIds).map(function(n) { return Number(n); })
+  this.documents = null
+  this.selection = null
+  this.render = this._render.bind(this)
+  this.init()
+}
 
-  /**
-   * Reference Widget
-   * Use:  new ReferenceWidget([entity_ids], { options })
-   * @param {Array|Integer|String} entityIds
-   * @param {Object} userOptions
-   */
-  function ExistingReferenceWidget(entityIds, userOptions) {
-    this.options = mergeOptions(ExistingReferenceWidget.DEFAULT_OPTIONS, userOptions);
-    this.entityIds = [].concat(entityIds).map(function(n) { return Number(n); });
-    this.documents = null;
-    this.selection = null;
-    this.render = this._render.bind(this);
-    this.init();
-  }
+// STATIC VALUES
+ExistingReferenceWidget.TYPEAHEAD_INPUT_ID = 'ref-widget-typeahead'
+ExistingReferenceWidget.TYPEAHEAD_INPUT_SELECTOR = '#' + ExistingReferenceWidget.TYPEAHEAD_INPUT_ID
+ExistingReferenceWidget.DEFAULT_OPTIONS = {
+  "containerDiv": "#reference-widget-container",
+  "afterRender": function() {},
+  "afterSelect": function() {}
+}
 
-  // STATIC VALUES
-  ExistingReferenceWidget.TYPEAHEAD_INPUT_ID = 'ref-widget-typeahead';
-  ExistingReferenceWidget.TYPEAHEAD_INPUT_SELECTOR = '#' + ExistingReferenceWidget.TYPEAHEAD_INPUT_ID;
-  ExistingReferenceWidget.DEFAULT_OPTIONS = {
-    "containerDiv": "#reference-widget-container",
-    "afterRender": function() {},
-    "afterSelect": function() {}
-  };
+ExistingReferenceWidget.prototype.init = function() {
+  this.getDocs().then(this.render);
+};
 
-  /**
-   * Initalize the ExistingReferenceWidget
-   */
-  ExistingReferenceWidget.prototype.init = function() {
-    this.getDocs().then(this.render);
-  };
+/**
+ * Replaces contents of container with a typeahead
+ */
+ExistingReferenceWidget.prototype._render = function() {
+  var self = this;
+  $(this.options.containerDiv).html(this._typeaheadInput());
 
-  /**
-   * Replaces contents of container with a typeahead
-   */
-  ExistingReferenceWidget.prototype._render = function() {
-    var self = this;
-    $(this.options.containerDiv).html(this._typeaheadInput());
-
-    // render the typeahead in to the div
-    $(ExistingReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)
-      .typeahead(TYPEAHEAD_OPTIONS, this._typeaheadConfig())
-      .on('typeahead:selected', function (e, datum) {
-	// set 'selection' property after picked
-	self.selection = datum;
-	self.options.afterSelect(datum);
-      })
-      .on('typeahead:render', function (e) {
-	// 'unselect' if suggestion is re-rendered
-	self.selection = null;
-      });
-
-    this.options.afterRender();
-  };
-
-  ///////////////////////////
-  // TYPEAHEAD COMPONENTS ///
-  //////////////////////////
-
-  /**
-   * Prepares Bloodhound search with provided references data
-   * @param {Array[References]} data
-   * @returns {Bloodhound}
-   */
-  function documentBloodhound(data) {
-    return new Bloodhound({
-      local: data,
-      identify: function(doc) { return doc.id; },
-      queryTokenizer: function(q) {
-	return Bloodhound.tokenizers.whitespace(q.toLowerCase());
-      },
-      datumTokenizer: function(d) {
-	return flatten(
-	  [d.name, d.url]
-	    .filter(function(x) { return x !== null; })
-	    .map(function(x) {
-	      return Bloodhound.tokenizers.whitespace(x.toLowerCase());
-	    })
-	);
-      }
+  // render the typeahead in to the div
+  $(ExistingReferenceWidget.TYPEAHEAD_INPUT_SELECTOR)
+    .typeahead(TYPEAHEAD_OPTIONS, this._typeaheadConfig())
+    .on('typeahead:selected', function (e, datum) {
+      // set 'selection' property after picked
+      self.selection = datum;
+      self.options.afterSelect(datum);
+    })
+    .on('typeahead:render', function (e) {
+      // 'unselect' if suggestion is re-rendered
+      self.selection = null;
     });
-  }
 
-  /**
-   * Returns function to be used as the 'source' of typeahead
-   * see https://typeahead.js.org/examples/#default-suggestions for docs on default suggestions
-   * @param {Array[References]} data
-   * @returns {Function}
-   */
-  function referenceSource(data) {
-    var bloodhound = documentBloodhound(data);
-    var defaultIds = data.slice(0,8).map(function(doc) { return doc.id; });
-    return function(q, sync) {
-      if (q === '') {
-	sync(bloodhound.get(defaultIds));
-      } else {
-	bloodhound.search(q, sync);
-      }
-    };
-  }
+  this.options.afterRender();
+};
 
-  /**
-   * Provides a configuartion object for typeahead
-   * Requires `this.documents` to be set
-   * @param {Array} references
-   * @returns {Object} Configuration for typeahead
-   */
-  ExistingReferenceWidget.prototype._typeaheadConfig = function() {
-    if (!Array.isArray(this.documents)) {
-      throw 'Documents are missing or invalid :(';
+///////////////////////////
+// TYPEAHEAD COMPONENTS ///
+//////////////////////////
+
+/**
+ * Prepares Bloodhound search with provided references data
+ * @param {Array[References]} data
+ * @returns {Bloodhound}
+ */
+function documentBloodhound(data) {
+  return new Bloodhound({
+    local: data,
+    identify: function(doc) { return doc.id; },
+    queryTokenizer: function(q) {
+      return Bloodhound.tokenizers.whitespace(q.toLowerCase());
+    },
+    datumTokenizer: function(d) {
+      return flatten(
+	[d.name, d.url]
+	  .filter(function(x) { return x !== null; })
+	  .map(function(x) {
+	    return Bloodhound.tokenizers.whitespace(x.toLowerCase());
+	  })
+      );
     }
+  });
+}
 
-    return {
-      name: 'references',
-      source: referenceSource(this.documents),
-      displayKey: 'name',
-      templates: {
-	empty: '<span class="reference-suggestion"><span class="reference-empty">No matches found</span></span>',
-	suggestion: suggestion
-      }
-    };
-  };
-
-
-  /**
-   * Retrives recent documents via an ajax call to /references/recent
-   * Sets this.documents if successful.
-   * @returns {Promise}
-   */
-  ExistingReferenceWidget.prototype.getDocs = function() {
-    var self = this;
-    var url = this._recentReferencesUrl();
-
-    return new Promise(function(resolve, reject){
-      $.getJSON(url)
-	.done(function(data) {
-	  self.documents = data;
-	  resolve(data);
-	})
-	.fail(function(){
-	  reject('failed to get references from /references/recent');
-	});
-    });
-  };
-
-
-  /**
-   * Url for request
-   * @returns {String}
-   */
-  ExistingReferenceWidget.prototype._recentReferencesUrl = function() {
-    var params =  $.param({
-      "entity_ids":  this.entityIds,
-      "per_page": REFERENCES_PER_PAGE,
-      "exclude_type": 'fec'
-    });
-
-    return REQUEST_URL + '?' + params;
-  };
-
-
-  ///////////////////
-  // DOM ELEMENTS ///
-  //////////////////
-
-  /**
-   * Input with id ref-widget-typeahead
-   * @returns {<input>}
-   */
-  ExistingReferenceWidget.prototype._typeaheadInput = function() {
-    return $('<input>', {
-      "type": 'text',
-      "placeholder": 'Search recent references',
-      "id": ExistingReferenceWidget.TYPEAHEAD_INPUT_ID,
-      "class": 'reference-typeahead'
-    });
-  };
-
-
-  var referenceVisitLink = [
-    '<div class="reference-visit-link">',
-    '<a href="{{url}}" target="_blank">',
-    '<span class="glyphicon glyphicon-new-window"></span>',
-    '</a></div>'
-  ].join('');
-
-  var suggestionRender = [
-      '<div class="reference-suggestion" title="{{url}}">',
-      referenceVisitLink,
-      '<div class="reference-suggestion-name">{{name}}</div>',
-      '<div class="reference-suggestion-url">{{trimUrl}}</div>',
-      '</div>'
-    ].join('');
-
-  /**
-   *
-   * @param {Object} doc
-   * @returns {String} html for suggestion
-   */
-    function suggestion(doc) {
-      var data = Object.assign({}, doc, {trimUrl: trimUrl(doc.url)});
-      return mustache.render(suggestionRender, data)
-  }
-
-
-  ///////////////
-  // HELPERS ///
-  /////////////
-
-  function trimUrl(url) {
-    var without_schema = url.replace('https://', '').replace('http://', '');
-    if (without_schema.length < 38) {
-      return without_schema;
+/**
+ * Returns function to be used as the 'source' of typeahead
+ * see https://typeahead.js.org/examples/#default-suggestions for docs on default suggestions
+ * @param {Array[References]} data
+ * @returns {Function}
+ */
+function referenceSource(data) {
+  var bloodhound = documentBloodhound(data);
+  var defaultIds = data.slice(0,8).map(function(doc) { return doc.id; });
+  return function(q, sync) {
+    if (q === '') {
+      sync(bloodhound.get(defaultIds));
     } else {
-      return (without_schema.slice(0,35) + '...');
+      bloodhound.search(q, sync);
     }
-  }
-
-  function mergeOptions(defaultOptions, provided) {
-    return Boolean(provided) ? Object.assign({}, defaultOptions, provided) : defaultOptions;
   };
+}
 
-  function flatten(arrays) {
-    return Array.prototype.concat.apply([], arrays);
+/**
+ * Provides a configuartion object for typeahead
+ * Requires `this.documents` to be set
+ * @param {Array} references
+ * @returns {Object} Configuration for typeahead
+ */
+ExistingReferenceWidget.prototype._typeaheadConfig = function() {
+  if (!Array.isArray(this.documents)) {
+    throw 'Documents are missing or invalid :(';
   }
 
-  return ExistingReferenceWidget;  // Return the constructor
-}));
+  return {
+    name: 'references',
+    source: referenceSource(this.documents),
+    displayKey: 'name',
+    templates: {
+      empty: '<span class="reference-suggestion"><span class="reference-empty">No matches found</span></span>',
+      suggestion: suggestion
+    }
+  };
+};
+
+
+/**
+ * Retrives recent documents via an ajax call to /references/recent
+ * Sets this.documents if successful.
+ * @returns {Promise}
+ */
+ExistingReferenceWidget.prototype.getDocs = function() {
+  var self = this;
+  var url = this._recentReferencesUrl();
+
+  return new Promise(function(resolve, reject){
+    $.getJSON(url)
+     .done(function(data) {
+       self.documents = data;
+       resolve(data);
+     })
+     .fail(function(){
+       reject('failed to get references from /references/recent');
+     });
+  });
+};
+
+
+ExistingReferenceWidget.prototype._recentReferencesUrl = function() {
+  return REQUEST_URL + '?' +  $.param({ "entity_ids": this.entityIds,
+                                        "per_page": REFERENCES_PER_PAGE,
+                                        "exclude_type": 'fec' })
+}
+
+
+///////////////////
+// DOM ELEMENTS ///
+//////////////////
+
+/**
+ * Input with id ref-widget-typeahead
+ * @returns {<input>}
+ */
+ExistingReferenceWidget.prototype._typeaheadInput = function() {
+  return $('<input>', {
+    "type": 'text',
+    "placeholder": 'Search recent references',
+    "id": ExistingReferenceWidget.TYPEAHEAD_INPUT_ID,
+    "class": 'reference-typeahead'
+  });
+};
+
+
+const referenceVisitLink = [
+  '<div class="reference-visit-link">',
+  '<a href="{{url}}" target="_blank">',
+  '<span class="glyphicon glyphicon-new-window"></span>',
+  '</a></div>'
+].join('')
+
+const suggestionRender = [
+  '<div class="reference-suggestion" title="{{url}}">',
+  referenceVisitLink,
+  '<div class="reference-suggestion-name">{{name}}</div>',
+  '<div class="reference-suggestion-url">{{trimUrl}}</div>',
+  '</div>'
+].join('')
+
+/**
+ *
+ * @param {Object} doc
+ * @returns {String} html for suggestion
+ */
+function suggestion(doc) {
+  var data = Object.assign({}, doc, {
+    trimUrl: trimUrl(doc.url)
+  })
+
+
+  return mustache.render(suggestionRender, data)
+}
+
+
+function trimUrl(url) {
+  if (!url) { return '' }
+
+  var without_schema = url.replace('https://', '').replace('http://', '')
+  if (without_schema.length < 38) {
+    return without_schema
+  } else {
+    return (without_schema.slice(0,35) + '...')
+  }
+}
+
+function mergeOptions(defaultOptions, provided) {
+  return Boolean(provided) ? Object.assign({}, defaultOptions, provided) : defaultOptions;
+}
+
+function flatten(arrays) {
+  return Array.prototype.concat.apply([], arrays);
+}
+
+export default ExistingReferenceWidget
--- app/javascript/packs/components/new_reference_form.js
+++ app/javascript/packs/components/new_reference_form.js
@@ -1,145 +1,133 @@
+const LABEL_CLASS = 'col-sm-3 control-label'
+const INPUT_CLASS = 'form-control text-left'
+const DEFAULT_COL_CLASS = 'col-sm-9'
+const FIELDS_SHOW = [
+  [ 'Url*', 'reference-url', 'url' ],
+  [ 'Name*', 'reference-name', 'text' ],
+]
+const FIELDS_COLLAPSE = [
+  [ 'Publication Date', 'reference-date', 'text'],
+  [ 'Type', 'reference-type', 'select', 'col-sm-6'],
+  [ 'Excerpt', 'reference-excerpt', 'textarea' ]
+]
+
+
 /**
- * Widget to select an existing reference or create a new one
+ * Form to create a new reference
+ *   Use: new NewReferenceForm('#container');
+ * @param {String} containerDiv Div to hold the container
  */
-(function (root, factory) {
-  if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('jquery'));
-  } else {
-    root.NewReferenceForm = factory(root.jQuery);
-  }
-}(this, function ($, utility) {
-  var LABEL_CLASS = 'col-sm-3 control-label';
-  var INPUT_CLASS = 'form-control text-left';
-  var DEFAULT_COL_CLASS = 'col-sm-9';
-  var FIELDS_SHOW = [
-    [ 'Url*', 'reference-url', 'url' ],
-    [ 'Name*', 'reference-name', 'text' ],
-  ];
-  var FIELDS_COLLAPSE = [
-    [ 'Publication Date', 'reference-date', 'text'],
-    [ 'Type', 'reference-type', 'select', 'col-sm-6'],
-    [ 'Excerpt', 'reference-excerpt', 'textarea' ]
-  ];
+function NewReferenceForm(containerDiv){
+  this.containerDiv = containerDiv
+  $(containerDiv).empty()
+  render(containerDiv)
+}
 
+NewReferenceForm.prototype.value = function (){
+  return {
+    "url": $('#reference-url').val(),
+    "name": $('#reference-name').val(),
+    "date": $('#reference-date').val(),
+    "type": $('#reference-type').val(),
+    "excerpt": $("#reference-excerpt").val()
+  };
+};
 
-  /**
-   * Form to create a new reference
-   *   Use: new NewReferenceForm('#container');
-   * @param {String} containerDiv Div to hold the container
-   */
-  function NewReferenceForm(containerDiv){
-    this.containerDiv = containerDiv;
-    $(containerDiv).empty();
-    render(containerDiv);
-  }
+function render(div) {
+  $(div).append(
+    $('<div>', { "class": 'new-reference-form-container'})
+      .append(form())
+  );
 
-  NewReferenceForm.prototype.value = function (){
-    return {
-      "url": $('#reference-url').val(),
-      "name": $('#reference-name').val(),
-      "date": $('#reference-date').val(),
-      "type": $('#reference-type').val(),
-      "excerpt": $("#reference-excerpt").val()
-    };
+  $('#collapseReference').on('show.bs.collapse', function(){
+    $('.collapse-toggle').toggle();
+  });
+
+  $('#collapseReference').on('hide.bs.collapse', function(){
+    $('.collapse-toggle').toggle();
+  });
+}
+
+function form() {
+  return FIELDS_SHOW.map(fieldHtml)
+                    .concat(collapse())
+                    .concat(
+	              collapseFieldsContainer().append(FIELDS_COLLAPSE.map(fieldHtml))
+                    );
+}
+
+function fieldHtml(x) {
+  return inputAndLabel(x[0], x[1], x[2], x[3]);
+}
+
+function inputAndLabel(text, id, type, colClass) {
+  return wrapInFormGroup(
+    $('<div>', { "class": 'row'})
+      .append(label(text, id))
+      .append(input(id, type, colClass))
+  );
+}
+
+function wrapInFormGroup(html) {
+  return $('<div>',  {"class": 'form-group'}).append(html);
+}
+
+function label(text, for_attr) {
+  return $('<label>', { "class": LABEL_CLASS, "for": for_attr, "text": text });
+}
+
+function input(id, type, colClass) {
+  if (typeof colClass === 'undefined') {
+    colClass = DEFAULT_COL_CLASS;
   };
 
-  function render(div) {
-    $(div).append(
-      $('<div>', { "class": 'new-reference-form-container'})
-	.append(form())
-    );
+  var div = $('<div>', { "class": colClass});
 
-    $('#collapseReference').on('show.bs.collapse', function(){
-      $('.collapse-toggle').toggle();
-    });
-
-    $('#collapseReference').on('hide.bs.collapse', function(){
-      $('.collapse-toggle').toggle();
-    });
+  if (['url', 'text'].includes(type)) {
+    div.append(urlTextInput(id, type));
+  } else if (type === 'select') {
+    div.append(select());
+  } else if (type === 'textarea') {
+    div.append(textArea(id));
   }
 
-  function form() {
-    return FIELDS_SHOW.map(fieldHtml)
-      .concat(collapse())
-      .concat(
-	collapseFieldsContainer().append(FIELDS_COLLAPSE.map(fieldHtml))
-      );
-  }
+  return div;
+}
 
-  function fieldHtml(x) {
-    return inputAndLabel(x[0], x[1], x[2], x[3]);
-  }
+function urlTextInput(id, type) {
+  return $('<input>', { "class": INPUT_CLASS, "type": type, "id": id });
+}
 
-  function inputAndLabel(text, id, type, colClass) {
-    return wrapInFormGroup(
-      $('<div>', { "class": 'row'})
-	.append(label(text, id))
-	.append(input(id, type, colClass))
-    );
-  }
+function select() {
+  return [ '<select class="form-control" id="reference-type">',
+	   '<option value="1">Generic</option>',
+	   '<option value="3">Newspaper</option>',
+	   '<option value="4">Government Document</option>',
+	   '</select>'
+	 ].join('');
+}
 
-  function wrapInFormGroup(html) {
-    return $('<div>',  {"class": 'form-group'}).append(html);
-  }
+function textArea(id) {
+  return $('<textarea>', { "name": id, "id": id, "class": 'form-control', "cols": 35, "rows": 6 });
+}
 
-  function label(text, for_attr) {
-    return $('<label>', { "class": LABEL_CLASS, "for": for_attr, "text": text });
-  }
+function collapseFieldsContainer() {
+  return $('<div>', { "class": "collapse", "id": "collapseReference", "aria-expanded": "true"});
+}
 
-  function input(id, type, colClass) {
-    if (typeof colClass === 'undefined') {
-      colClass = DEFAULT_COL_CLASS;
-    };
+var toggle = [ '<span class="collapse-toggle" >More <span class="glyphicon glyphicon-menu-down icon-link" aria-hidden="true"></span></span>',
+	       '<span class="collapse-toggle" style="display: none;">Less <span class="glyphicon glyphicon-menu-up icon-link" aria-hidden="true"></span></span>'
+	     ].join('');
 
-    var div = $('<div>', { "class": colClass});
-
-    if (['url', 'text'].includes(type)) {
-      div.append(urlTextInput(id, type));
-    } else if (type === 'select') {
-      div.append(select());
-    } else if (type === 'textarea') {
-      div.append(textArea(id));
-    }
-
-    return div;
-  }
-
-  function urlTextInput(id, type) {
-    return $('<input>', { "class": INPUT_CLASS, "type": type, "id": id });
-  }
-
-  function select() {
-    return [ '<select class="form-control" id="reference-type">',
-	     '<option value="1">Generic</option>',
-	     '<option value="3">Newspaper</option>',
-	     '<option value="4">Government Document</option>',
-	     '</select>'
-	   ].join('');
-  }
-
-  function textArea(id) {
-    return $('<textarea>', { "name": id, "id": id, "class": 'form-control', "cols": 35, "rows": 6 });
-  }
-
-  function collapseFieldsContainer() {
-    return $('<div>', { "class": "collapse", "id": "collapseReference", "aria-expanded": "true"});
-  }
-  
-  var toggle = [ '<span class="collapse-toggle" >More <span class="glyphicon glyphicon-menu-down icon-link" aria-hidden="true"></span></span>',
-		  '<span class="collapse-toggle" style="display: none;">Less <span class="glyphicon glyphicon-menu-up icon-link" aria-hidden="true"></span></span>'
-		].join('');
+function collapse() {
+  return wrapInFormGroup(
+    $('<div>', { "class": 'col-sm-4' })
+      .append(
+	$('<a>', { "data-toggle": 'collapse', "href": "#collapseReference",  "aria-expanded": "true", "aria-controls": "collapseReference",  "id": "toggle-reference-collapse"})
+	  .append(toggle)
+      )
+  );
+}
 
 
-  function collapse() {
-    return wrapInFormGroup(
-      $('<div>', { "class": 'col-sm-4' })
-	.append(
-	  $('<a>', { "data-toggle": 'collapse', "href": "#collapseReference",  "aria-expanded": "true", "aria-controls": "collapseReference",  "id": "toggle-reference-collapse"})
-	    .append(toggle)
-	)
-    );
-  }
-  
-  return NewReferenceForm;
-
-}));
+export default NewReferenceForm;
