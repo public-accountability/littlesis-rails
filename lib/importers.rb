82355e49411bbdf97ffa10fd4dd262e49a479d2b
ziggy <ziggy@littlesis.org>
2021-02-03 09:08:30 -0500

    remove legacy external data importer and test

--- lib/importers.rb
+++ lib/importers.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'importers/congress_importer'
 require_relative 'importers/iapd_importer'
 require_relative 'importers/nycc_importer'
-require_relative 'importers/nys_disclosure_importer'
 require_relative 'importers/nys_filer_importer'
 
 module Importers
