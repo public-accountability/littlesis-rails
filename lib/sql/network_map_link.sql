d0d4385c2e3fdce325833e9d852d1d7b15a16212
ziggy <ziggy@littlesis.org>
2021-02-12 10:28:52 -0500

    rewrite user maps sql function

--- lib/sql/network_map_link.sql
+++ lib/sql/network_map_link.sql
@@ -1,15 +1,9 @@
-DELIMITER //
-
-CREATE OR REPLACE FUNCTION network_map_link (id BIGINT, title VARCHAR(100))
-RETURNS TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' DETERMINISTIC
-BEGIN
-  DECLARE param varchar(250);
-  SET param = LCASE(REPLACE(REPLACE(TRIM(title), ' ', '-'), '/', '_'));
-  RETURN CONCAT('<a target="_blank" href="/maps/',
-  	 	CAST(id as CHAR), '-', param,
-		'">', TRIM(title), '</a>');
-END
-
-//
-
-DELIMITER ;
+CREATE OR REPLACE FUNCTION network_map_link(id bigint, title text) RETURNS TEXT AS $$
+  SELECT CONCAT('<a target="_blank" href="/maps/',
+                CAST(id as text),
+                '-',
+                LOWER(REPLACE(REPLACE(TRIM(title), ' ', '-'), '/', '_')),
+                '">',
+                TRIM(title),
+                '</a>');
+$$ LANGUAGE SQL IMMUTABLE;
