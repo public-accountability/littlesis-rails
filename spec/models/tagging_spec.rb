429f64f1377038f9604327cb9fba5cb9e7f1ebd3
ziggy <ziggy@littlesis.org>
2021-02-16 07:06:27 -0500

    remove before(:all) from tagging spec

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,22 +1,20 @@
 describe Tagging, type: :model do
-  it { should have_db_column(:tag_id) }
-  it { should have_db_column(:tagable_class) }
-  it { should have_db_column(:tagable_id) }
-  it { should have_db_column(:last_user_id) }
-
-  it { should validate_presence_of(:tag_id) }
-  it { should validate_presence_of(:tagable_class) }
-  it { should validate_presence_of(:tagable_id) }
-
-  it { should belong_to(:tag) }
-  it { should belong_to(:last_user) }
-
-  before(:all) { @user = create(:user) }
-  after(:all) { @user.delete }
-
-  let(:org) { create(:org) }
+  let(:user) { create_basic_user }
+  let(:org) { create(:entity_org) }
   let(:tag) { create(:tag) }
   let(:list) { create(:list) }
+
+  it { is_expected.to have_db_column(:tag_id) }
+  it { is_expected.to have_db_column(:tagable_class) }
+  it { is_expected.to have_db_column(:tagable_id) }
+  it { is_expected.to have_db_column(:last_user_id) }
+
+  it { is_expected.to validate_presence_of(:tag_id) }
+  it { is_expected.to validate_presence_of(:tagable_class) }
+  it { is_expected.to validate_presence_of(:tagable_id) }
+
+  it { is_expected.to belong_to(:tag) }
+  it { is_expected.to belong_to(:last_user) }
 
   describe 'update entity timestamp' do
     it 'updates entity timestamp after creating a tagging' do
@@ -25,7 +23,7 @@ describe Tagging, type: :model do
     end
 
     it 'sets last_user_id to be the system\'s default user' do
-      org.update_columns(updated_at: 1.day.ago, last_user_id: @user.id)
+      org.update_columns(updated_at: 1.day.ago, last_user_id: user.id)
       expect { org.add_tag(tag.id) }
         .to change { org.reload.last_user_id }.to(APP_CONFIG['system_user_id'])
     end
@@ -34,6 +32,7 @@ describe Tagging, type: :model do
   with_versioning do
     context 'when an has been tagged entity' do
       before { org.add_tag(tag.id) }
+
       it 'the tagging\'s version records entity id' do
         expect(org.taggings.first.versions.first.entity1_id).to eql org.id
       end
