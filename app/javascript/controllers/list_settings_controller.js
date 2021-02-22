3963d527778ac1b46966da003a5f11865333755e
Rob Lucas <rob@newleftreview.org>
2021-02-22 18:24:53 +0000

    Refactor list settings JS

---
+++ app/javascript/controllers/list_settings_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static values = { listAccessName: String }
+
+  connect() {
+    showHelp(this.listAccessNameValue)
+  }
+
+  switchAccess(event) {
+    markSelected(event.target)
+    showHelp(event.target.getAttribute('value'))
+  }
+}
+
+function markSelected(element) {
+  $('.btn-primary').removeClass('btn-primary')
+  $(element).addClass('btn-primary')
+}
+
+function showHelp(name){
+  $('.list-access-level-help').hide()
+  $(`#list-access-level-help-${name}`).show()
+}
