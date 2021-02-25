60230669dd61974334d00acf693654579717c102
ziggy <ziggy@littlesis.org>
2021-02-25 13:12:05 -0500

    don't send user signup emails

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -12,21 +12,19 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     super do |user|
-      # send welcome email to user - views/user_mailer/welcome_email
       if user.errors.empty?
-        UserMailer.welcome_email(user).deliver_later
         user.create_default_permissions
+        # send welcome email to user - views/user_mailer/welcome_email
+        UserMailer.welcome_email(user).deliver_later
+        # add to action network mailing list
+        NewsletterSignupJob.perform_later(user) if user.newsletter
 
-        # If the user's request is from a spamy ip address then automatically restrict the user
-        # and don't create a chat account
-        if IpBlocker.restricted?(request.remote_ip)
-          user.update(is_restricted: true)
-        else
-          # TODO: combine these into a unifed "post-signup" method
-          NotificationMailer.signup_email(user).deliver_later
-          NewsletterSignupJob.perform_later(user)
-        end
+        # User Sign Up to admin@littlesis.org
+        # NotificationMailer.signup_email(user).deliver_later
 
+        # if IpBlocker.restricted?(request.remote_ip)
+        #   user.update(is_restricted: true)
+        # end
       end
     end
   end
@@ -42,5 +40,4 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, resource)
     home_dashboard_path
   end
-
 end
