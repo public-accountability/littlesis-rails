3cb6813a75848f664c9682e1dcf804b9f605d0b6
Rob Lucas <roblucas@insicuri.net>
2021-03-04 20:09:14 +0000

    Make carousel JS unobtrusive

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from 'stimulus'
+import 'tinycarousel'
+
+export default class extends Controller {
+  static targets = [ 'carousel' ]
+
+  connect() {
+    $(this.carouselTarget).tinycarousel({ 
+      pager: true, 
+      start: 1, 
+      interval: true, 
+      intervaltime: 5000, 
+      rewind: true, 
+      animation: true 
+    })
+  }
+}
