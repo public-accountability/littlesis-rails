08817b290bb0f0dd04a9083a7f6c008a9879ba10
Rob Lucas <roblucas@insicuri.net>
2021-03-22 18:01:20 +0000

    Move entity merger JS into stimulus

---
+++ app/javascript/controllers/entity_merge_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from 'stimulus'
+import mustache from 'mustache'
+
+export default class extends Controller {
+  static targets = [ 'table', 'mergeButton' ] 
+  static values = { possibleMerges: Array, path: String }
+
+  initialize() {
+    const possibleMerges = this.possibleMergesValue
+    const path = this.pathValue
+    const mergeButton = $(this.mergeButtonTarget).html()
+
+    const selectButton = function(data) {
+      return mustache.render(mergeButton, {path: `${path}&${$.param({dest: data })}`})
+    }
+
+    $(this.tableTarget).DataTable( {
+      searching: false,
+      lengthChange: false,
+      pageLength: 15,
+      order: [],
+      data: possibleMerges,
+      columns: [
+        { title: "Name", render: renderName },
+        { title: "Description" , data: 'blurb', orderable: false },
+        { title: "Types", data: 'types', orderable: false },
+        { title: "Select", data: 'id', orderable: false, render: selectButton }
+      ]
+    })
+  }
+}
+
+const renderName = function(data, type, row) {
+  return $('<a>', { href: row.slug, text: row.name, target: '_blank' }).prop('outerHTML')
+}
