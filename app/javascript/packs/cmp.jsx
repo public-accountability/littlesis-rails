8468295d0aa89349bb4521d116e0ef22c5ede6e6
Rob Lucas <roblucas@insicuri.net>
2021-04-05 13:06:13 +0100

    Tidy up Corporate Mapping Project JS

--- app/javascript/packs/cmp.jsx
+++ app/javascript/packs/cmp.jsx
@@ -1,26 +1,23 @@
 /*
-Javascript for the cmp landing page
-
-Assumes datatables and jquery has been loaded in window
+Javascript for the Corporate Mapping Project landing page
 */
-const $ = window.$;
+import React from 'react'
+import ReactDOM from 'react-dom'
+import datatable from 'datatables.net'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import cmp_entities from './cmp/data.json';
+import cmp_entities from './cmp/data.json'
 // import EntityTagSearch from './tags/EntityTagSearch';
-import { EntitySearch } from './search/EntitySearch';
+import { EntitySearch } from './search/EntitySearch'
 
 const entityLink = (row) => {
   return `
 <div class="cmp-strata-entity-link">
   <a href=${row.url} target='_blank'>${row.name}</a>
   <div class="cmp-strata-blurb">${row.blurb || ''}</div>
-</div>`;
-};
+</div>`
+}
 
-const renderName = (data, type, row, meta) => type === 'display' ? entityLink(row) : data;
+const renderName = (data, type, row, meta) => type === 'display' ? entityLink(row) : data
 
 const columns = [
   { "data": "name", "title": "Name", "render": renderName }
@@ -33,19 +30,19 @@ const initializeDatatable = () => {
     "language": {
       "search": "Filter:"
     }
-  });
-};
+  })
+}
 
 const initializeEntityTagSearch = () => {
   ReactDOM.render(
     <EntitySearch tag="corporate-mapping-project"/>,
     document.getElementById('cmp-tag-search')
-  );
-};
+  )
+}
 
 const main = () => {
-  initializeDatatable();
-  initializeEntityTagSearch();
-};
+  initializeDatatable()
+  initializeEntityTagSearch()
+}
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", main)
