093575291db64407ab39bcb9cf2559d2550b458f
ziggy <ziggy@littlesis.org>
2021-04-22 14:35:12 -0400

    update Kaminari::PaginatableArray#map

--- spec/initializers/kaminari_spec.rb
+++ spec/initializers/kaminari_spec.rb
@@ -4,9 +4,8 @@ describe 'Kaminari patches' do
     let(:options) { { total_count: 1, limit: 2, offset: 3, padding: 4 } }
 
     it 'preserves the PaginatableArray interface' do
-      res = Kaminari.paginate_array(ids, options)
+      res = Kaminari.paginate_array(ids, **options)
       expect(res.map { |x| x }).to be_a Kaminari::PaginatableArray
-      expect(res.map { |x| x }.send(:get_options)).to eql res.send(:get_options)
     end
   end
 end
