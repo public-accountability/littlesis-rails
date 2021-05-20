beeb59721195dc66ef19ba6083a0f229e2fa8dfd
ziggy <ziggy@littlesis.org>
2021-05-20 14:39:35 -0400

    add NYC contributions dataset

--- app/models/external_dataset.rb
+++ app/models/external_dataset.rb
@@ -9,15 +9,16 @@ module ExternalDataset
 
   mattr_reader :descriptions do
     {
-      iapd_advisors: 'Investor Advisor corporations registered with the SEC',
-      iapd_schedule_a: 'Owners and board members of investor advisors',
+      iapd_advisors: 'Investor Advisor Corporations Registered with the SEC',
+      iapd_schedule_a: 'Owners and Board Members of Investor Advisors',
       nycc: 'New York City Council Members',
-      nys_disclosures: 'New Yorak State Campaign Contributions',
+      nyc_contributions: 'New York City Campaign Contributions',
+      nys_disclosures: 'New York State Campaign Contributions',
       nys_filers: 'New York State Campaign Finance Committees',
       fec_candidates: 'Candidates for US Federal Office',
       fec_committees: 'Federal Campaign Finance Committees',
       fec_contribution: 'Federal Campaign Finance Individual Contributions',
-      fec_donor: 'Donors extracted from FEC Individual Contributions'
+      fec_donor: 'Donors Extracted from FEC Individual Contributions'
     }.with_indifferent_access.freeze
   end
 
@@ -30,6 +31,10 @@ module ExternalDataset
     end
 
     # interface
+
+    def self.create_table
+      raise NotImplementedError
+    end
 
     def download
       raise NotImplementedError
@@ -106,6 +111,101 @@ module ExternalDataset
                  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
                  IGNORE 1 LINES
                  (#{File.open(@csv_file, &:readline).chomp})"
+    end
+  end
+
+  # nyc_contributions
+  # NOTE: currently hard-coded to use 2021's data
+  class NYCContribution < ApplicationRecord
+    extend DatasetInterface
+    self.dataset = :nyc_contributions
+
+    SOURCE_URL = "https://nyccfb.info/DataLibrary/2021_Contributions.csv"
+    LOCALPATH = ROOT_DIR.join('original', '2021_Contributions.csv').to_s.freeze
+    CSVPATH = ROOT_DIR.join('2021_Contributions.csv').to_s.freeze
+
+    def self.create_table
+      connection.create_table(table_name) do |t|
+        t.integer :election, limit: 2
+        t.string :officecd, limit: 2
+        t.text :recipid
+        t.string :canclass
+        t.text :recipname
+        t.text :committee
+        t.integer :filing, limit: 1
+        t.text :schedule
+        t.decimal :pageno
+        t.decimal :sequenceno
+        t.string :refno
+        t.date :date
+        t.date :refunddate
+        t.text :name
+        t.text :c_code
+        t.text :strno
+        t.text :strname
+        t.text :apartment
+        t.string :boroughcd, limit: 1
+        t.text :city
+        t.text :state
+        t.text :zip
+        t.text :occupation
+        t.text :empname
+        t.text :empstrno
+        t.text :empstrname
+        t.text :empcity
+        t.text :empstate
+        t.decimal :amnt, scale: 2, precision: 15
+        t.decimal :matchamnt
+        t.decimal :prevamnt
+        t.integer :pay_method, limit: 1
+        t.text :intermno
+        t.text :intermname
+        t.text :intstrno
+        t.text :intstrnm
+        t.text :intaptno
+        t.text :intcity
+        t.text :intst
+        t.text :intzip
+        t.text :intempname
+        t.text :intempstno
+        t.text :intempstnm
+        t.text :intempcity
+        t.text :intempst
+        t.text :intoccupa
+        t.text :purposecd
+        t.text :exemptcd
+        t.text :adjtypecd
+        t.text :rr_ind
+        t.text :seg_ind
+        t.text :int_c_code
+      end
+    end
+
+    def self.download
+      Utility.stream_file_if_not_exists(url: SOURCE_URL, path: LOCALPATH)
+    end
+
+    def self.extract
+      CSV.open(CSVPATH, 'w') do |outcsv|
+        CSV.foreach(LOCALPATH, headers: true) do |row|
+          row['DATE'] = format_date(row['DATE'])
+          row['REFUNDDATE'] = format_date(row['REFUNDDATE'])
+          outcsv << row
+        end
+      end
+    end
+
+    def self.load
+      run_query <<~SQL
+        COPY #{table_name} (ELECTION,OFFICECD,RECIPID,CANCLASS,RECIPNAME,COMMITTEE,FILING,SCHEDULE,PAGENO,SEQUENCENO,REFNO,DATE,REFUNDDATE,NAME,C_CODE,STRNO,STRNAME,APARTMENT,BOROUGHCD,CITY,STATE,ZIP,OCCUPATION,EMPNAME,EMPSTRNO,EMPSTRNAME,EMPCITY,EMPSTATE,AMNT,MATCHAMNT,PREVAMNT,PAY_METHOD,INTERMNO,INTERMNAME,INTSTRNO,INTSTRNM,INTAPTNO,INTCITY,INTST,INTZIP,INTEMPNAME,INTEMPSTNO,INTEMPSTNM,INTEMPCITY,INTEMPST,INTOCCUPA,PURPOSECD,EXEMPTCD,ADJTYPECD,RR_IND,SEG_IND,INT_C_CODE)
+        FROM '#{CSVPATH.gsub("/littlesis", "")}' WITH CSV;
+      SQL
+    end
+
+    private_class_method def self.format_date(d)
+      return nil if d.blank?
+
+      Date.strptime(d, '%m/%d/%Y').strftime('%F')
     end
   end
 
--- lib/scripts/data.rb
+++ lib/scripts/data.rb
@@ -5,17 +5,18 @@ if command == :help
   puts(<<HELP)
 littlesis data <command> <dataset>
 
- download | downloads original dataset
- extract  | creates csvs
- load     | loads data into the littlesis database
- export   | creates sql archives
- report   | table statistics
- list     | print list of datasets
+ download     | downloads original dataset
+ extract      | creates csvs
+ load         | loads data into the littlesis database
+ export       | creates sql archives
+ create_table | creates new table
+ report       | table statistics
+ list         | print list of datasets
 HELP
   exit
 end
 
-unless %i[download extract load export report list].include? command
+unless %i[download extract load export create_table report list].include? command
   abort "invalid command: #{command}"
 end
 
