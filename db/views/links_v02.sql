85aba8226f270ae02bf9eab37a771052c8fa176b
ziggy <ziggy@littlesis.org>
2021-05-27 15:26:19 -0400

    links_v02.sql

---
+++ db/views/links_v02.sql
@@ -0,0 +1,21 @@
+select
+  concat(id, 'normal') as id,
+  entity1_id,
+  entity2_id,
+  category_id,
+  id as relationship_id,
+  false as is_reverse
+from relationship
+where is_deleted = false
+
+union
+
+select
+  concat(id, 'reverse') as id,
+  entity2_id as entity1_id,
+  entity1_id as entity2_id,
+  category_id,
+  id as relationship_id,
+  true as is_reverse
+from relationship
+where is_deleted = false
