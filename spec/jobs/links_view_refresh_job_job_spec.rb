a0241eccef19e132912258fbcf2d47e6b84db015
Rob Lucas <roblucas@insicuri.net>
2021-05-27 11:33:21 +0100

    Minor correction to links job spec

--- spec/jobs/links_view_refresh_job_job_spec.rb
+++ spec/jobs/links_view_refresh_job_job_spec.rb
@@ -2,7 +2,7 @@ describe LinksViewRefreshJob, type: :job do
   include ActiveJob::TestHelper
 
   specify do
-    expect(Link).to receive(:refresh_materialized_view).once
+    allow(Link).to receive(:refresh_materialized_view).once
     expect { LinksViewRefreshJob.perform_later }.to have_enqueued_job.on_queue('default')
     perform_enqueued_jobs
   end
