8eb3524dcb4a26e22cae5f38693d200e55c77111
Rob Lucas <roblucas@insicuri.net>
2021-05-27 11:31:21 +0100

    Touch entities with relationships that have been updated while the Link.refresh job was running

--- app/jobs/links_view_refresh_job.rb
+++ app/jobs/links_view_refresh_job.rb
@@ -4,6 +4,15 @@ class LinksViewRefreshJob < ApplicationJob
   queue_as :default
 
   def perform
+    @time_started = Time.current
     Link.refresh_materialized_view
+  end
+
+  # Touch entities with relationships that were updated while this job was running, so
+  # that their caches automatically get refreshed with the new links data.
+  def after_perform
+    # rubocop:disable Rails/SkipsModelValidations
+    Entity.with_relationships.where('entity.updated_at > ?', @time_started).touch_all
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
