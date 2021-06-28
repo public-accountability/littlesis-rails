bec1c4d60dc99598bf8f3d84a3fc5f3d9dbf0fef
Rob Lucas <roblucas@insicuri.net>
2021-06-28 17:09:50 +0100

    Correct outdated migration table name

--- db/migrate/20210524101935_add_is_featured_column_to_relationships.rb
+++ db/migrate/20210524101935_add_is_featured_column_to_relationships.rb
@@ -1,6 +1,6 @@
 class AddIsFeaturedColumnToRelationships < ActiveRecord::Migration[6.1]
   def change
-    add_column :relationship, :is_featured, :boolean, null: false, default: false
-    add_index :relationship, :is_featured
+    add_column :relationships, :is_featured, :boolean, null: false, default: false
+    add_index :relationships, :is_featured
   end
 end
