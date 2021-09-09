d30a5fb6e1a57cbff558d7af6d180175a3218679
ziggy <ziggy@littlesis.org>
2021-09-09 16:45:11 -0400

    remove require iapd_importer

--- lib/importers.rb
+++ lib/importers.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'importers/congress_importer'
-require_relative 'importers/iapd_importer'
 require_relative 'importers/nycc_importer'
 require_relative 'importers/nys_filer_importer'
 
