e648b715387b2fc384215b420fe71836862a7878
ziggy <ziggy@littlesis.org>
2021-09-14 16:46:01 -0400

    permit all user setting keys

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -103,7 +103,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update,
-                                      keys: [:username, settings: [:show_stars]])
+                                      keys: [:username, { settings: UserSettings::DEFAULTS.keys }])
   end
 
   def user_settings_params
