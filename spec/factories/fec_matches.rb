5736608c6236e2f1fc96b6d599ab70497b0b7dd0
ziggy <ziggy@littlesis.org>
2021-09-27 16:16:03 -0400

    remove unused external data code

--- spec/factories/fec_matches.rb
+++ spec/factories/fec_matches.rb
@@ -1,11 +1,2 @@
 FactoryBot.define do
-  factory :entity_donor_fec_match, class: 'Entity' do
-    name { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
-    primary_ext { 'Person' }
-  end
-
-  factory :fec_match do
-    association :fec_contribution, factory: :fec_contribution
-    association :donor, factory: :entity_donor_fec_match
-  end
 end
