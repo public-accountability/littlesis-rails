90fb544aa8bfeed31b5073051b26a0db3cfb777e
ziggy <ziggy@littlesis.org>
2021-09-29 15:09:52 -0400

    lint tag header spec

--- spec/views/tags/_header.html.erb_spec.rb
+++ spec/views/tags/_header.html.erb_spec.rb
@@ -3,12 +3,12 @@ describe 'tags/header', :type => :view do
   let(:active_tab) { :entities }
 
   before do
-    render partial: 'tags/header.html.erb', locals: { tag: tag, active_tab: active_tab }
+    render partial: 'tags/header', locals: { tag: tag, active_tab: active_tab }
   end
 
   it 'contains correct tabs: all tagable classes and edits' do
     css 'ul.nav-tabs li', count: Tagable.categories.count + 1
-    (Tagable.categories + [:edits]). each { |tab| css "\#tag-nav-tab-#{tab}" }
+    (Tagable.categories + [:edits]).each { |tab| css "\#tag-nav-tab-#{tab}" }
   end
 
   context 'when lists tab is active' do
