b12fe4789b3773449642dac512b96e0691d896c2
ziggy <ziggy@littlesis.org>
2021-09-30 13:21:14 -0400

    remove entity view tests covered by feature tests

--- spec/views/entities/_actions.html.erb_spec.rb
+++ spec/views/entities/_actions.html.erb_spec.rb
@@ -19,35 +19,22 @@ describe "entities/actions" do
     end
 
     before do
-      expect(view).to receive(:user_signed_in?).and_return(true)
       assign(:entity, entity)
       render partial: 'entities/actions', locals: { entity: entity, current_user: user }
     end
 
-    context 'org page' do
+    describe 'org page' do
       specify { css 'a', text: 'remove' }
       specify { css 'a', text: 'add bulk' }
       specify { not_css 'a', text: 'match donations' }
     end
 
-    context 'person page' do
+    describe 'person page' do
       let(:entity) { person }
+
       specify { css 'a', text: 'remove' }
       specify { css 'a', text: 'add bulk' }
       specify { css 'a', text: 'match donations' }
     end
-  end
-
-  context 'when user is not signed in' do
-    before do
-      expect(view).to receive(:user_signed_in?).and_return(false)
-      assign(:entity, entity)
-      render partial: 'entities/actions', locals: { entity: entity, current_user: nil }
-    end
-
-    specify { css 'a', text: 'add relationship' }
-    specify { css 'a', text: 'edit' }
-    specify { css 'a', text: 'flag' }
-    specify { css '#entity-edited-history' }
   end
 end
