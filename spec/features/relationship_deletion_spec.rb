dab1ded9bc5bfc2fcc58fa2e69513327a55bd9cd
ziggy <ziggy@littlesis.org>
2021-09-30 14:41:03 -0400

    fix updated byline after relationship deletion

--- spec/features/relationship_deletion_spec.rb
+++ spec/features/relationship_deletion_spec.rb
@@ -26,36 +26,27 @@ describe 'deleting relationships' do
   end
 
   describe 'history on associated entity profile pages' do
-    let(:relationship) do
+    let!(:relationship) do
       with_versioning_for(user1) do
         Relationship.create!(entity: person, related: org, category_id: 1)
       end
     end
 
-    before { relationship }
+    before { login_as(user2, scope: :user) }
 
-    it 'shows correct user on entity page' do
+    after { logout(:user) }
+
+    it 'shows correct user on entity page after deleting' do
       visit entity_path(person)
-      expect(find('#entity-edited-history').text).to include user1.username
-    end
-
-    context 'when logged in as user2' do
-      before { login_as(user2, scope: :user) }
-
-      after { logout(:user) }
-
-      it 'shows correct user on entity page after deleting' do
-        visit entity_path(person)
-        expect(find('#entity-edited-history').text).to include user1.username
-        with_versioning_for(user2) do
-          relationship.current_user = user2
-          relationship.soft_delete
-        end
-        visit entity_path(person)
-        expect(find('#entity-edited-history').text).to include user2.username
-        visit entity_path(org)
-        expect(find('#entity-edited-history').text).to include user2.username
+      expect(find('#entity-edited-history em > span')['title']).to include user1.username
+      with_versioning_for(user2) do
+        relationship.current_user = user2
+        relationship.soft_delete
       end
+      visit entity_path(person)
+      expect(find('#entity-edited-history em > span')['title']).to include user2.username
+      visit entity_path(org)
+      expect(find('#entity-edited-history em > span')['title']).to include user2.username
     end
   end
 end
