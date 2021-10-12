e97727581092955873e61b37338a9bc894a8160e
ziggy <ziggy@littlesis.org>
2021-10-12 11:52:34 -0400

    fix cmp partners page

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 class PartnersController < ApplicationController
   def cmp
+    @cmp_case_studies = CSV.foreach(Rails.root.join('data', 'cmp_top_50.csv').to_s, headers: true)
   end
 end
