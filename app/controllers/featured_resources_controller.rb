0aae1d2a94062512580bb20e31f92462354e4be8
ziggy <ziggy@littlesis.org>
2021-10-14 10:27:21 -0400

    controller actions for FeaturedResource

--- app/controllers/featured_resources_controller.rb
+++ app/controllers/featured_resources_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class FeaturedResourcesController < ApplicationController
+  include EntitiesHelper
   before_action :authenticate_user!, :admins_only
 
   def create
+    FeaturedResource.create!(params.require(:featured_resource).permit(:entity_id, :title, :url))
+    redirect_to concretize_entity_path(
+                  Entity.find(params.require(:featured_resource).require(:entity_id))
+                )
   end
 
   def destroy
+    fr = FeaturedResource.find(params[:id])
+    entity = fr.entity
+    fr.destroy!
+    redirect_to concretize_entity_path(entity)
   end
 end
