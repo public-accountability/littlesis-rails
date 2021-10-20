06580e4ac21678192fc1fd6aa5d6b6dbce77656e
ziggy <ziggy@littlesis.org>
2021-10-20 11:18:41 -0400

    webpacker config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,27 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  api: 'packs/common/api'
-}))
-
-environment.loaders.append('babel', {
-  test: /\.js?/,
-  use: 'babel-loader',
-  exclude: /node_modules/
-})
-
-environment.loaders.append('json', {
-  test: /\.json?/,
-  use: 'json-loader',
-  exclude: /node_modules/,
-  type: 'javascript/auto'
-})
-
-environment.config.externals = {
-  oligrapher: 'Oligrapher'
-}
 
 module.exports = environment
