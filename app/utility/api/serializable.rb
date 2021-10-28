6263f92defbdbab25797f186fe45b120bbfa1071
ziggy <ziggy@elephant-bird.net>
2021-10-28 15:17:43 -0400

    refactor api

--- app/utility/api/serializable.rb
+++ app/utility/api/serializable.rb
@@ -5,33 +5,34 @@ module Api
     extend ActiveSupport::Concern
 
     class_methods do
-      def as_api_json(ids, **options)
-        Api.as_api_json(find(ids), **options)
+      def as_api_json(ids, meta: true)
+        Api.as_api_json(find(ids), meta: meta)
       end
     end
 
-    def as_api_json(route: nil, meta: true, **serializer_options)
-      json = route.present? ? public_send("api_#{route}") : api_json(serializer_options)
+    def as_api_json(meta: true, exclude: nil)
+      json = api_json(exclude: exclude)
       meta ? Api::META_HASH.merge(json) : json
     end
 
     # Options Hash-> Hash
-    def api_json(serializer_options = {})
-      json = { 'data' => api_data(serializer_options) }
-      json['included'] = api_included if api_included.present?
+    def api_json(exclude: nil, skip_included: false)
+      json = { 'data' => api_data(exclude: exclude) }
+      json['included'] = api_included if !skip_included && api_included.present?
       json
     end
 
-    def api_data(serializer_options = {})
+    def api_data(**options)
       {
         'type' => self.class.name.tableize.dasherize,
         'id' => id,
-        'attributes' => api_attributes(serializer_options)
+        'attributes' => api_attributes(**options)
       }.merge(api_links)
     end
 
     def api_links
       return {} unless api_linkable?
+
       link = Rails.application.routes.url_helpers.public_send("#{self.class.name.downcase}_url", self)
       { 'links' => { 'self' => link } }
     end
@@ -40,8 +41,8 @@ module Api
     def api_included
     end
 
-    def api_attributes(serializer_options = {})
-      Api::Serializer.new(self, **serializer_options).attributes
+    def api_attributes(**options)
+      Api::Serializer.new(self, **options).attributes
     end
 
     private
