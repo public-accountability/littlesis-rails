20221a6d77c0064b625ab29acf37b216335dc987
ziggy <ziggy@elephant-bird.net>
2021-11-02 16:25:11 -0400

    remove delayed_job from bin/littlesis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.web_console.allowed_ips = ['172.0.0.0/8']
   config.web_console.whiny_requests = false
 
-  config.good_job.execution_mode = :async
+  config.good_job.execution_mode = :async  # :external
   config.good_job.max_threads = 1
 
   # TODO: Maybe enable this?
