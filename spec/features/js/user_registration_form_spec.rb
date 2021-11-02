debc0f5e6d3c844fff88adac62532b19b600c93f
ziggy <ziggy@elephant-bird.net>
2021-11-02 09:40:33 -0400

    run registration tests without `within`

--- spec/features/js/user_registration_form_spec.rb
+++ spec/features/js/user_registration_form_spec.rb
@@ -11,68 +11,60 @@ feature 'User registration form', type: :feature, js: true do
   end
 
   scenario 'user is prompted to accept the terms of use' do
-    within '#new_user' do
-      fill_in 'First Name', with: 'Oedipa'
-      fill_in 'Last Name', with: 'Maas'
-      fill_in 'Email', with: 'oedipa@maas.net'
-      fill_in 'Username', with: 'oedipa'
-      fill_in 'Password (Minimum 8 letters)', with: 'trolleron'
-      fill_in 'Password confirmation', with: 'volauvent'
-      fill_in 'about-you-input', with: 'investigating Trystero and related matters'
-      fill_in_math_captcha('math_captcha')
-      click_on 'Sign up'
+    fill_in 'First Name', with: 'Oedipa'
+    fill_in 'Last Name', with: 'Maas'
+    fill_in 'Email', with: 'oedipa@maas.net'
+    fill_in 'Username', with: 'oedipa'
+    fill_in 'Password (Minimum 8 letters)', with: 'trolleron'
+    fill_in 'Password confirmation', with: 'volauvent'
+    fill_in 'about-you-input', with: 'investigating Trystero and related matters'
+    fill_in_math_captcha('math_captcha')
+    click_on 'Sign up'
 
-      expect(page).to have_css('.parsley-required', text: 'Unfortunately, you have to accept the terms first')
-    end
+    expect(page).to have_css('.parsley-required', text: 'Unfortunately, you have to accept the terms first')
   end
 
   scenario 'user enters a too-short password' do
-    within '#new_user' do
-      fill_in 'First Name', with: 'Oedipa'
-      fill_in 'Last Name', with: 'Maas'
-      fill_in 'Email', with: 'oedipa@maas.net'
-      fill_in 'Username', with: 'oedipa'
-      fill_in 'Password (Minimum 8 letters)', with: 'wamp'
-      fill_in 'Password confirmation', with: 'wamp'
-      fill_in 'about-you-input', with: 'investigating Trystero and related matters'
-      fill_in_math_captcha('math_captcha')
-      find('#terms_of_use').check
-      click_on 'Sign up'
+    fill_in 'First Name', with: 'Oedipa'
+    fill_in 'Last Name', with: 'Maas'
+    fill_in 'Email', with: 'oedipa@maas.net'
+    fill_in 'Username', with: 'oedipa'
+    fill_in 'Password (Minimum 8 letters)', with: 'wamp'
+    fill_in 'Password confirmation', with: 'wamp'
+    fill_in 'about-you-input', with: 'investigating Trystero and related matters'
+    fill_in_math_captcha('math_captcha')
+    find('#terms_of_use').check
+    click_on 'Sign up'
 
-      expect(page).to have_css('.parsley-minlength', text: 'Passwords must be at least 8 characters')
-    end
+    expect(page).to have_css('.parsley-minlength', text: 'Passwords must be at least 8 characters')
   end
 
   scenario 'user enters a bad password confirmation' do
-    within '#new_user' do
-      fill_in 'First Name', with: 'Oedipa'
-      fill_in 'Last Name', with: 'Maas'
-      fill_in 'Email', with: 'oedipa@maas.net'
-      fill_in 'Username', with: 'oedipa'
-      fill_in 'Password (Minimum 8 letters)', with: 'wampwamp'
-      fill_in 'Password confirmation', with: 'pwampwam'
-      fill_in 'about-you-input', with: 'investigating Trystero and related matters'
-      fill_in_math_captcha('math_captcha')
-      find('#terms_of_use').check
-      click_on 'Sign up'
+    fill_in 'First Name', with: 'Oedipa'
+    fill_in 'Last Name', with: 'Maas'
+    fill_in 'Email', with: 'oedipa@maas.net'
+    fill_in 'Username', with: 'oedipa'
+    fill_in 'Password (Minimum 8 letters)', with: 'wampwamp'
+    fill_in 'Password confirmation', with: 'pwampwam'
+    fill_in 'about-you-input', with: 'investigating Trystero and related matters'
+    fill_in_math_captcha('math_captcha')
+    find('#terms_of_use').check
+    click_on 'Sign up'
 
-      expect(page).to have_css('.parsley-equalto', text: 'Your password and password confirmation do not match :(')
-    end
+    expect(page).to have_css('.parsley-equalto', text: 'Your password and password confirmation do not match :(')
   end
 
   scenario 'user is prompted to fill out "about you"' do
-    within '#new_user' do
-      fill_in 'First Name', with: 'Oedipa'
-      fill_in 'Last Name', with: 'Maas'
-      fill_in 'Email', with: 'oedipa@maas.net'
-      fill_in 'Username', with: 'oedipa'
-      fill_in 'Password (Minimum 8 letters)', with: 'wampwamp'
-      fill_in 'Password confirmation', with: 'wampwamp'
-      fill_in_math_captcha('math_captcha')
-      find('#terms_of_use').check
+    fill_in 'First Name', with: 'Oedipa'
+    fill_in 'Last Name', with: 'Maas'
+    fill_in 'Email', with: 'oedipa@maas.net'
+    fill_in 'Username', with: 'oedipa'
+    fill_in 'Password (Minimum 8 letters)', with: 'wampwamp'
+    fill_in 'Password confirmation', with: 'wampwamp'
+    fill_in_math_captcha('math_captcha')
+    find('#terms_of_use').check
 
-      click_on 'Sign up'
-    end
+    click_on 'Sign up'
 
     expect(page).to have_css('#about-you-input:focus')
   end
