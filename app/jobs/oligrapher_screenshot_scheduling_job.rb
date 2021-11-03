7edfea5daf5f3ab6dcfefcd7653cb58a6a126ed8
ziggy <ziggy@elephant-bird.net>
2021-11-03 15:46:11 -0400

    refactor OligrapherScreenshotSchedulingJob

--- app/jobs/oligrapher_screenshot_scheduling_job.rb
+++ app/jobs/oligrapher_screenshot_scheduling_job.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class OligrapherScreenshotSchedulingJob < ApplicationJob
-  def perform(type, hours: 25)
+  def perform(type)
     case type
     when :recent
-      NetworkMap.where(is_private: false).where('updated_at > ?', hours.hours.ago).each do |map|
-        OligrapherScreenshotJob.perform_later(map.id, wait_after: 1)
+      NetworkMap.where(is_private: false).where('updated_at >= ?', 1.day.ago).limit(100).each do |map|
+        OligrapherScreenshotJob.perform_later(map.id)
       end
     when :missing
-      NetworkMap.where(is_private: false, screenshot: nil).find_each(batch_size: 100).each do |map|
-        OligrapherScreenshotJob.perform_later(map.id, wait_after: 1)
+      NetworkMap.where(is_private: false, screenshot: nil).order('updated_at desc').limit(250).each do |map|
+        OligrapherScreenshotJob.perform_later(map.id)
       end
     else
       raise ArgumentError
