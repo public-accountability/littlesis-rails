953e325d25b0d69711095f364435a1c104233379
ziggy <ziggy@elephant-bird.net>
2021-11-03 10:23:37 -0400

    update IA tests

--- spec/utility/internet_archive_spec.rb
+++ spec/utility/internet_archive_spec.rb
@@ -1,20 +1,31 @@
 describe InternetArchive do
   describe 'save_url' do
-    let(:http) { double('http') }
     it 'makes a request to the internet archive to save the url' do
-      expect(Net::HTTP).to receive(:start).and_yield(http)
-      expect(Net::HTTP::Get).to receive(:new)
-                                  .with('/save/https://littlesis.org', InternetArchive::HEADERS)
-      expect(http).to receive(:request).once
+      allow(Rails.env).to receive(:production?).and_return(true)
+
+      response = instance_double("Net::HTTPRedirection").tap do |r|
+        allow(r).to receive(:is_a?).with(Net::HTTPRedirection).and_return(true)
+        allow(r).to receive(:[]).with('location').and_return('https://...')
+      end
+
+      expect(Net::HTTP).to receive(:get_response).with(kind_of(URI)).and_return(response)
+
+      expect(Rails.logger).to receive(:info).with(/Saved to internet archive/)
 
       InternetArchive.save_url('https://littlesis.org')
     end
 
-    it 'logs waring if encourters erros' do
-      expect(Net::HTTP).to receive(:start).and_raise(SocketError)
+    it 'logs warning when response is not a redirect' do
+      allow(Rails.env).to receive(:production?).and_return(true)
+
+      response = instance_double("Net::HTTPUnauthorized").tap do |r|
+        allow(r).to receive(:is_a?).with(Net::HTTPRedirection).and_return(false)
+      end
+
+      expect(Net::HTTP).to receive(:get_response).with(kind_of(URI)).and_return(response)
+
       expect(Rails.logger).to receive(:warn).once
-      expect { InternetArchive.save_url('https://littlesis.org') }
-        .not_to raise_error
+      InternetArchive.save_url('https://littlesis.org')
     end
   end
 end
