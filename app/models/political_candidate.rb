c111bc269ef6580f19d86d2a0c66bc344d415716
ziggy <ziggy@elephant-bird.net>
2021-11-19 12:22:34 -0500

    remove political view spec, add fec methods to PoliticalCandidate

--- app/models/political_candidate.rb
+++ app/models/political_candidate.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PoliticalCandidate < ApplicationRecord
   has_paper_trail on: [:update],
                   meta: { entity1_id: :entity_id }
@@ -6,5 +8,13 @@ class PoliticalCandidate < ApplicationRecord
 
   def fec_candidate_ids
     entity.external_links.fec_candidate.pluck(:link_id)
+  end
+
+  def principle_committee_ids
+    ExternalDataset.fec_candidates.where(cand_id: fec_candidate_ids).map { |cand| cand.principle_committee.cmte_id }.uniq
+  end
+
+  def latest_committee
+    ExternalDataset.fec_committees.where(cmte_id: principle_committee_ids).order(fec_year: :desc).first
   end
 end
