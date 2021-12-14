e444a4a1a0d6a46e3f9b698e1c2e773491badf97
ziggy <ziggy@elephant-bird.net>
2021-12-14 13:33:48 -0500

    remove schema.rb; add functions structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -38,6 +38,41 @@ COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
 
 --
+-- Name: get_network_map_search_tsvector(bigint); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.get_network_map_search_tsvector(network_map_id bigint) RETURNS tsvector
+    LANGUAGE sql
+    AS $_$
+        SELECT setweight(to_tsvector(coalesce(network_maps.title,'')), 'A')  ||
+                      setweight(to_tsvector(coalesce(network_maps.description,'')), 'B') ||
+                      setweight(to_tsvector(coalesce(array_to_string(annotations.content, ' '), '')), 'C') ||
+                      setweight(to_tsvector(coalesce(array_to_string(entities.names, ' '), '')), 'C')
+        FROM network_maps,
+              LATERAL (
+                SELECT array_agg(entities.name) as names
+                -- array_agg(entities.blurb) as blurbs
+                FROM (
+                       SELECT json_object_keys(graph_data::json -> 'nodes') as keys
+                       FROM network_maps as sub
+                       WHERE sub.id = network_maps.id
+                ) AS nodes
+                INNER JOIN entities on entities.id = keys::bigint
+                WHERE nodes.keys ~ '^\d+$'
+              ) as entities,
+              LATERAL (
+                SELECT array_agg(content.content) as content
+                FROM  (
+                        SELECT regexp_replace((json_array_elements(annotations_data::json)->> 'text') || ' ' || (json_array_elements(annotations_data::json)->> 'header'), E'<[^>]+>', '', 'gi') AS content
+                        FROM network_maps as sub
+                        WHERE sub.id = network_maps.id
+                      ) as content
+             ) as annotations
+        WHERE network_maps.id = network_map_id
+$_$;
+
+
+--
 -- Name: is_numeric(text); Type: FUNCTION; Schema: public; Owner: -
 --
 
