fc950461656b7341d846f9507d3574001f4c40ed
ziggy <ziggy@littlesis.org>
2022-01-26 15:11:45 -0500

    use connection.exec_query instead of .execute

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -60,12 +60,12 @@ class ApplicationRecord < ActiveRecord::Base
   # example:
   #   execute_one('SELECT COUNT(*) from versions') => 100
   def self.execute_one(sql)
-    connection.execute(sql).values.first.first
+    connection.exec_query(sql).rows.first.first
   end
 
-  # convenience function for running `.connection.execute`
+  # convenience function for running `.connection.exec_query`
   def self.execute_sql(*args)
-    connection.execute(*args)
+    connection.exec_query(*args)
   end
 
   def self.sqlize_array(arr)
