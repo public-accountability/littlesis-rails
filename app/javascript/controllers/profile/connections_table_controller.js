e1cae8da6e91619734a63cbe51352e44962adad8
ziggy <ziggy@littlesis.org>
2022-02-10 18:12:29 -0500

    category and minimum amount filter

--- app/javascript/controllers/profile/connections_table_controller.js
+++ app/javascript/controllers/profile/connections_table_controller.js
@@ -1,7 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['table']
+
   initialize() {
-    $(this.element).find('table').DataTable()
+    $.fn.dataTable.ext.search.push(function(settings, searchData, index, rowData, counter) {
+      let minAmount = Number(document.getElementById('connections-table-minimum-amount-filter').value)
+      if (minAmount > 0) {
+        return Number(rowData[4]) > minAmount
+      } else {
+        return true
+      }
+    })
+
+    $(this.tableTarget).DataTable()
+  }
+
+  changeCategory(event) {
+    const categoryId = event.target.value
+
+    if (categoryId) {
+      this.api().column('category:name').search(`category-id-${categoryId}`).draw()
+    } else {
+      this.api().column('category:name').search('').draw()
+    }
+  }
+
+  redraw() {
+    this.api().draw()
+  }
+
+  api () {
+    return $(this.tableTarget).DataTable()
   }
 }
