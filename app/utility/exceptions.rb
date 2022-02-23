f4bc4bbf8059b32e57660d0295167bf2f8b4b88b
ziggy <ziggy@littlesis.org>
2022-02-23 15:33:24 -0500

    custom exceptions inherent from LittleSisError

--- app/utility/exceptions.rb
+++ app/utility/exceptions.rb
@@ -2,55 +2,55 @@
 
 module Exceptions
   class LittleSisError < StandardError; end
-  class PermissionError < StandardError; end
-  class NotFoundError < StandardError; end
-  class MissingApiTokenError < StandardError; end
-  class InvalidRelationshipCategoryError < StandardError; end
-  class RestrictedUserError < StandardError; end
-  class UserCannotEditError < StandardError; end
+  class PermissionError < LittleSisError; end
+  class NotFoundError < LittleSisError; end
+  class MissingApiTokenError < LittleSisError; end
+  class InvalidRelationshipCategoryError < LittleSisError; end
+  class RestrictedUserError < LittleSisError; end
+  class UserCannotEditError < LittleSisError; end
   class InvalidEntityIdError < LittleSisError; end
   class OligrapherAssetsError < LittleSisError; end
   class MatchingError < LittleSisError; end
-  class EditingDisabled <  LittleSisError; end
+  class EditingDisabled < LittleSisError; end
+  class HTTPRequestFailedError < LittleSisError; end
 
-
-  class CannotRestoreError < StandardError
+  class CannotRestoreError < LittleSisError
     def message
       "Cannot restore a model that has not yet been deleted"
     end
   end
 
-    class MissingEntityAssociationDataError < StandardError
-      def message
-        "Missing association data for this Entity"
-      end
+  class MissingEntityAssociationDataError < LittleSisError
+    def message
+      "Missing association data for this Entity"
     end
+  end
 
-    class UnauthorizedBulkRequest < StandardError
-      def message
-        "User lacks priveleges to submit bulk request of this size"
-      end
+  class UnauthorizedBulkRequest < LittleSisError
+    def message
+      "User lacks priveleges to submit bulk request of this size"
     end
+  end
 
-    class InvalidUrlError < ArgumentError
-      def message
-        "The URL is invalid"
-      end
+  class InvalidUrlError < ArgumentError
+    def message
+      "The URL is invalid"
     end
+  end
 
-    class ModelIsDeletedError < ActiveRecord::ActiveRecordError
-      def message
-        "The model has been deleted"
-      end
+  class ModelIsDeletedError < ActiveRecord::ActiveRecordError
+    def message
+      "The model has been deleted"
     end
+  end
 
-    class ThatsWeirdError < StandardError
+  class ThatsWeirdError < LittleSisError
     def message
       "Well, that's weird."
     end
   end
 
-  class MergedEntityError < StandardError
+  class MergedEntityError < LittleSisError
     attr_reader :merged_entity
 
     def initialize(merged_entity)
@@ -63,13 +63,13 @@ module Exceptions
     end
   end
 
-  class RedundantMergeReview < StandardError
+  class RedundantMergeReview < LittleSisError
     def message
       "Attempting to review a merge request that has already been reviewed."
     end
   end
 
-  class MissingCategoryIdParamError < StandardError
+  class MissingCategoryIdParamError < LittleSisError
     def message
       'missing required paramater: "category_id"'
     end
