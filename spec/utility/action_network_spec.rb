04aeed957cdd47b30848a17818202d9da0396353
ziggy <ziggy@littlesis.org>
2022-02-24 10:46:22 -0500

    fix error in ActionNetwork

---
+++ spec/utility/action_network_spec.rb
@@ -0,0 +1,8 @@
+describe ActionNetwork do
+  specify 'email_from_user' do
+    user = User.new(email: 'user@example.com')
+    expect(ActionNetwork.email_from_user(user)).to eq 'user@example.com'
+    expect(ActionNetwork.email_from_user('user@example.com')).to eq 'user@example.com'
+  end
+
+end
