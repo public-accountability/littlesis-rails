b06fd7d436df077a07f06be86963524d6aef3663
ziggy <ziggy@littlesis.org>
2022-03-04 16:03:27 -0500

    prevent unlimited retrying of jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,4 @@
 class ApplicationJob < ActiveJob::Base
+  # see https://github.com/bensheldon/good_job#exceptions-retries-and-reliability and https://guides.rubyonrails.org/active_job_basics.html#exceptions
+  retry_on StandardError, wait: :exponentially_longer, attempts: 20
 end
--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,1 +1,2 @@
 GoodJob.preserve_job_records = true
+GoodJob.retry_on_unhandled_error = false
