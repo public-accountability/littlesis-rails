cb68bb86fea7dc0d98aba3d9bcbef214209ba622
ziggy <ziggy@littlesis.org>
2022-04-07 14:48:03 -0400

    update new entity specs

--- spec/features/new_entity_spec.rb
+++ spec/features/new_entity_spec.rb
@@ -1,28 +1,25 @@
 describe '/entities/new', type: :feature do
-  let(:user) { create_basic_user }
+  let(:user) { create_editor }
 
   before { login_as(user, scope: :user) }
 
   after { logout(:user) }
 
-  describe 'linked to add entity with page a name' do
-    let(:url) { '/entities/new?name=exxon' }
-
-    before { visit url }
-
-    it 'has name field already filled out' do
-      successfully_visits_page url
-      expect(find('#entity_name').value).to eql 'exxon'
-    end
-  end
-
   describe 'creating a new entity' do
     let(:name) { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
     let(:blurb) { Faker::Quotes::Shakespeare.hamlet_quote.truncate(200) }
 
-    context 'when user is confirmed over 60 minutes  ago' do
+    describe 'linked to add entity with page a name' do
+      before { visit '/entities/new?name=exxon' }
+
+      it 'has name field already filled out' do
+        successfully_visits_page '/entities/new?name=exxon'
+        expect(find('#entity_name').value).to eq 'exxon'
+      end
+    end
+
+    describe 'when user is an editor' do
       before do
-        user.update_columns :confirmed_at => 61.minutes.ago
         visit '/entities/new'
       end
 
@@ -41,28 +38,30 @@ describe '/entities/new', type: :feature do
       end
     end
 
-    context 'when user is confirmed less than 60 minutes ago' do
-      before do
-        user.update_columns :confirmed_at => 50.minutes.ago
-        visit '/entities/new'
-      end
+    # describe 'when user is not an editor' do
+    #   let(:user) { create_user }
 
-      let!(:entity_count) { Entity.count }
+    #   let!(:entity_count) { Entity.count }
 
-      it 'cannot create a new person' do
-        successfully_visits_page '/entities/new'
+    #   before do
+    #     visit '/entities/new'
+    #   end
 
-        fill_in 'entity_name', with: name
-        fill_in 'entity_blurb', with: blurb
-        choose 'Person'
-        click_button 'Add'
 
-        successfully_visits_page '/home/dashboard'
+    #   it 'cannot create a new person' do
+    #     successfully_visits_page '/entities/new'
 
-        expect(Entity.count).to eq entity_count
+    #     fill_in 'entity_name', with: name
+    #     fill_in 'entity_blurb', with: blurb
+    #     choose 'Person'
+    #     click_button 'Add'
 
-        expect(page).to have_text 'In order to prevent abuse, new users are restricted from editing for the first hour.'
-      end
-    end
+    #     successfully_visits_page '/home/dashboard'
+
+    #     expect(Entity.count).to eq entity_count
+
+    #     expect(page).to have_text 'In order to prevent abuse, new users are restricted from editing for the first hour.'
+    #   end
+    # end
   end
 end
