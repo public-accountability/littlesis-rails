fd5b84a7902739ed1e0c1b2c99a0249c0214f489
ziggy <ziggy@elephant-bird.net>
2024-03-15 16:10:29 -0400

    update about pages tests

--- spec/features/translation_spec.rb
+++ spec/features/translation_spec.rb
@@ -2,12 +2,12 @@
 
 describe 'Translation', type: :feature do
   it "defaults to english" do
-    visit '/about'
+    visit '/database/about'
     expect(page).to have_text "LittleSis is a free database"
   end
 
   it "uses spanish upon request" do
-    visit '/about?locale=es'
+    visit '/database/about?locale=es'
     expect(page).to have_text "Little Sis es una base de datos gratuita"
   end
 end
