d1e265cc9b8005cfa55fe68bf211f1f3d08ad860
Joseph Lacey <joseph@offkeydigital.com>
2024-10-14 11:56:38 -0700

    Remove modal background when closing form

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal"
 export default class extends Controller {
   close() {
+    this.element.nextSibling.nextSibling.remove()
     this.element.parentElement.removeAttribute("src")
     this.element.remove()
   }
