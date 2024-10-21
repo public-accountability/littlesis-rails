223121e46c4857ddc786ed46b67afbbef0c92026
Joseph Lacey <joseph@offkeydigital.com>
2024-10-21 10:01:10 -0700

    Refinements to the close button.  This includes a full page reload which needs to be improved.

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,9 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  close() {
-    this.element.nextSibling.nextSibling.remove()
-    this.element.parentElement.removeAttribute("src")
-    this.element.remove()
+
+  close(event) {
+    window.location.reload()
+  }
+
+  // hide modal when clicking ESC
+  // action: "keyup@window->turbo-modal#closeWithKeyboard"
+  closeWithKeyboard(e) {
+    console.log(e);
+    if (e.code === "Escape") {
+      this.close()
+    }
+  }
+  // hide modal when clicking outside of modal
+  // action: "click@window->turbo-modal#closeBackground"
+  closeBackground(e) {
+    console.log(e);
+    if (e && this.modalTarget.contains(e.target)) {
+      return
+    }
+    this.close()
   }
 }
