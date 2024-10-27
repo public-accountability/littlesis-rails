5d0fc3047d8b5855c155f44d9bedada5e209b301
jessib <jessib@riseup.net>
2024-10-26 17:49:26 -0700

    had logic flipped.

--- app/queries/lists_index_query.rb
+++ app/queries/lists_index_query.rb
@@ -4,11 +4,11 @@ class ListsIndexQuery
   SORTABLE_COLUMNS = %i[created_at entity_count name].freeze
   def initialize(user_id: nil)
     check_creator_snippet = user_id.present? ? "creator_user_id = #{user_id} OR" : ""
-    user_or_private_select = "*, IF(#{check_creator_snippet} access!=#{Permissions::ACCESS_PRIVATE}, 0,1) AS users_or_private"
+    users_or_not_private_select = "*, IF(#{check_creator_snippet} access!=#{Permissions::ACCESS_PRIVATE}, 1,0) AS users_or_not_private"
 
     @options = {
-      select: user_or_private_select,
-      with: {users_or_private: 1, is_deleted: false },
+      select: users_or_not_private_select,
+      with: {users_or_not_private: 1, is_deleted: false },
       page: 1,
       per_page: 20
     }
