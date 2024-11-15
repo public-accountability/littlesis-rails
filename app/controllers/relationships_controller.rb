86f4631d62db25af01b100722de28590fe209c6c
Joseph Lacey <joseph@offkeydigital.com>
2024-11-15 15:47:24 -0800

    Initial migration of add source to relationship to modal

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -70,6 +70,11 @@ class RelationshipsController < ApplicationController
     @selected_document_id = params[:new_ref].present? ? @relationship.documents.last&.id : nil
   end
 
+  def add_source
+    @relationship = Relationship.find(params[:id])
+    render partial: 'shared/reference_new', locals: {model: @relationship, reference: @reference }
+  end
+
   def edit_relationship
     @relationship = Relationship.find(params[:id])
     render partial: 'edit_relationship'
