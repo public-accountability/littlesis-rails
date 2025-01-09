3766b5f969fa46b50712306896abb730b00c1af8
Joseph Lacey <joseph@offkeydigital.com>
2025-01-09 10:08:30 -0800

    Correcting table name misspellings in migration file

--- db/migrate/20241030174515_delete_os_tables.rb
+++ db/migrate/20241030174515_delete_os_tables.rb
@@ -1,7 +1,7 @@
 class DeleteOsTables < ActiveRecord::Migration[7.1]
   def change
-    drop_table :os_candidate
-    drop_table :os_committee
+    drop_table :os_candidates
+    drop_table :os_committees
     drop_table :os_donations
     drop_table :os_entity_donor
     drop_table :os_matches
