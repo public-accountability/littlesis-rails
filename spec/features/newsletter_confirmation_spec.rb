633cb67580935a1270c41743a19e8852acc8868e
Joseph Lacey <joseph@offkeydigital.com>
2025-02-01 09:26:12 -0800

    Changing newsletter signup test to newsletter from tech.  The option was removed.

--- spec/features/newsletter_confirmation_spec.rb
+++ spec/features/newsletter_confirmation_spec.rb
@@ -5,7 +5,6 @@ describe 'Newsletters Signing' do
     visit '/newsletters/signup'
 
     expect(page).to have_checked_field(id: "newsletters_tags_newsletter")
-    expect(page).to have_checked_field(id: "newsletters_tags_tech")
     fill_in 'Email', with: email
 
     click_button 'Submit'
@@ -14,9 +13,9 @@ describe 'Newsletters Signing' do
   end
 
   specify 'confirming' do
-    confirmation_link = NewslettersConfirmationLink.create(Faker::Internet.email, ['tech'])
+    confirmation_link = NewslettersConfirmationLink.create(Faker::Internet.email, ['newsletter'])
     expect(NewsletterSignupJob).to receive(:perform_later)
-                                     .with(confirmation_link.email, [:tech]).once
+                                     .with(confirmation_link.email, [:newsletter]).once
     visit confirmation_link.url
     expect(page).to have_text "You are now subscribed!"
     visit confirmation_link.url
