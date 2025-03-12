05432ec9f34ee94e1559ad33143606ecc2db520a
Joseph Lacey <joseph@offkeydigital.com>
2025-03-12 15:22:44 -0700

    Further refactoring for hCatpcha verification concern

--- app/forms/concerns/form_hcaptcha.rb
+++ app/forms/concerns/form_hcaptcha.rb
@@ -4,27 +4,30 @@ module FormHcaptcha
   extend ActiveSupport::Concern
 
   included do
-     attr_accessor :user_signed_in
+     attr_accessor ":g-recaptcha-response", :user_signed_in
 
-     validate verify(params), :unless => :user_signed_in
+     validate :correct_captcha, :unless => :user_signed_in
    end
 
-  def verify(params)
+  private
+
+  def correct_captcha
+    errors.add(:base, 'hCaptcha could not be verified. Please try again.') unless verify_hcaptcha
+  end
+
+  def verify_hcaptcha
     # Include a test response if we're in testing mode
     if Rails.application.config.littlesis.hcaptcha_site_key == '10000000-ffff-ffff-ffff-000000000001'
-      params['g-recaptcha-response'] = "10000000-aaaa-bbbb-cccc-000000000001";
+      g-recaptcha-response = "10000000-aaaa-bbbb-cccc-000000000001";
     end
 
     uri = URI('https://hcaptcha.com/siteverify')
     res = Net::HTTP.post_form(uri, 'secret' => Rails.application.config.littlesis.hcaptcha_secret_key,
-                                   'response' => params['g-recaptcha-response'])
+                                   'response' => g-recaptcha-response)
 
     response_data = JSON.parse(res.body)
 
     return if ActiveModel::Type::Boolean.new.cast(response_data['success'])
-
-    flash[:errors] = 'hCaptcha could not be verified.'
-    redirect_to request.referrer || root_path
   end
 end
 
