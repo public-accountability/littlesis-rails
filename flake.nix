bc94c59865c75585c95dcc397900cc8f737b7083
Joseph Lacey <joseph@offkeydigital.com>
2025-07-09 15:53:53 -0700

    Adding direnv and Nix flake dev environment configuration

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A Nix-flake-based Ruby development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ruby_3_3 ];
+        };
+      });
+    };
+}
