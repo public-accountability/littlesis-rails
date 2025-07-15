abd61a4e2fecd4ec5338b4e1e15ace34bdebfbed
Joseph Lacey <joseph@offkeydigital.com>
2025-07-15 11:51:19 -0700

    Improvements to Nix flake configuration

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
   description = "A Nix-flake-based Ruby development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  inputs = {
+   # Proivdes legacy compatibility for nix-shell
+   flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+   # Provides some nice helpers for multiple system compatibility
+   flake-utils.url = "github:numtide/flake-utils";
+   # Specify the nixpkgs for our particular ruby version
+   # https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=ruby
+   nixpkgs.url = "github:nixos/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
+  };
 
-  outputs = inputs:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ ruby_3_3 ];
-        };
-      });
-    };
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    # Calls the provided function for each "default system", which
+    # is the standard set.
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        # instantiate the package set for the supported system, with our
+        # rust overlay
+        let pkgs = import nixpkgs { inherit system; };
+        in
+        # "unpack" the pkgs attrset into the parent namespace
+        with pkgs;
+        {
+          devShell = mkShell {
+            # Packages required for development.
+            buildInputs = [
+              ruby
+            ];
+          };
+        });
 }
