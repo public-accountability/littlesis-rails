7123da678f85085178163b3129eea30550942367
Joseph Lacey <joseph@offkeydigital.com>
2026-05-13 14:29:41 -0700

    Adding Rails ENV to HTML to exclude Analytics output modal from testing environment

--- app/javascript/controllers/analytics_opt_out_first_time_controller.js
+++ app/javascript/controllers/analytics_opt_out_first_time_controller.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const railsEnv = document.querySelector('meta[name="rails-env"]')?.content;
+
+  if (railsEnv === "test") {
+    return;
+  }
+
   const storageKey = "analytics_opt_out_shown";
 
   if (!localStorage.getItem(storageKey)) {
